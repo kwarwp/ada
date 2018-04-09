0a1fec0465697a1258389d6ea3324b2b920218cd
kwarwp <38007182+kwarwp@users.noreply.github.com>
2018-04-09 11:19:58 -0300

    Saved roxanne/main.py at Timestamp: 2018-04-09 11:19:41

--- roxanne/main.py
+++ roxanne/main.py
@@ -1,10 +1,10 @@
 # ada.roxanne.main.py
 from browser import document
 from _spy.vitollino.main import Cena, STYLE
-STYLE[&#039;width&#039;] = 790
-document[&#039;pydiv&#039;]
+STYLE[&amp;#039;width&amp;#039;] = 790
+document[&amp;#039;pydiv&amp;#039;]
 
-A_NORTE = &quot;https://i.imgur.com/aLEjWgB.png&quot;
+A_NORTE = &amp;quot;https://i.imgur.com/aLEjWgB.png&amp;quot;
 
 def main():
     a_norte = Cena(img=A_NORTE)
