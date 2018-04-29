5845b1adf26f21c743b54e307f5bc95cf3d03f5a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-04-29 09:19:10 -0300

    Saved roxanne/__log__.py at Timestamp: 2018-04-29 09:19:08

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -16,3 +16,11 @@ Sun Apr 29 2018 09:03:17 GMT-0300 (-03) -X- SuPyGirls -X-
   def main(:
             ^
 SyntaxError: invalid syntax
+
+{'date': 'Sun Apr 29 2018 09:19:08 GMT-0300 (-03) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 64
+  style = {position': 'absolute', 'overflow': 'hidden',
+                    ^
+SyntaxError: invalid syntax
+'''},
