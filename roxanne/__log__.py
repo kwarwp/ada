d4798c3ebdc1649d6b82f9f90b01f9963f2bf48b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-04-29 08:59:20 -0300

    Saved roxanne/__log__.py at Timestamp: 2018-04-29 08:59:18

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -3,4 +3,9 @@ XXXX - executa_acao if error else lambda self.error - XXXX
   oce = "https://i.imgur.com/oDqeaBp.jpg
                                         ^
 SyntaxError: EOL while scanning string literal
-XXXX - executa_acao if error else lambda self.error - XXXX
+XXXX - executa_acao if error else lambda self.error - XXXXSun Apr 29 2018 08:59:18 GMT-0300 (-03)
+
+ module <string> line 25
+  class Planta(Cena, Droppable)
+                                ^
+SyntaxError: invalid syntax
