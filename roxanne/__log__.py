0fdad891aaea378a53d08bf244c22617189e4c0c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-04-29 08:43:03 -0300

    Saved roxanne/__log__.py at Timestamp: 2018-04-29 08:43:02

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -1,1 +1,5 @@
 XXXX - executa_acao if error else lambda self.error - XXXX
+ module <string> line 21
+  oce = "https://i.imgur.com/oDqeaBp.jpg
+                                        ^
+SyntaxError: EOL while scanning string literal
