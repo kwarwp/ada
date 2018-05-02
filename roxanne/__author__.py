eb5656d12709c6eacf3170978be8bb7441bd9342
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-02 00:07:07 -0300

    Created roxanne/__author__.py at Timestamp: 2018-05-02 00:07:06

---
+++ roxanne/__author__.py
@@ -0,0 +1,1 @@
+{"author_nick": "carlo", "author_name": "Carlo Emmanoel", "author_email": "carlo@nce.ufrj.br", "author_org": "LABASE", "author_site": "http://labase.superpython.net", "author_public": "on"}
