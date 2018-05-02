204e8eb40435689bea35a0c2e297e9d80d20ee39
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-02 00:25:32 -0300

    Saved __author__.py at Timestamp: 2018-05-02 00:25:30

--- __author__.py
+++ __author__.py
@@ -1,3 +1,4 @@
 {
   "ada": {"author_nick": "carlo", "author_name": "Carlo Emmanoel", "author_email": "carlo@nce.ufrj.br", "author_org": "LABASE", "author_site": "http://labase.superpython.net", "author_public": "on"},
   "roxanne": {"author_nick": "carlo", "author_name": "Carlo Emmanoel", "author_email": "carlo@nce.ufrj.br", "author_org": "LABASE", "author_site": "http://labase.superpython.net", "author_public": "on"},
+"sara": {"author_nick": "carlo", "author_name": "Carlo Emmanoel", "author_email": "carlo@ufrj.br", "author_org": "LABASE", "author_site": "http://labase.superpython.net", "author_public": "on"},
