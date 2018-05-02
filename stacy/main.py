7d4fb477b8055005868413b583b1bd0e6fa92f9e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-02 00:32:08 -0300

    Saved stacy/main.py at Timestamp: 2018-05-02 03:32:07

--- stacy/main.py
+++ stacy/main.py
@@ -5,15 +5,17 @@ NEWTON = "http://4.bp.blogspot.com/-5gZeG2PCX-0/ViRIc3iUP_I/AAAAAAAACn4/IWvXueCe
 GALILEU = "https://pbs.twimg.com/profile_images/666413287639269377/-Yw4RtjG_400x400.png"
 MACA = "https://macmagazine.com.br/wp-content/uploads/2011/02/24-rainbows.png"
 ILUMINATTI = "https://vignette.wikia.nocookie.net/clubpenguin/images/2/2d/Illuminati.png/revision/latest?cb=20150117022611"
+GROOT = "https://vignette.wikia.nocookie.net/marvelcomicsfanon/images/9/98/Subject-401_%28Earth-4045%29.png"
 
 def teorias():
     inicio = Cena(img=NEWTON)
-    inicio_t = Texto(inicio, "Eis que a gravidade se faz")
+    inicio_t = Texto(inicio, "A gravidade da situação está em tornar tudo um material de consumo")
     iluminatti = Texto (inicio, "Ahhh mas disso você já sabia!")
-    inicio_e = Elemento (img = MACA, tit="Eis que a gravidade aparece", style = dict(left= 70,top=170, width=50, height=200,bottom=100))
+    inicio_e = Elemento (img = MACA, tit="Gravidade", style = dict(left= 70,top=170, width=50, height=200,bottom=100))
     #personagem = Elemento (img = linkpersonagem, tit="garotinho", style = dict(left= 150, top=100, width=60, height=200))
     lado1 = Cena(img=GALILEU, direita =inicio)
     lado1_e = Elemento(img=ILUMINATTI, tit='O universo não gira em torno de você não,bb', style = dict(left= 150, top=240, width=40, height=200, bottom=100))
+    iluminatti = Texto (inicio, "Ahhh mas disso você já sabia!")
     
     inicio_e.entra(inicio)
     inicio_e.vai = inicio_t.vai
