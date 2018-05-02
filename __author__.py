67e64228b9823c7ff2af89cca786877076f94382
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-02 00:38:16 -0300

    fix libby

--- __author__.py
+++ __author__.py
@@ -2,5 +2,4 @@
   "ada": {"author_nick": "carlo", "author_name": "Carlo Emmanoel", "author_email": "carlo@nce.ufrj.br", "author_org": "LABASE", "author_site": "http://labase.superpython.net", "author_public": "on"},
   "roxanne": {"author_nick": "carlo", "author_name": "Carlo Emmanoel", "author_email": "carlo@nce.ufrj.br", "author_org": "LABASE", "author_site": "http://labase.superpython.net", "author_public": "on"},
 "sara": {"author_nick": "carlo", "author_name": "Carlo Emmanoel", "author_email": "carlo@ufrj.br", "author_org": "LABASE", "author_site": "http://labase.superpython.net", "author_public": "on"},
-"libby": {"author_nick": "cetoli", "author_name": "Carlo Emmanoel", "author_email": "cetoli@gmail.com", "author_org": "", "author_site": "", "author_public": "on"},
 "libby": {"author_nick": "carlotolla", "author_name": "Carlo", "author_email": "hello@br", "author_org": "", "author_site": "", "author_public": "on"},
