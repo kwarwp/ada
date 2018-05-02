34781ca5fdea517e51c4be8c84597b4996dc700e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-02 00:25:34 -0300

    Created sara/__author__.py at Timestamp: 2018-05-02 00:25:33

---
+++ sara/__author__.py
@@ -0,0 +1,1 @@
+"sara": {"author_nick": "carlo", "author_name": "Carlo Emmanoel", "author_email": "carlo@ufrj.br", "author_org": "LABASE", "author_site": "http://labase.superpython.net", "author_public": "on"},
