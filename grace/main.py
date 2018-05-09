da27c1744a9919e45042df8823c2b3a48aecf64a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-09 14:55:00 -0300

    Saved grace/main.py at Timestamp: 2018-05-09 17:55:00

--- grace/main.py
+++ grace/main.py
@@ -1,3 +1,4 @@
 # ada.grace.main.py
-https://tse3.mm.bing.net/th?id=OIP.qvqwa_VKXUG0GWutYZGaOAHaGH&pid=15.1&P=0&w=208&h=172
+# https://tse3.mm.bing.net/th?id=OIP.qvqwa_VKXUG0GWutYZGaOAHaGH&pid=15.1&P=0&w=208&h=172
+
 
