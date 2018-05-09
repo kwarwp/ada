ae23a8f065999ee4e5ca23ad69c30ac03bf093ad
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-09 15:01:37 -0300

    Saved grace/main.py at Timestamp: 2018-05-09 18:01:36

--- grace/main.py
+++ grace/main.py
@@ -1,4 +1,2 @@
-# ada.grace.main.py
-# https://tse3.mm.bing.net/th?id=OIP.qvqwa_VKXUG0GWutYZGaOAHaGH&pid=15.1&P=0&w=208&h=172
-
-
+from_spy.vittolino.main.import.cena.elemento.texto 
+# Link do goku black: https://tse3.mm.bing.net/th?id=OIP.qvqwa_VKXUG0GWutYZGaOAHaGH&pid=15.1&P=0&w=208&h=172
