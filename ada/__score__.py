c8d2a1ae77f4a994bfab01f166812e5ad1e50ffc
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-25 20:27:53 -0300

    Saved ada/__score__.py at Timestamp: 2018-05-25 20:27:51

--- ada/__score__.py
+++ ada/__score__.py
@@ -5,3 +5,4 @@
     {'doc_id': '6e88910a', 'carta': 'carta', 'casa': 'casa', 'move': 'START', 'ponto': 0, 'valor': 1, '_level': 2, 'tempo': 1527289705054},
     {'doc_id': '75cc5862', 'carta': 'carta', 'casa': 'casa', 'move': 'START', 'ponto': 0, 'valor': 1, '_level': 2, 'tempo': 1527290209090},
     {'doc_id': 'e026a688', 'carta': 'carta', 'casa': 'casa', 'move': 'START', 'ponto': 0, 'valor': 1, '_level': 2, 'tempo': 1527290585272},
+    {'doc_id': '5a6a9f59', 'carta': 'carta', 'casa': 'casa', 'move': 'START', 'ponto': 0, 'valor': 1, '_level': 2, 'tempo': 1527290870252},
