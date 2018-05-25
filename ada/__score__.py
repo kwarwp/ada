9ce74663554f5445f16655e7d6b9ffed8bc668a4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-25 20:08:40 -0300

    Saved ada/__score__.py at Timestamp: 2018-05-25 20:08:37

--- ada/__score__.py
+++ ada/__score__.py
@@ -2,3 +2,4 @@
 "e0cb3ba5": {"author_nick": "carlo", "author_name": "carlo emmanoel", "author_email": "carlo@ufrj.br", "author_org": "LABASE", "author_site": "labase.selfip.org", "author_public": "on"},[
 ]
 "8a42c924": {"author_nick": "carlo", "author_name": "carlo emmanoel", "author_email": "carlo@ufrj.br", "author_org": "LABASE", "author_site": "labase.selfip.org", "author_public": "on"},[
+    {'doc_id': '6e88910a', 'carta': 'carta', 'casa': 'casa', 'move': 'START', 'ponto': 0, 'valor': 1, '_level': 2, 'tempo': 1527289705054},
