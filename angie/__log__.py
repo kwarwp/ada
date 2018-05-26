e29e3be560f48d68dcddf395a5576068385342e3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-26 02:56:12 -0300

    Saved angie/__log__.py at Timestamp: 2018-05-26 02:56:10

---
+++ angie/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Sat May 26 2018 02:56:10.472 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 5
+  Cena(OCEANO)vai()
+               ^
+SyntaxError: invalid syntax
+'''},
