3ad55ec3e4576ede832bb94d724acaa02414255b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-26 10:04:16 -0300

    Saved kathryn/__log__.py at Timestamp: 2018-05-26 10:04:14

--- kathryn/__log__.py
+++ kathryn/__log__.py
@@ -6,3 +6,10 @@
      ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Sat May 26 2018 10:04:14.446 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 2
+  x=
+     ^
+SyntaxError: invalid syntax
+'''},
