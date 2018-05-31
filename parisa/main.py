d96279abbbc1aeb9955e91b96e6d8da78981480a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-31 13:56:41 -0300

    Created parisa/main.py at Timestamp: 2018-05-31 13:56:39

---
+++ parisa/main.py
@@ -0,0 +1,11 @@
+# ada.parisa.main.py
+from browser import doc, html
+#doc["glowscript"] <= html.H1('XXXXXXXXX -------------- XXXXXXXXXXX')
+from _spy.vpython.main import *
+_gs=Glow('pydev')
+scene=canvas()
+scene.background= color.blue #"rgba(0, 0, 200, 0.5)"
+scene.width = 600
+scene.height = 600
+#box(color=color.red)
+ 
