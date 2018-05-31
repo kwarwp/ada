5caf502790780a663ed2d9079ea2cba3889a380f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-31 13:39:47 -0300

    Saved parisa/__log__.py at Timestamp: 2018-05-31 13:39:45

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -69,3 +69,21 @@ from vpython.main import *
     from vpython.main import Glow, canvas, box
 ImportError: cannot import name 'Glow'
 '''},
+{'date': 'Thu May 31 2018 13:39:45.71 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 188
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 360
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 332
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 349
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 2
+    from _spy.vpython.main import Glow, canvas, box
+  module _spy.vpython.main line 3
+    GLOWSCRIPT = window.glowscript
+AttributeError: no attribute glowscript for [object Window]
+'''},
