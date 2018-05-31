764b2e6f9485e8fdfe2784712f4d353bbccecefc
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-31 13:05:40 -0300

    Saved parisa/__log__.py at Timestamp: 2018-05-31 13:05:38

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -31,3 +31,19 @@ NameError: name 'Glow' is not defined
     _gs=Glow('pydiv')
 NameError: name 'Glow' is not defined
 '''},
+{'date': 'Thu May 31 2018 13:05:37.960 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 188
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 360
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 332
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 349
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 6
+    _gs=Glow('pydiv')
+NameError: name 'Glow' is not defined
+'''},
