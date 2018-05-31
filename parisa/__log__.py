8a3ef054d7121cbeddc64d7ff7bbc7426429ef10
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-31 13:38:45 -0300

    Saved parisa/__log__.py at Timestamp: 2018-05-31 13:38:43

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -53,3 +53,19 @@ Exception: Cannot read property '__class__' of null
   module '$exec_466' line 2
 from vpython.main import *
 '''},
+{'date': 'Thu May 31 2018 13:38:43.155 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 188
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 360
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 332
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 349
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 2
+    from vpython.main import Glow, canvas, box
+ImportError: cannot import name 'Glow'
+'''},
