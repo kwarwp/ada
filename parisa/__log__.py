3706e9b8d16e1f6fdb54693888e7c5bf875ec5d4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-01 06:14:39 -0300

    Saved parisa/__log__.py at Timestamp: 2018-06-01 06:14:37

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -379,3 +379,21 @@ IndexError: list index out of range
     sup = compound(pts, pos=(-1,0,0))
 TypeError: __init__() got an unexpected keyword argument 'pos'
 '''},
+{'date': 'Fri Jun 01 2018 06:14:36.969 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 171
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 188
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 360
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 332
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 349
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 6
+    _gs=Glow('glowscript')
+  module _spy.vpython.main line 17
+    self._id = document.get(id=container)[0]
+IndexError: list index out of range
+'''},
