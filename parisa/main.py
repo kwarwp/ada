bed3fa87d0dcc0ac30a81951f2e5f7e6c19b7d57
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-01 07:48:19 -0300

    Saved parisa/main.py at Timestamp: 2018-06-01 07:48:17

--- parisa/main.py
+++ parisa/main.py
@@ -3,7 +3,7 @@ from browser import doc, html
 #doc["glowscript"] <= html.H1('XXXXXXXXX -------------- XXXXXXXXXXX')
 from _spy.vpython.main import *
 doc['pydiv'].html=''
-_gs=Glow('glowscript')
+_gs=Glow('pydiv')
 scene=canvas()
 scene.background= color.blue #"rgba(0, 0, 200, 0.5)"
 scene.width = 600
