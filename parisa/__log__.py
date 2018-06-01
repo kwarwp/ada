636ab6be896027ed70b54356b525049d0f125b74
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-01 08:59:21 -0300

    Saved parisa/__log__.py at Timestamp: 2018-06-01 08:59:19

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -547,3 +547,19 @@ HTTPError: file not found
     raise error. HTTPError('file not found')
 HTTPError: file not found
 '''},
+{'date': 'Fri Jun 01 2018 08:59:18.994 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 188
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 360
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 332
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 349
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 38
+    rate(10)
+TypeError: rate() missing 1 positional argument: ['func']
+'''},
