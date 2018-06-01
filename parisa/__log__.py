8a9fad02b7a0cf16409367d305103367dc7bdb1e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-01 05:58:55 -0300

    Saved parisa/__log__.py at Timestamp: 2018-06-01 05:58:53

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -309,3 +309,21 @@ KeyError: pydev
     self._id = document.get(id=container)[0]
 IndexError: list index out of range
 '''},
+{'date': 'Fri Jun 01 2018 05:58:52.205 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 171
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 188
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 360
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 332
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 349
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 5
+    _gs=Glow('glowscript')
+  module _spy.vpython.main line 17
+    self._id = document.get(id=container)[0]
+IndexError: list index out of range
+'''},
