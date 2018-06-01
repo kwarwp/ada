825ea45732f6bb83634df76083ed7d57adc89e46
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-01 05:19:28 -0300

    Saved parisa/__log__.py at Timestamp: 2018-06-01 05:19:27

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -149,3 +149,21 @@ Exception: Cannot read property '__class__' of null
   module '$exec_615' line 4
 from _spy.vpython.main import *
 '''},
+{'date': 'Fri Jun 01 2018 05:19:26.152 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 171
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 188
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 360
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 332
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 349
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 4
+    from _spy.vpython.main import *
+  module _spy.vpython.main line 34
+    color = window.glowscript.color
+AttributeError: no attribute glowscript for [object Window]
+'''},
