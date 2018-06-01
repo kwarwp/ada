017d4dcfa7045927e996c2c3af67aed117f92236
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-01 09:50:40 -0300

    Saved parisa/main.py at Timestamp: 2018-06-01 09:50:38

--- parisa/main.py
+++ parisa/main.py
@@ -33,9 +33,10 @@ phs = [sphere(pos=(x/2+2.2, 0.15, 0.9), size=(0.3,0.3,0.3), **silv) for x in ran
 php = [sphere(pos=(x/2+2.2, 0.15, -0.9), size=(0.3,0.3,0.3), **silv) for x in range(5)]
 pts = [h1, h2, aft, fr, body, caft, cfor, cbody]+per+php+phs
 sup = compound(pts, pos=(-1,0,0))
-def sail(*_):
-    sup.pos = vec(-1+x,0,0)
 
+
+def sail(x):
+    sup.pos = vec(-1+x,0,0)
+    
 for x in range(10):
-    rate(sail, 10)
-    # sup.pos = vec(-1+x,0,0)
+    rate(0.1, lambda *_: sail(x))
