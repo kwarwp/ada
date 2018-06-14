37e4ac590fa01519b3b13804e90ab49fa400f6f0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-14 11:24:01 -0300

    Saved danae/main.py at Timestamp: 2018-06-14 14:24:01

--- danae/main.py
+++ danae/main.py
@@ -1,1 +1,11 @@
-# ada.danae.main.py
+from _spy.vitollino import Cena, Texto, Elemento, STYLE
+
+STYLE['width'] = 1000
+STYLE['height'] = "800px"
+
+entrada = "https://media-cdn.tripadvisor.com/media/photo-s/07/94/9a/9a/primeiro-portao-instalado.jpg"
+pixacao = "https://interartive.org/wp-content/uploads/pixo1.jpg"
+lagodastartarugas = "https://image.slidesharecdn.com/jardimbotnico-rj-121110093756-phpapp02/95/jardim-botnico-rj-27-638.jpg?cb=1352540459"
+
+def qualquernome()
+cena1 = Cena (img = entrada)
