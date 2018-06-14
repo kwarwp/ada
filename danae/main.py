f80fc2eaaad6aa1ab850da0140601986dd9f6688
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-14 12:16:19 -0300

    Saved danae/main.py at Timestamp: 2018-06-14 15:16:19

--- danae/main.py
+++ danae/main.py
@@ -1,11 +1,23 @@
-from _spy.vitollino import Cena, Texto, Elemento, STYLE
+from _spy.vitollino.main import Cena, Texto, Elemento, STYLE
 
 STYLE['width'] = 1000
-STYLE['height'] = "800px"
+STYLE['height'] = "600px"
 
 entrada = "https://media-cdn.tripadvisor.com/media/photo-s/07/94/9a/9a/primeiro-portao-instalado.jpg"
 pixacao = "https://interartive.org/wp-content/uploads/pixo1.jpg"
 lagodastartarugas = "https://image.slidesharecdn.com/jardimbotnico-rj-121110093756-phpapp02/95/jardim-botnico-rj-27-638.jpg?cb=1352540459"
+portao = "https://activufrj.nce.ufrj.br/file/pedropeclat/1528988110963.png?disp=inline"
+def qualquernome():
 
-def qualquernome()
-cena1 = Cena (img = entrada)
+    cena1 = Cena(img = entrada)
+    elemento1 = Elemento(img = pixacao, style = dict(left= 100, top=90,height= '200px', width= 115,bottom=20,))
+    elemento2 = Elemento(img = portao, style = dict(left= 215, top=0,height= '600px', width= 575,bottom=0))
+    lago = Cena(img = lagodastartarugas)
+    elemento1.entra(cena1)
+    elemento2.entra(cena1)
+    txtcena1 = Texto(cena1, "O portao esta trancado")
+    elemento2.vai = txtcena1.vai
+    elemento1.vai=lago.vai
+    cena1.vai()
+
+qualquernome()
