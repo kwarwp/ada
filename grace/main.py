e91ca679941c0e289b99e60ef0620fc56dab28b2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-18 19:24:02 -0300

    Saved grace/main.py at Timestamp: 2018-06-18 22:24:02

--- grace/main.py
+++ grace/main.py
@@ -1,11 +1,43 @@
-from _spy.vitollino.main import cena, elemento, texto
-
-linkcena = "https://tse1.mm.bing.net/th?id=OIP.gq6rYCRLClsXkh8VyDBOIwHaEK&pid=15.1&P=0&w=326&h=184"
-
-def historiaEmocao ():
-  cena1 = cena(img=linkcena)
-  personagem = elemento()
-  textoPersonagens = texto()
-  cena1.vai()
-
-historiaEmocao()
+from random import randint
+from time import sleep
+itens = ('Pedra', 'Papel', 'Tesoura')
+comp = randint(0,2)
+jog = int(input('''\033[;32;mSuas opções:
+\033[;36;m[ 0 ] PEDRA
+[ 1 ] PAPEL
+[ 2 ] TESOURA
+\033[;32;mQual é a sua jogada? '''))
+print('\033[;31;mJO')
+sleep(1)
+print('\033[;31;mKEN')
+sleep(1)
+print('\033[;31;mPO!!!\033[m')
+print(11*'\033[;35;m-=','\n\033[;32;mComputador jogou {}\nJogador jogou {}'.format(itens[comp], itens[jog]))
+print('\033[;35;m-=\033[m'*11)
+if comp == 0:
+    if jog == 0:
+        print('EMPATE')
+    elif jog == 1:
+        print('Jogador Ganhou')
+    elif jog == 2:
+        print('Computado Ganhou')
+    else:
+        print('JOGADA INVALIDA')
+if comp == 1:
+    if jog == 0:
+        print('Computador Ganhou')
+    elif jog == 1:
+        print('EMPATE')
+    elif jog == 2:
+        print('Jogador Ganhou')
+    else:
+        print('JOGADA INVALIDA')
+if comp == 2:
+    if jog == 0:
+        print('Jogador Ganhou')
+    elif jog == 1:
+        print('Computador Ganhou')
+    elif jog == 2:
+        print('EMPATE')
+    else:
+        print('JOGADA INVALIDA')
