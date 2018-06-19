162b3c5a009d236b201d5cf89487d2ef959666dc
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-19 09:58:01 -0300

    Saved kristen/main.py at Timestamp: 2018-06-19 12:58:00

--- kristen/main.py
+++ kristen/main.py
@@ -1,1 +1,7 @@
-# ada.kristen.main.py
+from _spy.vitollino.main import Cena, Texto, Elemento, STYLE
+
+
+###########################################TAKE2############################################################
+
+STYLE['width'] = 1000
+STYLE['height'] = "600px"
