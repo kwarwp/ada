22f66d12f6f8098f53490c4aa3f6d516a40cccda
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-04 10:09:49 -0300

    Saved amanda/main.py at Timestamp: 2018-07-04 13:09:49

--- amanda/main.py
+++ amanda/main.py
@@ -13,9 +13,9 @@ class CenaTutorialInterativo():
     
     self.cena_t = Cena(img = TutorialInterativo)
     
-    self.cena_d = Cena(Cena_direita, direita=self.cena_t)
+    self.cena_d = Cena(Cena_direita, esquerda=self.cena_t)
 
-    self.cena_e  = Cena(Cena_esquerda, esquerda=self.cena_t)
+    self.cena_e  = Cena(Cena_esquerda, direita=self.cena_t)
 
     
     #self.cena_t = Cena(img = TutorialInterativo)
