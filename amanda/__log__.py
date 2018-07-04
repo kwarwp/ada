68361aa70bb28f2841e24bdcee4dd55e1821768c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-04 13:01:46 -0300

    Saved amanda/__log__.py at Timestamp: 2018-07-04 16:01:46

--- amanda/__log__.py
+++ amanda/__log__.py
@@ -35,3 +35,23 @@ AttributeError: 'CenaTutorialInterativo' object has no attribute 'cena_o'
     self.cena_t.vai()
 AttributeError: 'CenaTutorialInterativo' object has no attribute 'cena_t'
 '''},
+{'date': 'Wed Jul 04 2018 13:01:29.474 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 48
+    CenaTutorialInterativo().vai()
+  module <module> line 22
+    cti.vai()
+  module <module> line 22
+    cti.vai()
+UnboundLocalError: local variable 'cti' referenced before assignment
+'''},
