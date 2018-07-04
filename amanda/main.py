744b1548283e814ceea7a1d3ce8b2bbd64d38c22
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-04 10:38:12 -0300

    Saved amanda/main.py at Timestamp: 2018-07-04 13:38:12

--- amanda/main.py
+++ amanda/main.py
@@ -8,6 +8,11 @@ TutorialInterativo = "https://i.imgur.com/tfatd8y.png"
 Cena_direita = "https://i.imgur.com/yWH5AGZ.png"
 Cena_esquerda = "https://i.imgur.com/AegnKJL.png"
 comousarelemento = "https://i.imgur.com/amn4taQ.png"
+
+#cretaceo_l = "https://i.imgur.com/O3o9Oqx.jpg"
+#cretaceo_s = "https://i.imgur.com/c9lK8Vm.jpg"
+#cretaceo_o = "https://i.imgur.com/QFLlccY.jpg"
+#cretaceo_n = "https://i.imgur.com/k1LChZw.jpg"
 class CenaTutorialInterativo():
   def __init__(self):
     
@@ -27,43 +32,36 @@ class CenaTutorialInterativo():
     self.cena_t.vai()
     
     
-  def vai(self, *_):
-    self.cena_t.vai()
+#  def vai(self, *_):
+#    self.cena_t.vai()
     
     
     
-STYLE['width'] = 700
-
-cretaceo_l = "https://i.imgur.com/O3o9Oqx.jpg"
-cretaceo_s = "https://i.imgur.com/c9lK8Vm.jpg"
-cretaceo_o = "https://i.imgur.com/QFLlccY.jpg"
-cretaceo_n = "https://i.imgur.com/k1LChZw.jpg"
-
-class CenaCretaceo():
-  def __init__(self):
-    self.cena_n = Cena(img = cretaceo_n)
-    self.cena_o = Cena(cretaceo_o, direita=self.cena_n)
-    self.cena_s  = Cena(cretaceo_s, direita=self.cena_o)
-    self.cena_l  = Cena(cretaceo_l, direita=self.cena_s,
-    esquerda = self.cena_n)
-    self.cena_n.esquerda = self.cena_o
-    self.cena_n.direita = self.cena_l
-    self.cena_o.esquerda = self.cena_s
-    self.cena_s.esquerda = self.cena_l
-    self.cena_s.meio = Cena(vai=self.vai_sul)
+#class CenaCretaceo():
+#  def __init__(self):
+#    self.cena_n = Cena(img = cretaceo_n)
+#    self.cena_o = Cena(cretaceo_o, direita=self.cena_n)
+#    self.cena_s  = Cena(cretaceo_s, direita=self.cena_o)
+#    self.cena_l  = Cena(cretaceo_l, direita=self.cena_s,
+#    esquerda = self.cena_n)
+#    self.cena_n.esquerda = self.cena_o
+#    self.cena_n.direita = self.cena_l
+#    self.cena_o.esquerda = self.cena_s
+#    self.cena_s.esquerda = self.cena_l
+#    self.cena_s.meio = Cena(vai=self.vai_sul)
     #cena2 = Cena2()
     #self.cena1.direita = cena2
 
-    self.cena_n.vai()
-  def vai(self, *_):
-    self.cena_o.vai()
-  def vai_sul(self, *_):
-    from anastasia.main import go_anhanguera
-    go_anhanguera()
+#    self.cena_n.vai()
+#  def vai(self, *_):
+#    self.cena_o.vai()
+#  def vai_sul(self, *_):
+#    from anastasia.main import go_anhanguera
+#    go_anhanguera()
 
-def go_cretaceo():
-  cena_cretaceo = CenaCretaceo()
-  cena_cretaceo.vai()
+#def go_cretaceo():
+#  cena_cretaceo = CenaCretaceo()
+#  cena_cretaceo.vai()
 
-if __name__ == "__main__":
-	go_cretaceo()
+#if __name__ == "__main__":
+#	go_cretaceo()
