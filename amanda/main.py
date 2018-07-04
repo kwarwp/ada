fd098dbc06431cab6bd6c3e614c944bd86882ff5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-04 11:17:27 -0300

    Saved amanda/main.py at Timestamp: 2018-07-04 14:17:27

--- amanda/main.py
+++ amanda/main.py
@@ -35,36 +35,36 @@ class CenaTutorialInterativo():
 if __name__ == "__main__":
     CenaTutorialInterativo()
     
-#  def vai(self, *_):
-#    self.cena_t.vai()
+  def vai(self, *_):
+    self.cena_t.vai()
     
     
     
-#class CenaCretaceo():
-#  def __init__(self):
-#    self.cena_n = Cena(img = cretaceo_n)
-#    self.cena_o = Cena(cretaceo_o, direita=self.cena_n)
-#    self.cena_s  = Cena(cretaceo_s, direita=self.cena_o)
-#    self.cena_l  = Cena(cretaceo_l, direita=self.cena_s,
-#    esquerda = self.cena_n)
-#    self.cena_n.esquerda = self.cena_o
-#    self.cena_n.direita = self.cena_l
-#    self.cena_o.esquerda = self.cena_s
-#    self.cena_s.esquerda = self.cena_l
-#    self.cena_s.meio = Cena(vai=self.vai_sul)
+class CenaCretaceo():
+  def __init__(self):
+    self.cena_n = Cena(img = cretaceo_n)
+    self.cena_o = Cena(cretaceo_o, direita=self.cena_n)
+    self.cena_s  = Cena(cretaceo_s, direita=self.cena_o)
+    self.cena_l  = Cena(cretaceo_l, direita=self.cena_s,
+    esquerda = self.cena_n)
+    self.cena_n.esquerda = self.cena_o
+    self.cena_n.direita = self.cena_l
+    self.cena_o.esquerda = self.cena_s
+    self.cena_s.esquerda = self.cena_l
+    self.cena_s.meio = Cena(vai=self.vai_sul)
     #cena2 = Cena2()
     #self.cena1.direita = cena2
 
-#    self.cena_n.vai()
-#  def vai(self, *_):
-#    self.cena_o.vai()
-#  def vai_sul(self, *_):
-#    from anastasia.main import go_anhanguera
-#    go_anhanguera()
+    self.cena_n.vai()
+  def vai(self, *_):
+    self.cena_o.vai()
+  def vai_sul(self, *_):
+    from anastasia.main import go_anhanguera
+    go_anhanguera()
 
-#def go_cretaceo():
-#  cena_cretaceo = CenaCretaceo()
-#  cena_cretaceo.vai()
+def go_cretaceo():
+  cena_cretaceo = CenaCretaceo()
+  cena_cretaceo.vai()
 
-#if __name__ == "__main__":
-#	go_cretaceo()
+if __name__ == "__main__":
+	go_cretaceo()
