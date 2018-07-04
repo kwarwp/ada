56135e32a4af808731273cbcded5ef968fb9adad
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-04 12:59:35 -0300

    Saved amanda/__log__.py at Timestamp: 2018-07-04 15:59:35

--- amanda/__log__.py
+++ amanda/__log__.py
@@ -17,3 +17,21 @@
     self.cena_t.meio = Cena(vai=self.cena_o)
 AttributeError: 'CenaTutorialInterativo' object has no attribute 'cena_o'
 '''},
+{'date': 'Wed Jul 04 2018 12:59:18.314 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 48
+    CenaTutorialInterativo().vai()
+  module <module> line 22
+    self.cena_t.vai()
+AttributeError: 'CenaTutorialInterativo' object has no attribute 'cena_t'
+'''},
