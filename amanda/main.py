f1bea3ac01aaa7feb318eb7f73418659e180743a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-04 10:07:10 -0300

    Saved amanda/main.py at Timestamp: 2018-07-04 13:07:10

--- amanda/main.py
+++ amanda/main.py
@@ -1,1 +1,47 @@
 # ada.amanda.main.py
+# henrietta.adda.main.py
+from _spy.vitollino.main import Cena, STYLE
+
+STYLE['width'] = 699
+
+TutorialInterativo = "https://i.imgur.com/tfatd8y.png"
+Cena_direita = "https://imgur.com/yWH5AGZ"
+Cena_esquerda = "https://imgur.com/AegnKJL"
+comousarelemento = "https://i.imgur.com/amn4taQ.png"
+class CenaTutorialInterativo():
+  def __init__(self):
+    
+    self.cena_t = Cena(img = TutorialInterativo)
+    
+    self.cena_d = Cena(Cena_direita, direita=self.cena_t)
+
+    self.cena_e  = Cena(Cena_esquerda, esquerda=self.cena_t)
+
+    
+    #self.cena_t = Cena(img = TutorialInterativo)
+    
+    self.cena_t.direita = self.cena_d
+    self.cena_t.esquerda = self.cena_e
+    
+    self.cena_t.meio = Cena(vai=self.vai_importar)
+    self.cena_t.vai()
+    
+    
+  def vai(self, *_):
+    self.cena_t.vai()
+    
+    
+    
+  def vai_importar(self, *_):
+    from kristen.main import go_cretaceo
+    go_cretaceo()
+    
+def vai_CenaTutorialInterativo():
+  cenaImporta = CenaTutorialInterativo()
+  cenaImporta.vai()
+ 
+if __name__ == "__main__":
+	vai_CenaTutorialInterativo()
+    
+     
+ 
