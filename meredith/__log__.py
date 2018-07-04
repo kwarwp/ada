ded462c8ab39feca50f3cdc8eaf4c91a80dd51eb
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-04 13:05:37 -0300

    Created meredith/__log__.py at Timestamp: 2018-07-04 16:05:37

---
+++ meredith/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Wed Jul 04 2018 13:05:19.121 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 21
+  self.cena_t = Cena(img = cretaceo_l, volta_tut, volta_tut, volta_tut)
+                                                ^
+SyntaxError: non-keyword arg after keyword arg
+'''},
