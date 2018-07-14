fc6de35942864e5e992d310e8ac355214cc91b68
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-14 17:43:08 -0300

    Saved roxanne/main.py at Timestamp: 2018-07-14 20:43:08

--- roxanne/main.py
+++ roxanne/main.py
@@ -29,7 +29,7 @@ class Planta(Cena, Droppable):
         Droppable.__init__(self, self.divesq, "regador", self.regou)
 
     def regou(self, *_):
-        alert("VocÃª regou a planta")
+        alert("VocÃÂª regou a planta")
         
 class Regador(Elemento, Dragger):
 
@@ -181,7 +181,7 @@ class Bloco:
         self.pecas_colocadas += valor_peca
         if len(self.pecas_colocadas) == 4:
             if all(self.pecas_colocadas):
-                input("O texto estÃ¡ certo.")
+                input("O texto estÃÂ¡ certo.")
             else:
                 vai = input("Tentar de novo?")
                 if vai == "s":
