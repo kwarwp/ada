4bfecd8dbd1519c2a375310f9a563ccaa30ab4a1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-31 16:56:08 -0300

    Saved grace/__log__.py at Timestamp: 2018-07-31 19:56:08

--- grace/__log__.py
+++ grace/__log__.py
@@ -34,3 +34,22 @@ Traceback (most recent call last):
     raise NotImplementedError("Blocking functions like time.sleep() are not "
 NotImplementedError: Blocking functions like time.sleep() are not supported in the browser. Use functions in module browser.timer instead.
 '''},
+{'date': 'Tue Jul 31 2018 16:52:33.898 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''[;31;mJO
+Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 11
+    sleep(1)
+  module time line 223
+    raise NotImplementedError("Blocking functions like time.sleep() are not "
+NotImplementedError: Blocking functions like time.sleep() are not supported in the browser. Use functions in module browser.timer instead.
+'''},
