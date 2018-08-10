1741834fa15091dc82f77480aad0001faf152232
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-10 13:48:04 -0300

    Saved meredith/main.py at Timestamp: 2018-08-10 16:48:04

--- meredith/main.py
+++ meredith/main.py
@@ -1,5 +1,5 @@
 # ada.meredith.main.py
-from _spy.vitollino.main import Cena, STYLE
+from _spy.vitollino.main import Cena, STYLE, Codigo
 
 STYLE['width'] = 699
 
@@ -7,6 +7,16 @@ cretaceo_l = "https://i.imgur.com/O3o9Oqx.jpg"
 cretaceo_s = "https://i.imgur.com/c9lK8Vm.jpg"
 cretaceo_o = "https://i.imgur.com/QFLlccY.jpg"
 cretaceo_n = "https://i.imgur.com/k1LChZw.jpg"
+TOPO ="""
+uma linha <br>
+outra <br>
+<em> ital </em>
+"""
+COD ="""
+def x(a=0):
+  if True:
+    print(a)
+"""
 
 class CenaTutorialInterativo():
     CENA_TUTORIAL = None
@@ -19,12 +29,14 @@ class CenaTutorialInterativo():
                 cti().vai()
         volta_tut = Cena(vai=_vai_tutorial)
         self.cena_t = Cena(cretaceo_l, volta_tut, volta_tut, volta_tut)
+        codigo = Codigo(cena= self.cena_t, topo=TOPO, codigo=COD, style=dict(left=300))
         return self
     def vai(self):
         cti = CenaTutorialInterativo.CENA_TUTORIAL
         if cti is None:
             cti =  CenaTutorialInterativo.CENA_TUTORIAL = CenaTutorialInterativo().__cria()
         cti.cena_t.vai()
+        
         return cti
 
     
