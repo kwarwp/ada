59f2ae4f00792e995d62009f1b409425a6cc66de
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-30 14:28:00 -0300

    Saved kristen/__log__.py at Timestamp: 2018-08-30 17:28:00

--- kristen/__log__.py
+++ kristen/__log__.py
@@ -164,3 +164,31 @@ Traceback (most recent call last):
     class Elemento(Element):
 NameError: name 'Element' is not defined
 '''},
+{'date': 'Thu Aug 30 2018 14:27:37.24 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 170
+    teste()
+  module <module> line 167
+    figura = Elemento(ABELHA, x=150, y=50, w=100, h=100, cena = fundo)
+  module <module> line 14
+    self._auto_score = self.score if score else self._auto_score
+AttributeError: 'Elemento' object has no attribute '_auto_score'
+'''},
