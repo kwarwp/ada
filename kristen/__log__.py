790fc81710023f754072dc15d007026c4f761ec5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-30 14:25:14 -0300

    Created kristen/__log__.py at Timestamp: 2018-08-30 17:25:14

---
+++ kristen/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Thu Aug 30 2018 14:24:50.247 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 164
+  figura = Element(ABELHA, style = dict(top = 100, left = 50, height = 50, width = 50) cena = fundo)
+                                                                                        ^
+SyntaxError: invalid syntax
+'''},
