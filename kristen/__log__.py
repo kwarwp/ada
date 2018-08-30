7c7dc0b395c5b33f27b42473bdb8a1a460fa4bc4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-30 14:29:09 -0300

    Saved kristen/__log__.py at Timestamp: 2018-08-30 17:29:09

--- kristen/__log__.py
+++ kristen/__log__.py
@@ -276,3 +276,31 @@ NameError: name 'html' is not defined
     self.elt = html.DIV(Id=tit + drop, title=tit, style=self.style)
 NameError: name 'html' is not defined
 '''},
+{'date': 'Thu Aug 30 2018 14:28:45.742 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 172
+    teste()
+  module <module> line 169
+    figura = Elemento(ABELHA, x=150, y=50, w=100, h=100, cena = fundo)
+  module <module> line 43
+    self.elt.onclick = self._click
+AttributeError: 'Elemento' object has no attribute '_click'
+'''},
