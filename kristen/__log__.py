f277d5ceedec58b5e7829a0eb0036443d5a11801
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-30 16:34:42 -0300

    Saved kristen/__log__.py at Timestamp: 2018-08-30 19:34:42

--- kristen/__log__.py
+++ kristen/__log__.py
@@ -1713,3 +1713,10 @@ SyntaxError: invalid syntax
                                                                                ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Thu Aug 30 2018 16:34:19.413 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 228
+  backg = Elemento(BACKG, drag= False, x=100, y=100
+                                                             ^
+SyntaxError: invalid syntax
+'''},
