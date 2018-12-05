b12d3cdebd5a651e851c945f027a8ec7a5066c94
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-12-05 18:47:53 -0200

    Created julia/__log__.py at Timestamp: 2018-12-05 20:47:53

---
+++ julia/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Wed Dec 05 2018 18:47:51.238 GMt-0200 (Horário de Verão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 2
+  print "Estou aqui";
+        ^
+SyntaxError: missing parenthesis in call to 'print'
+'''},
