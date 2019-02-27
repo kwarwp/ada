04bd5bdb8c4bd87867c24ec4de6178890b6d55c7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-02-27 16:19:36 -0300

    Saved adda/main.py at Timestamp: 2019-02-27 19:19:36

--- adda/main.py
+++ adda/main.py
@@ -1,1 +1,10 @@
 # ada.adda.main.py
+from _spy.vitollino.main import Cena, Elemento, STYLE
+STYLE ["width"] = 960
+STYLE ["height"] = "600px" 
+wall = "https://i.imgur.com/y2Cmt0D.png"
+floor = "https://i.imgur.com/hQGOZGZ.png"
+cena = Cena(wall)
+elemento = Elemento(floor,style=dict(width="920px"))
+elemento.entra(cena)
+cena.vai()
