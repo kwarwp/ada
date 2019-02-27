fee8a7f88adadfb3dc27ed77940cb78f76e46bc1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-02-27 16:27:53 -0300

    Saved adda/main.py at Timestamp: 2019-02-27 19:27:51

--- adda/main.py
+++ adda/main.py
@@ -4,7 +4,8 @@ STYLE ["width"] = 960
 STYLE ["height"] = "600px" 
 wall = "https://i.imgur.com/y2Cmt0D.png"
 floor = "https://i.imgur.com/hQGOZGZ.png"
+window = "https://i.imgur.com/O1pEa77.png"
 cena = Cena(wall)
-elemento = Elemento(floor,style=dict(width="960px",height="500px"))
+elemento = Elemento(floor,style=dict(width="960px",height="900px"))
 elemento.entra(cena)
 cena.vai()
