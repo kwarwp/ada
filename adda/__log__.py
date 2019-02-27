6881a2306c1187c2bec6c97a7371e653e9fd5ebc
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-02-27 16:14:31 -0300

    Created adda/__log__.py at Timestamp: 2019-02-27 19:14:31

---
+++ adda/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Wed Feb 27 2019 16:14:29.116 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 4
+  STYLE ["height"] = 
+                      ^
+SyntaxError: invalid syntax
+'''},
