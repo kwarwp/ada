f801b171eb52aca16bf24c4b1caaeec7f988afaa
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-02-27 17:08:21 -0300

    Saved adda/main.py at Timestamp: 2019-02-27 20:08:21

--- adda/main.py
+++ adda/main.py
@@ -4,8 +4,19 @@ STYLE ["width"] = 960
 STYLE ["height"] = "600px" 
 wall = "https://i.imgur.com/y2Cmt0D.png"
 floor = "https://i.imgur.com/hQGOZGZ.png"
-window = "https://i.imgur.com/O1pEa77.png"
+window_= "https://i.imgur.com/O1pEa77.png"
+window_2 = "https://i.imgur.com/996rAAB.png"
+torch_= "https://i.imgur.com/lnuZO60.gif"
+torch_2 = "https://i.imgur.com/lnuZO60.gif"
 cena = Cena(wall)
 elemento = Elemento(floor,style=dict(width="960px",height="900px"))
+window = Elemento(window_,style=dict(left=190,top=-50,width="420px",height="600px"))
+window2 = Elemento(window_2,style=dict(left=370,top=-50,width="420px",height="600px"))
+torch = Elemento(torch_,style=dict(left=-55,top=-5,width="500px",height="400px"))
+torch_2 = Elemento(torch_2,style=dict(left=540,top=-4,width="500px",height="400px"))
 elemento.entra(cena)
+window.entra(cena)
+window2.entra(cena)
+torch.entra(cena)
+torch_2.entra(cena)
 cena.vai()
