162ef873755658f2a3444c0b65dd2c4e144843bb
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-01 18:30:58 -0300

    Created heather/__log__.py at Timestamp: 2019-03-01 21:30:58

---
+++ heather/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Fri Mar 01 2019 18:30:56.580 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 111
+  return [self.desenha_o_corpo, self.,cauda_superior self.cauda_inferior]
+                                      ^
+SyntaxError: invalid syntax
+'''},
