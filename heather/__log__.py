d4e15322e637627e0385eb658c84667c1a592439
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-01 18:40:47 -0300

    Saved heather/__log__.py at Timestamp: 2019-03-01 21:40:47

--- heather/__log__.py
+++ heather/__log__.py
@@ -38,3 +38,27 @@ NameError: name 'sqrt' is not defined
     GRAUS_30= pi/6.0
 NameError: name 'pi' is not defined
 '''},
+{'date': 'Fri Mar 01 2019 18:40:46.287 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 154
+    cenario.principal()
+  module <module> line 98
+    peixinho=Peixe()
+  module <module> line 104
+    self.esqueleto=compound(self.desenha())
+  module <module> line 113
+    self.cauda_superior.rotate(angle=GRAUS_30, axis=EIXO_Z)
+  module _spy.vpython.primitive line 23
+    kwargs['axis'] = kwargs['axis']._vec
+AttributeError: 'tuple' object has no attribute '_vec'
+'''},
