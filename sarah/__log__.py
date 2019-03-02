b917711623d686a2af7ed0262dda81b8f9353e0a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-02 16:29:46 -0300

    Saved sarah/__log__.py at Timestamp: 2019-03-02 19:29:46

--- sarah/__log__.py
+++ sarah/__log__.py
@@ -152,3 +152,27 @@ TypeError: unhashable type: 'list'
 'error': '''
 Exception: <TypeError: obj is undefined>
 '''},
+{'date': 'Sat Mar 02 2019 16:29:44.907 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 92
+    _ = Batalha()
+  module <module> line 83
+    a0 = AgroBatalha((20, 20),[200, 50, 50])
+  module <module> line 37
+    self.procria()
+  module <module> line 56
+    self.dpr()
+  module <module> line 41
+    self.arena.paint((l, self.dp), color)
+AttributeError: 'Batalha' object has no attribute 'paint'
+'''},
