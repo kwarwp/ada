c139bc6707b6f3c4c460510a9d75085a26e0cbd1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-02 12:59:16 -0300

    Saved sarah/__log__.py at Timestamp: 2019-03-02 15:59:16

--- sarah/__log__.py
+++ sarah/__log__.py
@@ -6,3 +6,25 @@
                 ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Sat Mar 02 2019 12:59:15.297 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 14
+    _ = Batalha()
+  module <module> line 9
+    cena = Cena(ARENA)
+  module _spy.vitollino.main line 780
+    self._cria_divs(width)
+  module _spy.vitollino.main line 785
+    divesq.style.width = width // 3  # 100
+TypeError: unsupported operand type(s) for //: 'str' and 'int'
+'''},
