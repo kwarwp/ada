3b381d7b1468dfa898121720e98131e740005b94
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-02 16:13:09 -0300

    Saved sarah/__log__.py at Timestamp: 2019-03-02 19:13:09

--- sarah/__log__.py
+++ sarah/__log__.py
@@ -128,3 +128,23 @@ TypeError: Can't convert int to str implicitely
     a0 = AgroBatalha([20, 20],[200, 50, 50])
 TypeError: object() takes no parameters
 '''},
+{'date': 'Sat Mar 02 2019 16:13:08.444 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 82
+    _ = Batalha()
+  module <module> line 74
+    a0 = AgroBatalha([20, 20],[200, 50, 50])
+  module <module> line 33
+    if self.lugar not in self.colonia:
+TypeError: unhashable type: 'list'
+'''},
