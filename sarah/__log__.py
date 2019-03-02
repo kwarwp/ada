b77fdd69eea099eea05f01da5aeb75cc7cc69b1d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-02 16:11:21 -0300

    Saved sarah/__log__.py at Timestamp: 2019-03-02 19:11:21

--- sarah/__log__.py
+++ sarah/__log__.py
@@ -110,3 +110,21 @@ TypeError: Can't convert int to str implicitely
     self.ctx.fillStyle = "rgba("+r+","+g+","+b+","+(a/255)+")"
 TypeError: Can't convert int to str implicitely
 '''},
+{'date': 'Sat Mar 02 2019 16:11:20.97 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 82
+    _ = Batalha()
+  module <module> line 74
+    a0 = AgroBatalha([20, 20],[200, 50, 50])
+TypeError: object() takes no parameters
+'''},
