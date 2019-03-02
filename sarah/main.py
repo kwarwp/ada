dac5a6eb7fa00ec0c5f6aca94b3bcf3a1a25b6c1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-02 13:01:29 -0300

    Saved sarah/main.py at Timestamp: 2019-03-02 16:01:28

--- sarah/main.py
+++ sarah/main.py
@@ -1,11 +1,13 @@
 # ada.sarah.main.py
-from _spy.vitollino.main import Cena, Elemento
+from _spy.vitollino.main import Cena, Elemento, STYLE
 
 
-class Batalha
+class Batalha:
     def __init__(self):
+        STYLE["width"] = 900
         ARENA = "https://i.imgur.com/nS8Tas9.jpg"
         cena = Cena(ARENA)
+        cena.vai()
 
 
 if __name__ == "__main__":
