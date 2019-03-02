4e8ee72702db4779f3276a56c03e781283b4bb13
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-02 14:30:02 -0300

    Saved sarah/__log__.py at Timestamp: 2019-03-02 17:30:02

--- sarah/__log__.py
+++ sarah/__log__.py
@@ -70,3 +70,23 @@ Exception: <TypeError: attr.substr is not a function>
     alert(d[0],d[1],d[2], d[3], d[4])
 KeyError: <unprintable KeyError object>
 '''},
+{'date': 'Sat Mar 02 2019 14:30:01.661 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 46
+    _ = Batalha()
+  module <module> line 42
+    canvas.paint(10, 10, 200, 50, 50)
+  module <module> line 23
+    self.ctx.fillStyle = "rgba("+r+","+g+","+b+","+(a/255)+")"
+TypeError: Can't convert int to str implicitely
+'''},
