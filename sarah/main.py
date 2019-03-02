503a4329ef84d67e014f3f920d2bc93cd4cb0110
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-02 12:57:53 -0300

    Created sarah/main.py at Timestamp: 2019-03-02 15:57:53

---
+++ sarah/main.py
@@ -0,0 +1,12 @@
+# ada.sarah.main.py
+from _spy.vitollino.main import Cena, Elemento
+
+
+class Batalha
+    def __init__(self):
+        ARENA = "https://i.imgur.com/nS8Tas9.jpg"
+        cena = Cena(ARENA)
+
+
+if __name__ == "__main__":
+    _ = Batalha()
