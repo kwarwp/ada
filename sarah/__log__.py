e5657cbd4e38026caa66119362724fd531c7c6e0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-02 12:57:58 -0300

    Created sarah/__log__.py at Timestamp: 2019-03-02 15:57:58

---
+++ sarah/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Sat Mar 02 2019 12:57:56.624 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 5
+  class Batalha
+                ^
+SyntaxError: invalid syntax
+'''},
