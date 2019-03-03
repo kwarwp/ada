18ea8c31d9437a6ba511c017f77c19d19066fb8f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-03 10:56:04 -0300

    Saved julia/main.py at Timestamp: 2019-03-03 13:56:04

--- julia/main.py
+++ julia/main.py
@@ -2,6 +2,7 @@
 from _spy.vitollino.main import Cena, Elemento, STYLE
 from math import sqrt
 from random import randint
+from browser import timer
 IMGSIZE = "240px"
 
 class Sprite(Elemento):
@@ -18,9 +19,25 @@ class Button(Sprite):
         super().__init__(x, y, image, cena, index)
         self.x, self.y, self.image, self.cena, self.index = x, y, image, cena, index
         self.grav, self.ele= 10, 10
+        timer.set_timeout(self.move, 10)
+    def move(self):
+        forces = zip(*[b.force(self.x) for b in Button.BUTTONS] if b != self)
+        dx, dy = sum(forces[0]), sum(forces[1])
+        self.x += int(dx)
+        self.y += int(dy)
+        if dx < 2 or dy > 2:
+            timer.set_timeout(self.move, 10)
+            
+    def force(self, x, y):
+        dx, dy = x - self.x, y - self.y
+        distance = sqrt(dx*dx + dy*dy)
+        pull = 10 / min(0.1, distance)
+        pull = 10 / min(0.1, distance)
+        
+        
         
     def create(self):
-        Button.BUTTONS = [Button(randint(800), randint(800), self.image, self.cena, index) for index in range(9)]
+        Button.BUTTONS = [Button(randint(0, 800), randint(0, 300), self.image, self.cena, index) for index in range(9)]
         return Button.BUTTONS
 
 class Project:
@@ -30,7 +47,7 @@ class Project:
         Buttons = "https://i.imgur.com/v6JS64Y.png"
         ARENA = "https://i.imgur.com/nS8Tas9.jpg"
         cena = Cena(ARENA)
-        b0 = Button(10, 10, Buttons, cena, 0)
+        b0 = Button(10, 10, Buttons, cena, 0).create()
         #b0.entra(cena)
         cena.vai()
 
