2862851dba97c3266208ef273456fc35971ad71e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-03 10:19:16 -0300

    Saved julia/main.py at Timestamp: 2019-03-03 13:19:15

--- julia/main.py
+++ julia/main.py
@@ -1,16 +1,27 @@
 # ada.julia.main.py
 from _spy.vitollino.main import Cena, Elemento, STYLE
+from math import sqrt
+from random import randint
 IMGSIZE = "240px"
 
 class Sprite(Elemento):
-    def __init__(self, x, y, image, index=0):
-        super().__init__(image, style=dict(left=x,top=x,width="80px",height="80px",overflow="hidden"))
+    def __init__(self, x, y, image, cena, index=0):
+        super().__init__(image, cena=cena, style=dict(left=x,top=x,width="80px",height="80px",overflow="hidden"))
         style = dict(position="relative",left=f"-{index%3*80}px",top=f"-{index//3*80}px", width=IMGSIZE, height=IMGSIZE)
         style.update({"max-width": IMGSIZE, "max-height": IMGSIZE})
         self.img.style=style
         
 
-
+class Button(Sprite):
+    BUTTONS = []
+    def __init__(self, x, y, image, cena, index=0):
+        super().__init__(x, y, image, cena, index)
+        self.x, self.y, self.image, self.cena, self.index = x, y, image, cena, index
+        self.grav, self.ele= 10, 10
+        
+    def create(self):
+        Button.BUTTONS = [Button(randint(800), randint(800), self.image, self.cena, index) for index in range(9)]
+        return Button.BUTTONS
 
 class Project:
     def __init__(self):
@@ -19,8 +30,8 @@ class Project:
         Buttons = "https://i.imgur.com/v6JS64Y.png"
         ARENA = "https://i.imgur.com/nS8Tas9.jpg"
         cena = Cena(ARENA)
-        b0 = Sprite(10, 10, Buttons, 7)
-        b0.entra(cena)
+        b0 = Button(10, 10, Buttons, cena, 0)
+        #b0.entra(cena)
         cena.vai()
 
 
