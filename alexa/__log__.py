43a39a8e85b3ecf14d3479c959c696e64c25f218
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-27 20:28:27 -0300

    Saved alexa/__log__.py at Timestamp: 2019-03-27 23:28:27

--- alexa/__log__.py
+++ alexa/__log__.py
@@ -31,3 +31,19 @@ TypeError: __init__() missing 1 positional argument: um_valor
     print(a0, a1, a0 == a1, isinstance(a0, A), a0.um_valor, a1.um_valor)
 AttributeError: 'B' object has no attribute 'um_valor'
 '''},
+{'date': 'Wed Mar 27 2019 20:28:26.5 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 16
+    print(a0, a1, a0 == a1, isinstance(a0, A), a0.um_valor, a1.um_valor)
+AttributeError: 'B' object has no attribute 'um_valor'
+'''},
