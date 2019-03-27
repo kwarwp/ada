798ed782bdf226fd31dc7a70284daefc1063ffaf
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-27 20:55:33 -0300

    Saved alexa/__log__.py at Timestamp: 2019-03-27 23:55:33

--- alexa/__log__.py
+++ alexa/__log__.py
@@ -63,3 +63,22 @@ AttributeError: 'B' object has no attribute 'um_valor'
     print(a0, a1, a0 == a1, isinstance(a0, A), a0.um_valor, a1.um_valor)
 AttributeError: 'B' object has no attribute 'um_valor'
 '''},
+{'date': 'Wed Mar 27 2019 20:55:31.891 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''[<__main__.A object>, <__main__.A object>, <__main__.B object>, <__main__.B object>]
+Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 26
+    a.muda()
+  module <module> line 11
+    self.um_valor *= 2
+TypeError: unsupported operand types for *: 'function' and 'int'
+'''},
