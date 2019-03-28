8a66e643e98a5b969e7286a98b5c16ced6cffe1f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-28 15:05:29 -0300

    Created ruzwana/main.py at Timestamp: 2019-03-28 18:05:29

---
+++ ruzwana/main.py
@@ -0,0 +1,8 @@
+# ada.ruzwana.main.py
+from _spy.vpython.main import *
+doc['pydiv'].html=''
+_gs=Glow('pydiv')
+scene=canvas()
+scene.background= color.blue #"rgba(0, 0, 200, 0.5)"
+scene.width = 600
+scene.height = 600
