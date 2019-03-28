de26ea7185c242e912eec9e75b5b2efbeeef19f2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-28 11:48:19 -0300

    Saved adda/main.py at Timestamp: 2019-03-28 14:48:17

--- adda/main.py
+++ adda/main.py
@@ -2,7 +2,7 @@
 from _spy.vitollino.main import Cena, Elemento, STYLE
 from random import choice
 from browser import timer
-STYLE ["width"] = 1157
+STYLE ["width"] = 1155
 STYLE ["height"] = "600px" 
 wall = "https://i.imgur.com/OclGL7S.png"
 window_= "https://i.imgur.com/O1pEa77.png"
@@ -20,13 +20,13 @@ os_glows = [glow1,glow2,glow3,glow4,glow5]
 cena = Cena(wall)
 window = Elemento(window_,style=dict(left=0,top=-50,width="420px",height="600px",overflow="hidden"))
 window.img.style=dict(position="relative",left="-200px")
-window2 = Elemento(window_2,style=dict(left=540,top=-45,width="420px",height="590px",overflow="hidden"))
+window2 = Elemento(window_2,style=dict(left=735,top=-45,width="420px",height="590px",overflow="hidden"))
 window2.img.style=dict(position="relative",right="-230px")
-torch = Elemento(torch_,style=dict(left=-100,top=-5,width="600px",height="400px"))
-torch_2 = Elemento(torch_2,style=dict(left=540,top=-4,width="500px",height="400px"))
+torch = Elemento(torch_,style=dict(left=-66,top=-5,width="600px",height="400px"))
+torch_2 = Elemento(torch_2,style=dict(left=700,top=-4,width="500px",height="400px"))
 glow1 = Elemento(glow1,style=dict(left=-55,top=0,width="500px",height="400px",opacity=0.5))
 glow_1= Elemento(glow_1,style=dict(left=540,top=0,width="500px",height="400px",opacity=0.7))
-door = Elemento(door,style=dict(left=10,top=7,width="950px",height="612px"))
+door = Elemento(door,style=dict(left=-20,top=7,width="1200px",height="612px"))
 #dark= Elemento("",style=dict(width="960px",height="600px",backgroundColor="black",opacity=0.5))
 window.entra(cena)
 window2.entra(cena)
