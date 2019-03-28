02d98bd3f6b27c796f327f0dc60ff3229ab70776
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-28 16:19:07 -0300

    Saved ruzwana/main.py at Timestamp: 2019-03-28 19:19:05

--- ruzwana/main.py
+++ ruzwana/main.py
@@ -1,4 +1,5 @@
 # ada.ruzwana.main.py
+from browser import doc
 from _spy.vpython.main import *
 doc['pydiv'].html=''
 _gs=Glow('pydiv')
@@ -6,3 +7,6 @@ scene=canvas()
 scene.background= color.blue #"rgba(0, 0, 200, 0.5)"
 scene.width = 600
 scene.height = 600
+box(color=color.red)
+cylinder(color=color.blue)
+tr = shapes.triangle(length=5)
