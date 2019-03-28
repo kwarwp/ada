377d6391df8cdab52bc2bb944a0013a9a632c644
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-28 11:23:45 -0300

    Saved adda/__log__.py at Timestamp: 2019-03-28 14:23:44

--- adda/__log__.py
+++ adda/__log__.py
@@ -77,3 +77,23 @@ NameError: name 'dark' is not defined
                          ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Thu Mar 28 2019 11:23:48.555 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 20
+    cena = Cena(wall)
+  module _spy.vitollino.main line 780
+    self._cria_divs(width)
+  module _spy.vitollino.main line 785
+    divesq.style.width = width // 3  # 100
+TypeError: unsupported operand type(s) for //: 'str' and 'int'
+'''},
