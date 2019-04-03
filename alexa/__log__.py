a7ec600836d6a76d64f6d86826fc4ab62ef658d6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-04-03 17:45:56 -0300

    Saved alexa/__log__.py at Timestamp: 2019-04-03 20:45:56

--- alexa/__log__.py
+++ alexa/__log__.py
@@ -82,3 +82,23 @@ Traceback (most recent call last):
     self.um_valor *= 2
 TypeError: unsupported operand types for *: 'function' and 'int'
 '''},
+{'date': 'Wed Apr 03 2019 17:45:54.811 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''faz_algo 7
+faz_qqc 7
+Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 37
+    faz_algo()
+  module <module> line 33
+    print("inspeciona o_lambda", dir(o_lambda), (o_lambda.__code__.co_locals))
+AttributeError: 'code' object has no attribute 'co_locals'
+'''},
