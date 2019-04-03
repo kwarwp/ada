0098d85811346c2abcaab9357a59486802311c84
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-04-03 17:56:09 -0300

    Saved alexa/__log__.py at Timestamp: 2019-04-03 20:56:09

--- alexa/__log__.py
+++ alexa/__log__.py
@@ -122,3 +122,19 @@ Traceback (most recent call last):
     print("inspeciona o_lambda", dir(o_lambda), (o_lambda.__code__).co_locals)
 AttributeError: 'code' object has no attribute 'co_locals'
 '''},
+{'date': 'Wed Apr 03 2019 17:56:08.489 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 37
+    vez2 = {x: X*2 for x in [1,2,3,4]}
+NameError: name 'X' is not defined
+'''},
