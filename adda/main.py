a7edb98a5b6f1a69bbf4fd29fd570a7e47e4dc94
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-04-09 18:01:15 -0300

    Saved adda/main.py at Timestamp: 2019-04-09 21:01:15

--- adda/main.py
+++ adda/main.py
@@ -17,10 +17,15 @@ glow4 = "https://i.imgur.com/W3OysVW.png"
 glow5 = "https://i.imgur.com/EajKgg3.png"
 door = "https://i.imgur.com/MwjexQO.png"
 fire1 = "https://i.imgur.com/dNHUPQZ.png"
+fire_1 = "https://i.imgur.com/dNHUPQZ.png"
 fire2 = "https://i.imgur.com/I5eZveE.png"
+fire_2 = "https://i.imgur.com/I5eZveE.png"
 fire3 = "https://i.imgur.com/Gw9pNuR.png"
+fire_3 = "https://i.imgur.com/Gw9pNuR.png"
 fire4 = "https://i.imgur.com/tANP62N.png"
+fire_4 = "https://i.imgur.com/tANP62N.png"
 fire5 = "https://i.imgur.com/TCG2mkF.png"
+fire_5 = "https://i.imgur.com/TCG2mkF.png"
 os_fire = [fire1,fire2,fire3,fire4,fire5]
 os_glows = [glow1,glow2,glow3,glow4,glow5]
 cena = Cena(wall)
@@ -33,8 +38,18 @@ torch_2 = Elemento(torch_2,style=dict(left=650,top=-4,width="600px",height="400p
 glow1 = Elemento(glow1,style=dict(left=-15,top=0,width="500px",height="400px",opacity=0.7))
 glow_1= Elemento(glow_1,style=dict(left=700,top=0,width="500px",height="400px",opacity=0.7))
 door = Elemento(door,style=dict(left=-20,top=7,width="1200px",height="612px"))
-os_fire1 = Elemento(os_fire,style=dict(left=180,top=-103,width="600px",height="300px"))
-os_fire2 = Elemento(os_fire,style=dict(left=900,top=-103,width="600px",height="300px"))
+fire1 = Elemento(fire1,style=dict(left=180,top=-103,width="600px",height="300px"))
+fire_1 = Elemento(fire_1,style=dict(left=900,top=-103,width="600px",height="300px"))
+fire2 = Elemento(fire2,style=dict(left=65,top=-103,width="600px",height="300px"))
+fire_2 = Elemento(fire_2,style=dict(left=785,top=-103,width="600px",height="300px"))
+fire3 = Elemento(fire3,style=dict(left=-50,top=-103,width="600px",height="300px"))
+fire_3 = Elemento(fire_3,style=dict(left=660,top=-103,width="600px",height="300px"))
+fire4 = Elemento(fire4,style=dict(left=-160,top=-103,width="600px",height="300px"))
+fire_4 = Elemento(fire_4,style=dict(left=550,top=-103,width="600px",height="300px"))
+fire5 = Elemento(fire5,style=dict(left=430,top=-103,width="600px",height="300px"))
+fire_5 = Elemento(fire_5,style=dict(left=-280,top=-103,width="600px",height="300px"))
+#fire2 = Elemento(fire2,style=dict(left=800,top=-103,width="600px",height="300px"))
+#os_fire1 = Elemento(os_fire,style=dict(left=180,top=-103,width="600px",height="300px"))
 #dark= Elemento("",style=dict(width="960px",height="600px",backgroundColor="black",opacity=0.5))
 window.entra(cena)
 window2.entra(cena)
@@ -43,8 +58,18 @@ torch_2.entra(cena)
 glow1.entra(cena)
 glow_1.entra(cena)
 door.entra(cena)
-os_fire1.entra(cena)
-os_fire2.entra(cena)
+fire1.entra(cena)
+fire_1.entra(cena)
+fire2.entra(cena)
+fire_2.entra(cena)
+fire3.entra(cena)
+fire_3.entra(cena)
+fire4.entra(cena)
+fire_4.entra(cena)
+fire5.entra(cena)
+fire_5.entra(cena)
+#os_fire1.entra(cena)
+#os_fire2.entra(cena)
 #dark.entra(cena)
 cena.vai()
 o_glow = 0
@@ -57,3 +82,13 @@ def troca_glow(ev=0): #efeito gif
         o_glow = 0
 troca_glow()
 timer.set_interval(troca_glow,200)
+o_fire = 0
+def troca_fire(ev=0): #efeito gif
+    global o_fire
+    fire.img.src= choice(os_glows) #os_glows[o_glow]
+    fire_1.img.src= choice(os_glows) #os_glows[o_glow]
+    o_fire += 1
+    if o_fire > 4:
+        o_fire = 0
+troca_fire()
+timer.set_interval(troca_fire,200)
