b5b436619536b43094a09247664df3668ef11e8a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-04-09 17:20:49 -0300

    Saved adda/__log__.py at Timestamp: 2019-04-09 20:20:49

--- adda/__log__.py
+++ adda/__log__.py
@@ -304,3 +304,19 @@ NameError: name 'fire_3' is not defined
     fire_3 = Elemento(fire_3,style=dict(left=785,top=-103,width="600px",height="300px"))
 NameError: name 'fire_3' is not defined
 '''},
+{'date': 'Tue Apr 09 2019 17:20:48.599 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 43
+    fire_3 = Elemento(fire_3,style=dict(left=785,top=-103,width="600px",height="300px"))
+NameError: name 'fire_3' is not defined
+'''},
