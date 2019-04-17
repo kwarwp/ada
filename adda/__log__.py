617b9031894ca1366f117a6bf13740fff0103c67
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-04-17 18:03:36 -0300

    Saved adda/__log__.py at Timestamp: 2019-04-17 21:03:36

--- adda/__log__.py
+++ adda/__log__.py
@@ -1750,3 +1750,21 @@ NameError: name 'coice' is not defined
     thefire1 = coice(os_fire)
 NameError: name 'coice' is not defined
 '''},
+{'date': 'Wed Apr 17 2019 18:03:34.903 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 111
+    troca_fire()
+  module <module> line 94
+    thefire1 = coice(os_fire)
+NameError: name 'coice' is not defined
+'''},
