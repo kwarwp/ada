a7676708fe355d0ca57b53003c091053efd6a0d4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-04-17 17:00:31 -0300

    Saved adda/__log__.py at Timestamp: 2019-04-17 20:00:30

--- adda/__log__.py
+++ adda/__log__.py
@@ -1374,3 +1374,19 @@ AttributeError: 'str' object has no attribute 'img'
     fire2.img.src= choice(os_fire)
 AttributeError: 'str' object has no attribute 'img'
 '''},
+{'date': 'Wed Apr 17 2019 17:00:29.939 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 34
+    fire_offset = {fire1:100,fire_1:200,fire2:300,fire:400,fire_2:500,fire3:600,fire_3:700,fire4:800,fire_4:900}
+NameError: name 'fire' is not defined
+'''},
