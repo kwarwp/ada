df15a5bdd529fc03cf4619ab3379b2bf3837c1b5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-04-18 12:03:54 -0300

    Saved sara/main.py at Timestamp: 2019-04-18 15:03:53

--- sara/main.py
+++ sara/main.py
@@ -1,3 +1,14 @@
 # ada.sara.main.py
-from roxanne.main import main
-main()
+from _spy.vitollino.main import Cena, Elemento, STYLE
+from browser import html, alert, timer
+from random import choice, shuffle
+STYLE["width"], STYLE["height"] = 900, "600px"
+
+class Kamui:
+    def __init__(self):
+        _kamui = "https://i.imgur.com/R3pCA3a.jpg"
+        self.kamui = Cena(_kamui)
+        self.kamui.vai()
+
+if __name__ == "__main__":
+    _ = Kamui()
