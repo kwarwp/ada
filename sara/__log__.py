701b48e79b961bbb18c52bb6730eada04abdc080
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-04-18 12:22:30 -0300

    Saved sara/__log__.py at Timestamp: 2019-04-18 15:22:29

--- sara/__log__.py
+++ sara/__log__.py
@@ -17,3 +17,21 @@
     def pula_issun(_=0, issun_=self.issun.elt.style):
 AttributeError: 'Kamui' object has no attribute 'issun'
 '''},
+{'date': 'Thu Apr 18 2019 12:22:28.397 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 24
+    _ = Kamui()
+  module <module> line 21
+    timer.setinterval(300,pula_issun)
+AttributeError: 'module' object has no attribute 'setinterval'
+'''},
