fc0215b2521115379805f36320e2c4770f4e4f6a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-04-18 12:22:40 -0300

    Saved sara/main.py at Timestamp: 2019-04-18 15:22:39

--- sara/main.py
+++ sara/main.py
@@ -6,9 +6,19 @@ STYLE["width"], STYLE["height"] = 900, "600px"
 
 class Kamui:
     def __init__(self):
+        self.pulo = 10
         _kamui = "https://i.imgur.com/R3pCA3a.jpg"
+        _okami = "https://i.imgur.com/1jSbzEj.png"
+        _issun = "https://i.imgur.com/lAYKq3F.png"
         self.kamui = Cena(_kamui)
+        self.okami = Elemento(_okami, cena=self.kamui)
+        self.issun = Elemento(
+            _issun, cena=self.kamui, style=dict(left=135, top=400, width=20, height="20px"))
         self.kamui.vai()
+        def pula_issun(_=0, issun_=self.issun.elt.style):
+            issun_.top = issun_.top + self.pulo
+            self.pulo = -self.pulo
+        timer.setinterval(300,pula_issun)
 
 if __name__ == "__main__":
     _ = Kamui()
