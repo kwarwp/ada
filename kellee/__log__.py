36ef879b006323d677624d0f5b91783396ff6de0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-30 16:52:22 -0300

    Saved kellee/__log__.py @2019-09-30 19:52

--- kellee/__log__.py
+++ kellee/__log__.py
@@ -17,3 +17,21 @@
     class Testing(unittest.TestCase):
 AttributeError: 'module' object has no attribute 'TestCase'
 '''},
+{'date': 'Mon Sep 30 2019 16:52:21.398 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 43
+    from roxanne.main import test
+  module roxanne.main line 200
+    class Testing(unittest.case.TestCase):
+AttributeError: 'module' object has no attribute 'case'
+'''},
