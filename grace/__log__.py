8982b98e62a3664c8b594532bd04d7d576d99f26
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-02 19:26:23 -0300

    Saved grace/__log__.py @2019-10-02 22:26

--- grace/__log__.py
+++ grace/__log__.py
@@ -53,3 +53,19 @@ Traceback (most recent call last):
     raise NotImplementedError("Blocking functions like time.sleep() are not "
 NotImplementedError: Blocking functions like time.sleep() are not supported in the browser. Use functions in module browser.timer instead.
 '''},
+{'date': 'Wed Oct 02 2019 19:26:21.245 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 5
+    jog = int(input('''\033[;32;mSuas opÃÂ§ÃÂµes:
+ValueError: invalid literal for int() with base 10: ''
+'''},
