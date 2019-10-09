dc5f1463460d20f76d54189f47325c7093987555
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-09 17:53:00 -0300

    Saved grace/main.py @2019-10-09 20:53

--- grace/main.py
+++ grace/main.py
@@ -2,11 +2,11 @@ from random import randint
 from time import sleep
 itens = ('Pedra', 'Papel', 'Tesoura')
 comp = randint(0,2)
-jog = int(input('''\033[;32;mSuas opÃ§Ãµes:
+jog = int(input('''\033[;32;mSuas opções:
 \033[;36;m[ 0 ] PEDRA
 [ 1 ] PAPEL
 [ 2 ] TESOURA
-\033[;32;mQual Ã© a sua jogada? '''))
+\033[;32;mQual e a sua jogada? '''))
 print('\033[;31;mJO')
 sleep(1)
 print('\033[;31;mKEN')
