415a001e02fca03e4034d1264cb536d9de8d3a61
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-01 10:55:22 -0300

    Created libby/__log__.py @2019-11-01 13:55

---
+++ libby/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Fri Nov 01 2019 10:55:21.725 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 7
+  cdd= Cena(img=CDD)
+  ^
+IndentationError: expected an indented block
+'''},
