4a3b320a3558fa85fb198080658f1df52a0f6cb1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-01 13:00:32 -0300

    Created natalia/main.py @2019-11-01 16:00

---
+++ natalia/main.py
@@ -0,0 +1,5 @@
+# ada.natalia.main.py
+ from _spy.vitollino.main import CENA,Elemento 
+ from _spy.vitollino.main import INVENTARIO as inv
+ CANADA= "https://www.estudopratico.com.br/wp-content/uploads/2019/04/ottawa-capital-canada.jpg"
+ SALA= " https://cdn.imgbin.com/17/18/18/imgbin-kim-taehyung-bts-k-pop-tomorrow-exo-others-B9xAfxjVW72prTfKWQzahU4PQ.jpg"
