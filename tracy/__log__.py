c3fc16bc7050471baea5880ebf28d0f0db8657e3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-01 13:17:09 -0300

    Created tracy/__log__.py @2019-11-01 16:17

---
+++ tracy/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Fri Nov 01 2019 13:17:07.596 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 6
+  class vkook
+  ^
+IndentationError: unexpected indent
+'''},
