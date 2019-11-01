80467e290d407629f52bebdbf7f3605d7f0e8d48
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-01 11:41:31 -0300

    Created naomi/__log__.py @2019-11-01 14:41

---
+++ naomi/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Fri Nov 01 2019 11:41:28.675 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 5
+  CR7="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8c/Cristiano_Ronaldo_2018.jpg/399px-Cristiano_Ronaldo_2018.jpg
+                                                                                                                            ^
+SyntaxError: EOL while scanning string literal
+'''},
