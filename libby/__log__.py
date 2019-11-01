d3c44bb72cd2ac9d48b7aa5379e18a51ee19d6f6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-01 10:55:49 -0300

    Saved libby/__log__.py @2019-11-01 13:55

--- libby/__log__.py
+++ libby/__log__.py
@@ -13,3 +13,10 @@ IndentationError: expected an indented block
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Fri Nov 01 2019 10:55:49.928 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 7
+  cdd= Cena(img=CDD)
+  ^
+IndentationError: expected an indented block
+'''},
