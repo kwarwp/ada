c89eb09502c1d53e1bd6295044461ed71a422b09
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-01 11:42:56 -0300

    Saved naomi/main.py @2019-11-01 14:42

--- naomi/main.py
+++ naomi/main.py
@@ -2,4 +2,4 @@
 from _spy.vitollino.main import Cena, Elemento
 from _spy.vitollino.main import INVENTARIO as inv 
 STAR="http://media.comicbook.com/2017/10/central-city-the-flash-1038707.jpg"
-CR7="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8c/Cristiano_Ronaldo_2018.jpg/399px-Cristiano_Ronaldo_2018.jpg
+CR7="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8c/Cristiano_Ronaldo_2018.jpg/399px-Cristiano_Ronaldo_2018.jpg"
