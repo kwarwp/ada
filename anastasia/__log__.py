3c93f113ae790ce5f9eccec356ca193b9a057e28
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-01 14:15:00 -0300

    Created anastasia/__log__.py @2019-11-01 17:15

---
+++ anastasia/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Fri Nov 01 2019 14:14:54.501 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 2
+  from _spy.Vitollino.main Import Cena,Elemento
+                                      ^
+SyntaxError: invalid syntax
+'''},
