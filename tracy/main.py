27c6dbb21a0020d5566523dfa469c98075ecca9d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-01 13:19:32 -0300

    Saved tracy/main.py @2019-11-01 16:19

--- tracy/main.py
+++ tracy/main.py
@@ -3,7 +3,7 @@ from _spy.vitollino.main import Cena,Elemento
 from _spy.vitollino.main import INVENTARIO as inv
 CANADA= " https://image.freepik.com/fotos-gratis/porto-vancouver-em-por-do-sol-columbia-britanica-canada_67340-69.jpg"
 JUNGKOOK= "https://img1.gratispng.com/20180423/afe/kisspng-jungkook-2017-bts-live-trilogy-episode-iii-the-wi-spring-day-5ade62b93a30b3.5168236115245237052384.jpg"
- class vkook():
+class vkook():
     canada= Cena(img=CANADA)
     jungkook= Elemento(img=JUNGKOOK)
     jungkook.entra(canada)
