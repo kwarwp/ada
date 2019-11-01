4c32fdebcf056124837bfe85c9ea2326e3b440b2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-01 14:14:36 -0300

    Saved anastasia/main.py @2019-11-01 17:14

--- anastasia/main.py
+++ anastasia/main.py
@@ -1,1 +1,11 @@
 # ada.anastasia.main.py
+from _spy.Vitollino.main Import Cena,Elemento
+from _spy.Vitollino.main Import Inventario as inv
+BARBIE="https://www.bigw.com.au/medias/sys_master/images/images/hc6/h0c/12811409752094.jpg"
+CIDADE="https://www.bigw.com.au/medias/sys_master/images/images/hc6/h0c/12811409752094.jpg"
+class barbie():
+    barbie= Elemento(img=BARBIE)
+    cidade= Cena(img=CIDADE)
+    cidade.entra(cidade)
+    cidade.vai()
+barbie()
