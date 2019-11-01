40a70581af755e4d3972c1699e4c8eef43698c46
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-01 11:47:20 -0300

    Saved naomi/main.py @2019-11-01 14:47

--- naomi/main.py
+++ naomi/main.py
@@ -3,3 +3,9 @@ from _spy.vitollino.main import Cena, Elemento
 from _spy.vitollino.main import INVENTARIO as inv 
 STAR="http://media.comicbook.com/2017/10/central-city-the-flash-1038707.jpg"
 CR7="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8c/Cristiano_Ronaldo_2018.jpg/399px-Cristiano_Ronaldo_2018.jpg"
+class jogador():
+    star=Cena(img=STAR)
+    cr7=Elemento(img=CR7)
+    cr7.entra(star)
+    star.vai()
+jogador()    
