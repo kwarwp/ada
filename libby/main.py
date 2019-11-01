8eea907bce3d2c01783c29f618707ade7ef8ea8b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-01 10:55:16 -0300

    Saved libby/main.py @2019-11-01 13:55

--- libby/main.py
+++ libby/main.py
@@ -1,1 +1,10 @@
 # ada.libby.main.py
+from _spy.vitollino.main  import Cena,Elemento
+from _spy.vitollino.main import INVENTARIO as inv
+CDD="https://upload.wikimedia.org/wikipedia/commons/e/e9/Cidade_de_Deus.jpg"
+FLASH="https://pngriver.com/wp-content/uploads/2018/03/Download-Flash-PNG-Pic-For-Designing-Projects.png"
+class sempai():
+cdd= Cena(img=CDD)
+flash= Elemento(img=FLASH)
+flash.entra(cdd)
+sempai()
