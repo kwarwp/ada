86a06eca5aed55dd90010d24e5546c9550ccd1ba
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-01 13:23:42 -0300

    Saved tracy/__log__.py @2019-11-01 16:23

--- tracy/__log__.py
+++ tracy/__log__.py
@@ -198,3 +198,10 @@ ImportError: _spy
     from _spy.vitollino.main import Cena, Elemento
 ImportError: _spy
 '''},
+{'date': 'Fri Nov 01 2019 13:23:41.374 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 3
+  from _spy.vitollino.main import INVENTARIO as inv
+  ^
+IndentationError: unexpected indent
+'''},
