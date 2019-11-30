e8d9876983b0d14a93b56c8ffc5a2b1b60a18fc9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-30 16:39:19 -0300

    Saved elevador/__log__.py @2019-11-30 19:39

--- elevador/__log__.py
+++ elevador/__log__.py
@@ -180,3 +180,23 @@ SyntaxError: keyword can't be an expression
     self.posicao = dict(x=elt.x, y=elt.y)
 NameError: name 'elt' is not defined
 '''},
+{'date': 'Sat Nov 30 2019 16:39:18.741 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 170
+    Elevador()
+  module <module> line 109
+    self.cesta = Item(CESTA, dict(x=0, y=300), cena=predio, x=250, y=50,w=180,h=180)
+  module <module> line 48
+    self.posicao = dict(x=self.elt.x, y=self.elt.y)
+AttributeError: 'Item' object has no attribute 'elt'
+'''},
