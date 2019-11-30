69d4eae3c9d25f0246ef6557ab50152e055c5800
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-30 11:24:23 -0300

    Saved elevador/__log__.py @2019-11-30 14:24

--- elevador/__log__.py
+++ elevador/__log__.py
@@ -24,3 +24,23 @@ TypeError: 'module' object is not callable
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Sat Nov 30 2019 11:24:22.342 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 111
+    Elevador()
+  module <module> line 66
+    self.cesta = Elemento(CESTA, x=300, y=100,w=180,h=180, cena=predio, vai=self.sobe_desce)
+  module <module> line 15
+    self.posicao_inicial, self.posicao_final = posicao_inicial, posicao_final
+NameError: name 'posicao_inicial' is not defined
+'''},
