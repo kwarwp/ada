2504cb5345f6da00d093827ada689cd0b74a27d4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-30 17:14:12 -0300

    Saved elevador/__log__.py @2019-11-30 20:14

--- elevador/__log__.py
+++ elevador/__log__.py
@@ -238,3 +238,21 @@ AttributeError: 'Item' object has no attribute 'x'
     self.doggie = Passageiro(Doggie, dict(x=20, y=40), veiculo=self.cesta, cena=predio, x=440, y=60)
 AttributeError: 'Elevador' object has no attribute 'cesta'
 '''},
+{'date': 'Sat Nov 30 2019 17:14:11.70 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 45
+    class Item:
+  module <module> line 46
+    def __init__(self, imagem, posicao_final, vai=None **kwargs):
+NameError: name 'kwargs' is not defined
+'''},
