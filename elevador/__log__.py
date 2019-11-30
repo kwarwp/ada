95cf935a81e41a93720a5421f29030660187be23
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-30 16:40:32 -0300

    Saved elevador/__log__.py @2019-11-30 19:40

--- elevador/__log__.py
+++ elevador/__log__.py
@@ -200,3 +200,23 @@ NameError: name 'elt' is not defined
     self.posicao = dict(x=self.elt.x, y=self.elt.y)
 AttributeError: 'Item' object has no attribute 'elt'
 '''},
+{'date': 'Sat Nov 30 2019 16:40:31.59 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 170
+    Elevador()
+  module <module> line 110
+    self.doggie = Passageiro(Doggie, dict(x=-420, y=0), veiculo=self.cesta, cena=predio, x=440, y=60)
+  module <module> line 81
+    self.posicao = Pos(self.veiculo.x - self.elt.x, self.veiculo.y - self.elt.y)
+AttributeError: 'Item' object has no attribute 'x'
+'''},
