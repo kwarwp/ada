80a08e47cbeac4d3eaf6727b47f19b8c54dd2f21
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-30 17:05:48 -0300

    Saved elevador/__log__.py @2019-11-30 20:05

--- elevador/__log__.py
+++ elevador/__log__.py
@@ -220,3 +220,21 @@ AttributeError: 'Item' object has no attribute 'elt'
     self.posicao = Pos(self.veiculo.x - self.elt.x, self.veiculo.y - self.elt.y)
 AttributeError: 'Item' object has no attribute 'x'
 '''},
+{'date': 'Sat Nov 30 2019 17:05:47.24 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 172
+    Elevador()
+  module <module> line 112
+    self.doggie = Passageiro(Doggie, dict(x=20, y=40), veiculo=self.cesta, cena=predio, x=440, y=60)
+AttributeError: 'Elevador' object has no attribute 'cesta'
+'''},
