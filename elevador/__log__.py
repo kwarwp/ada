c01ad09de354656dcb12979742dd20e0d1c96e6c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-30 11:31:21 -0300

    Saved elevador/__log__.py @2019-11-30 14:31

--- elevador/__log__.py
+++ elevador/__log__.py
@@ -64,3 +64,23 @@ NameError: name 'posicao_inicial' is not defined
     self.posicao_inicial, self.posicao_final = posicao_inicial, posicao_final
 NameError: name 'posicao_inicial' is not defined
 '''},
+{'date': 'Sat Nov 30 2019 11:31:20.15 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 119
+    Elevador()
+  module <module> line 59
+    self.cesta = Item(CESTA, dict(x=0, y=300), cena=predio, x=300, y=100,w=180,h=180)
+  module <module> line 35
+    self.posicao_inicial, self.posicao_final = posicao_inicial, posicao_final
+NameError: name 'posicao_inicial' is not defined
+'''},
