6188d04180f438a0b05378ae3abb16a74fea2928
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-01 08:06:46 -0300

    Saved elevador/__log__.py @2019-12-01 11:06

--- elevador/__log__.py
+++ elevador/__log__.py
@@ -334,3 +334,9 @@ Elevador()
     self.scorer = dict(ponto=1, valor=cena.nome, carta=tit or img, casa=self.xy, move=None)
 AttributeError: 'Plataforma' object has no attribute 'nome'
 '''},
+{'date': 'Sun Dec 01 2019 08:06:45.700 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+Exception: $B.get_class(...) is undefined
+  module '$exec_445' line 221
+Elevador()
+'''},
