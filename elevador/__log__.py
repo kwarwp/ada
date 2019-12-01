19af249f7b782227c3ec9e7fefb54aa3f272fda1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-01 13:34:32 -0300

    Saved elevador/__log__.py @2019-12-01 16:34

--- elevador/__log__.py
+++ elevador/__log__.py
@@ -480,3 +480,25 @@ RuntimeError: too much recursion
   module '$exec_2074' line 183
         self.doggie.entra(p0)
 '''},
+{'date': 'Sun Dec 01 2019 13:34:31.86 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 185
+    Elevador()
+  module <module> line 183
+    self.doggie.entra(p0)
+  module <module> line 151
+    cena <= self
+  module <module> line 129
+    assert isinstance(other,Elemento), other
+AssertionError: <__main__.Passageiro object>
+'''},
