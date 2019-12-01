b06d921e919311229cc8833ef8e3750d37df1f09
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-01 08:05:34 -0300

    Saved elevador/__log__.py @2019-12-01 11:05

--- elevador/__log__.py
+++ elevador/__log__.py
@@ -308,3 +308,29 @@ Exception: $B.get_class(...) is undefined
   module '$exec_445' line 221
 Elevador()
 '''},
+{'date': 'Sun Dec 01 2019 08:05:32.925 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 220
+    Elevador()
+  module <module> line 154
+    self.doggie = Passageiro(Doggie, dict(x=20, y=40), veiculo=self.cesta, cena=p0, x=10, y=10)
+  module <module> line 113
+    super().__init__(imagem, posicao_final, cena=cena, **kwargs)
+  module <module> line 53
+    self.elt = Elemento(imagem, vai=self.movimenta, **kwargs)
+  module <module> line 15
+    super().__init__(img=img, vai=vai, style=style, tit=tit, alt=alt, cena=cena,
+  module _spy.vitollino.main line 539
+    self.scorer = dict(ponto=1, valor=cena.nome, carta=tit or img, casa=self.xy, move=None)
+AttributeError: 'Plataforma' object has no attribute 'nome'
+'''},
