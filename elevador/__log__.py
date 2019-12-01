ba9aa64c100995ebb4d7c34a2e5a6f77d6aa7978
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-01 13:55:03 -0300

    Saved elevador/__log__.py @2019-12-01 16:55

--- elevador/__log__.py
+++ elevador/__log__.py
@@ -532,3 +532,21 @@ AssertionError: <__main__.Passageiro object>
     assert isinstance(other, Passageiro), other
 AssertionError: <__main__.Elemento object>
 '''},
+{'date': 'Sun Dec 01 2019 13:55:02.414 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 152
+    Elevador()
+  module <module> line 149
+    self.doggie = Passageiro(Doggie, dict(x=20, y=40), veiculo=p0, x=10, y=10)
+TypeError: __init__ missing 1 positional argument: 'cena'
+'''},
