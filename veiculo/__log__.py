5f9b8c6e7d5bc65adfbb37c0298d6da1a02ae9c1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-03 14:19:23 -0300

    Created veiculo/__log__.py @2019-12-03 17:19

---
+++ veiculo/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Tue Dec 03 2019 14:19:21.450 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 18
+  super().__init__(img=img, vai=vai, tit=tit, alt=alt,
+                                                                               ^
+SyntaxError: invalid syntax
+'''},
