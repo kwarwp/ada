d42ebf8bdf87b19df52a199a0d18024f05724509
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-05 11:29:51 -0300

    Saved elevador/__log__.py @2019-12-05 14:29

--- elevador/__log__.py
+++ elevador/__log__.py
@@ -576,3 +576,29 @@ TypeError: __init__ missing 1 positional argument: 'cena'
     self.elt.style.backgroundImage = f"url({value})"
 AttributeError: 'Elemento' object has no attribute 'elt'
 '''},
+{'date': 'Thu Dec 05 2019 11:29:50.374 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 164
+    Elevador()
+  module <module> line 154
+    self.plataforma0 = p0 = Plataforma(cena=predio, x=320, y=50,w=250,h=180)
+  module <module> line 108
+    self.elt = Elemento(imagem, style=dict(opacity=1), **kwargs)
+  module <module> line 17
+    super().__init__(img=img, vai=vai, style=style, tit=tit, alt=alt, cena=cena,
+  module _spy.vitollino.main line 519
+    self.img, self.title, self.dropper, self.alt = img, tit, drop, alt
+  module <module> line 38
+    self.elt.style.backgroundImage = f"url({value})"
+AttributeError: 'Elemento' object has no attribute 'elt'
+'''},
