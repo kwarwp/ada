f3acc06831ed741f57c7b1a506bd7ad47ba30f4f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-06 09:37:28 -0300

    Saved elevador/__log__.py @2019-12-06 12:37

--- elevador/__log__.py
+++ elevador/__log__.py
@@ -692,3 +692,27 @@ TypeError: unsupported operand type(s) for //: 'int' and 'tuple'
     self.elt.width = self._w // x
 TypeError: unsupported operand type(s) for //: 'int' and 'tuple'
 '''},
+{'date': 'Fri Dec 06 2019 09:37:26.654 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 182
+    Elevador()
+  module <module> line 171
+    self.plataforma0 = p0 = Plataforma(cena=predio, x=320, y=50,w=250,h=180)
+  module <module> line 125
+    self.elt = Elemento(imagem, style=dict(opacity=1), **kwargs)
+  module <module> line 26
+    self.s = s
+  module <module> line 37
+    self._s = (x, y)
+NameError: name 'x' is not defined
+'''},
