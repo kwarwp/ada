a5babcd30c14dc4b37ed1117808bc18d41eb631b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-06 10:07:26 -0300

    Saved elevador/__log__.py @2019-12-06 13:07

--- elevador/__log__.py
+++ elevador/__log__.py
@@ -716,3 +716,23 @@ TypeError: unsupported operand type(s) for //: 'int' and 'tuple'
     self._s = (x, y)
 NameError: name 'x' is not defined
 '''},
+{'date': 'Fri Dec 06 2019 10:07:25.105 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 194
+    Elevador()
+  module <module> line 190
+    self.guria.elt.p(1, 1)
+  module <module> line 33
+    return self._p
+AttributeError: 'Elemento' object has no attribute '_p'
+'''},
