61e28ac5b56ed7ebb1de2984317643fd1c30e040
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-09 11:15:36 -0300

    Saved elevador/__log__.py @2019-12-09 14:15

--- elevador/__log__.py
+++ elevador/__log__.py
@@ -862,3 +862,23 @@ TypeError: 'int' object is not callable
     x, y = value if len(value) > 1 else (value % self._s[0], value // self._s[0])
 TypeError: unsupported operand type(s) for %: 'list' and 'int'
 '''},
+{'date': 'Mon Dec 09 2019 11:15:34.705 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 195
+    Elevador()
+  module <module> line 191
+    self.guria.elt.p = 1, 2
+  module <module> line 39
+    x, y = value if len(value) > 1 else (value[0] % self._s[0], value[0] // self._s[0])
+TypeError: unsupported operand type(s) for %: 'list' and 'int'
+'''},
