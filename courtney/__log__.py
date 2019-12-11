fc07dd65ea469b95b52b6116167a5a8f2092b8ae
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-11 12:32:14 -0300

    Saved courtney/__log__.py @2019-12-11 15:32

--- courtney/__log__.py
+++ courtney/__log__.py
@@ -153,3 +153,23 @@ NameError: name 'integrantes' is not defined
     self.vai = self.destino.entra_na_cesta(self)
 AttributeError: 'Plataforma' object has no attribute 'entra_na_cesta'
 '''},
+{'date': 'Wed Dec 11 2019 12:32:12.465 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 102
+    Basico()
+  module <module> line 94
+    self.doggie = Personagem(DOG, destino=self.cesta_esquerda, cena=cena, tit = "10kg", x=540, y=145, w=80,h=50)
+  module <module> line 36
+    self.vai = self.destino.entra_na_cesta(self, evento=None)#self.move
+TypeError: entra_na_cesta() got an unexpected keyword argument 'evento'
+'''},
