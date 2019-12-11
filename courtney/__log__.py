74c31088ea90f8877c963f130edbd975b6c34414
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-11 11:01:36 -0300

    Created courtney/__log__.py @2019-12-11 14:01

---
+++ courtney/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Wed Dec 11 2019 11:01:34.237 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 95
+  def entra_na_cesta(self, personagem)
+                                       ^
+SyntaxError: invalid syntax
+'''},
