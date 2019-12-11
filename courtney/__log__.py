f1ab4231d5fa2eb562b7082491d2c91903602975
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-11 14:55:44 -0300

    Saved courtney/__log__.py @2019-12-11 17:55

--- courtney/__log__.py
+++ courtney/__log__.py
@@ -480,3 +480,23 @@ TypeError: __init__() got multiple values for argument 'controlador'
     self.doggie = Personagem(DOG, controlador= self, destino=self.cesta_esquerda, cena=cena, tit = "10kg", x=540, y=145, w=80,h=50)
 TypeError: __init__() got an unexpected keyword argument 'controlador'
 '''},
+{'date': 'Wed Dec 11 2019 14:55:42.840 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 115
+    Controlador()
+  module <module> line 107
+    self.doggie = Personagem(DOG, controlador= self, destino=self.cesta_esquerda, cena=cena, tit = "10kg", x=540, y=145, w=80,h=50)
+  module <module> line 38
+    self,controlador = controlador
+TypeError: 'Controlador' object is not iterable
+'''},
