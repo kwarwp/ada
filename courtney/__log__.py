6f69b593528ba21d128b311990164f920c96df85
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-11 14:52:44 -0300

    Saved courtney/__log__.py @2019-12-11 17:52

--- courtney/__log__.py
+++ courtney/__log__.py
@@ -426,3 +426,21 @@ AttributeError: 'Cesta' object has no attribute 'fundo'
                  ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Dec 11 2019 14:52:42.248 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 114
+    Controlador()
+  module <module> line 100
+    self.cesta_esquerda = Cesta(CEST, controlador= self, destino=self.base_topo, cena=self.base)
+TypeError: __init__() got multiple values for argument 'controlador'
+'''},
