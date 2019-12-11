6e5dd31dbad57c93b6b5c35ba8f4b313dc72c65d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-11 14:54:14 -0300

    Saved courtney/__log__.py @2019-12-11 17:54

--- courtney/__log__.py
+++ courtney/__log__.py
@@ -444,3 +444,21 @@ SyntaxError: invalid syntax
     self.cesta_esquerda = Cesta(CEST, controlador= self, destino=self.base_topo, cena=self.base)
 TypeError: __init__() got multiple values for argument 'controlador'
 '''},
+{'date': 'Wed Dec 11 2019 14:54:13.101 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 114
+    Controlador()
+  module <module> line 100
+    self.cesta_esquerda = Cesta(CEST, controlador = self, destino=self.base_topo, cena=self.base)
+TypeError: __init__() got multiple values for argument 'controlador'
+'''},
