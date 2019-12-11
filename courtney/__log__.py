b6ade6bb369ebda67e5a9e2555f223d3427c90fa
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-11 12:41:36 -0300

    Saved courtney/__log__.py @2019-12-11 15:41

--- courtney/__log__.py
+++ courtney/__log__.py
@@ -200,3 +200,21 @@ TypeError: entra_na_cesta() got multiple values for argument 'evento'
                                                          ^
 SyntaxError: non-keyword arg after keyword arg
 '''},
+{'date': 'Wed Dec 11 2019 12:41:34.227 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 104
+    Controlador()
+  module <module> line 96
+    self.doggie = Personagem(DOG, controlador=self, cena=cena, tit = "10kg", x=540, y=145, w=80,h=50)
+TypeError: __init__() got an unexpected keyword argument 'controlador'
+'''},
