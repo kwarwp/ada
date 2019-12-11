60549d2e8892a7ea1cb3b4f20b996af46c2b2aa8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-11 12:44:08 -0300

    Saved courtney/__log__.py @2019-12-11 15:44

--- courtney/__log__.py
+++ courtney/__log__.py
@@ -236,3 +236,21 @@ TypeError: __init__() got an unexpected keyword argument 'controlador'
     self.cesta_esquerda = Cesta(CEST, destino=self.base_topo, cena=self.base)
 TypeError: __init__() got an unexpected keyword argument 'destino'
 '''},
+{'date': 'Wed Dec 11 2019 12:44:06.391 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 103
+    Controlador()
+  module <module> line 89
+    self.cesta_esquerda = Cesta(CEST, destino=self.base_topo, cena=self.base)
+TypeError: __init__() got an unexpected keyword argument 'destino'
+'''},
