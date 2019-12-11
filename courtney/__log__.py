5e165e5aa67cb41d517af896028516854da87259
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-11 11:09:22 -0300

    Saved courtney/__log__.py @2019-12-11 14:09

--- courtney/__log__.py
+++ courtney/__log__.py
@@ -89,3 +89,25 @@ AttributeError: 'Cesta' object has no attribute 'enta_na_cesta'
     for p in personagens:
 NameError: name 'personagens' is not defined
 '''},
+{'date': 'Wed Dec 11 2019 11:09:20.302 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 126
+    Basico()
+  module <module> line 118
+    self.doggie = Personagem(DOG, destino=self.cesta_esquerda, cena=cena, tit = "10kg", x=540, y=150, w=80,h=50)
+  module <module> line 36
+    self.vai = self.destino.entra_na_cesta(self)
+  module <module> line 99
+    for p in integrantes:
+NameError: name 'integrantes' is not defined
+'''},
