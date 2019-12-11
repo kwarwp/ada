a3a2117257902fe76c06afe73b5dd808f2d30c60
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-11 14:21:28 -0300

    Saved courtney/__log__.py @2019-12-11 17:21

--- courtney/__log__.py
+++ courtney/__log__.py
@@ -261,3 +261,23 @@ TypeError: __init__() got an unexpected keyword argument 'destino'
                                     ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Dec 11 2019 14:21:26.448 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 118
+    Controlador()
+  module <module> line 104
+    self.cesta_esquerda = Cesta(CEST, controlador=self, destino=self.base_topo, cena=self.base, x = 1)
+  module <module> line 57
+    self.fundo.vai = self.controlador.cesta_topo.mover
+AttributeError: 'Controlador' object has no attribute 'cesta_topo'
+'''},
