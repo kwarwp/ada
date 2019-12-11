639fffea882aa014fd137d072d4e3d4fd414bc59
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-11 14:33:52 -0300

    Saved courtney/__log__.py @2019-12-11 17:33

--- courtney/__log__.py
+++ courtney/__log__.py
@@ -381,3 +381,21 @@ AttributeError: 'Controlador' object has no attribute 'cesta_topo'
     frente.vai = self.controlador.cesta_topo.mover
 AttributeError: 'Controlador' object has no attribute 'cesta_topo'
 '''},
+{'date': 'Wed Dec 11 2019 14:33:48.899 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 103
+    Controlador()
+  module <module> line 92
+    self.cesta_esquerda.outro, self.cesta_direita.outro = self.cesta_direita.outro, self.cesta_esquerda.outro
+AttributeError: 'Cesta' object has no attribute 'outro'
+'''},
