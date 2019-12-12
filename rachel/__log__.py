7cfaa062a01cee87dbce25cce45f28fce4e8ad60
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-12 10:14:54 -0300

    Saved rachel/__log__.py @2019-12-12 13:14

--- rachel/__log__.py
+++ rachel/__log__.py
@@ -111,3 +111,23 @@ AttributeError: 'Predio' object has no attribute 'h'
     self.base1 = Plataforma(BASE, x=710, y=640, cena=cena, h=10)
 TypeError: __init__() got an unexpected keyword argument 'h'
 '''},
+{'date': 'Thu Dec 12 2019 10:14:52.636 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 121
+    Controlador()
+  module <module> line 114
+    self.menina = Personagem2(GIRL, destino=self.cesta.fundo, cena=cena)
+  module <module> line 51
+    super().__init__(imagem, cena=cena,  tit = "20kg", x=controlador.base1.x, y=y, w=60, h=80)
+NameError: name 'controlador' is not defined
+'''},
