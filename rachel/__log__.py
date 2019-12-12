4d7ba7aa931639c9410b0766b74a788fe7fa01d2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-12 10:06:29 -0300

    Saved rachel/__log__.py @2019-12-12 13:06

--- rachel/__log__.py
+++ rachel/__log__.py
@@ -53,3 +53,23 @@ AttributeError: 'Controlador' object has no attribute 'base_topo'
     self.cesta_esquerda = Cesta(CEST, controlador = self, destino=self.base_topo, cena=self.base, x=1)
 TypeError: __init__() got an unexpected keyword argument 'controlador'
 '''},
+{'date': 'Thu Dec 12 2019 10:06:28.68 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 121
+    Controlador()
+  module <module> line 113
+    self.doggie = Personagem(DOG, controlador=self, destino=self.cesta.fundo, cena=cena)
+  module <module> line 36
+    senf.entra(controlador.base1)
+NameError: name 'senf' is not defined
+'''},
