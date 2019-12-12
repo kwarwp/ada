1d44b455f6bdf9c4beb9e5c67f5f35ffada4cc92
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-12 11:28:04 -0300

    Saved rachel/__log__.py @2019-12-12 14:28

--- rachel/__log__.py
+++ rachel/__log__.py
@@ -232,3 +232,21 @@ SyntaxError: invalid syntax
     self.destino = self.controlador.cesta_topo
 AttributeError: 'Controlador' object has no attribute 'cesta_topo'
 '''},
+{'date': 'Thu Dec 12 2019 11:28:02.902 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 114
+    Controlador()
+  module <module> line 96
+    self.cesta_topo,
+AttributeError: 'Controlador' object has no attribute 'cesta_topo'
+'''},
