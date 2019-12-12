c184abec6aedfc7ffe1dbe910acd893a7b97311b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-12 11:03:16 -0300

    Saved rachel/__log__.py @2019-12-12 14:03

--- rachel/__log__.py
+++ rachel/__log__.py
@@ -212,3 +212,23 @@ AttributeError: 'Controlador' object has no attribute 'cesta'
                                                            ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Thu Dec 12 2019 11:03:14.672 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 109
+    Controlador()
+  module <module> line 94
+    self.doggie = Personagem(DOG, controlador=self, cena=cena, tit="10kg", h=50, w=80)
+  module <module> line 39
+    self.destino = self.controlador.cesta_topo
+AttributeError: 'Controlador' object has no attribute 'cesta_topo'
+'''},
