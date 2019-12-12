ca03ba1dbfdca068131ee8036ebef737b1d86c65
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-12 10:19:18 -0300

    Saved rachel/__log__.py @2019-12-12 13:19

--- rachel/__log__.py
+++ rachel/__log__.py
@@ -151,3 +151,21 @@ NameError: name 'controlador' is not defined
     super().__init__(imagem, cena=cena,  tit = "20kg", x=controlador.base1.x, y=y, w=60, h=80)
 NameError: name 'controlador' is not defined
 '''},
+{'date': 'Thu Dec 12 2019 10:19:16.991 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 121
+    Controlador()
+  module <module> line 105
+    self.base1 = Plataforma(BASE, x=710, y=540, cena=cena, h=10)
+TypeError: __init__() got an unexpected keyword argument 'h'
+'''},
