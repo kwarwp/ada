14ec710ed693b1cfb1fcace589f864cdd7b1fe16
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-13 18:13:52 -0300

    Saved rachel/main.py @2019-12-13 21:13

--- rachel/main.py
+++ rachel/main.py
@@ -7,6 +7,7 @@ __designer__ = "Marília Campos Galvão"
 __version__ = "05.12.2019"
 from _spy.vitollino.main import Cena, Texto, Elemento, INVENTARIO, STYLE, Musica
 
+STYLE ["width"] = 1320
 STYLE ["height"] = "600px"
 IGR = "https://i.imgur.com/"
 CEST, DOG, BASE, CENA, PRED = f"{IGR}qtw6IoO.png", f"{IGR}ek5NQYw.png", f"{IGR}7Wh2Px0.png", f"{IGR}zRGdYRp.gif", f"{IGR}vL9kR9Y.png"
@@ -20,8 +21,8 @@ class Predio(Elemento): #predio que  inicia bom e no fim fica queimado
 
 #Lado esquerdo
 class Plataforma(Elemento): #retangulo tranparente
-    def __init__(self, imagem, cena, x=390, y=0, h=200):
-        super().__init__(imagem, cena=cena, w=570,h=200, x=x, y=y)
+    def __init__(self, imagem, cena, x=390, y=0, h=200, w=570):
+        super().__init__(imagem, cena=cena, w=w,h=h, x=x, y=y)
         self.destino = self
         self.fundo = self
         self.nome = "base"
@@ -146,7 +147,7 @@ class Controlador:
         self.cesta_direita = Cesta(CEST, destino= self.base1, cena=self.base1, x=410, nome="direita", controlador=controlador)
         self.cesta_esquerda.outro, self.cesta_direita.outro = self.cesta_direita, self.cesta_esquerda
         
-        self.base_solo = Plataforma(BASE, x=460, y=350,cena=cena)
+        self.base_solo = Plataforma(BASE, x=430, y=350, w=250,cena=cena)
         
         self.cesta_topo, self.cesta_base = self.cesta_esquerda, self.cesta_direita
         
