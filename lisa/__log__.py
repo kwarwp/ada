2983e81b1898bbd16953f61430d5fd8612715902
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-14 16:00:20 -0300

    Saved lisa/__log__.py @2019-12-14 19:00

--- lisa/__log__.py
+++ lisa/__log__.py
@@ -6,3 +6,10 @@
                                                   ^
 SyntaxError: non-keyword arg after keyword arg
 '''},
+{'date': 'Sat Dec 14 2019 16:00:21.998 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 30
+  super().__init__(iamgem, cena=cena, tit = tit, x, y, w=80, h=50)
+                                                  ^
+SyntaxError: non-keyword arg after keyword arg
+'''},
