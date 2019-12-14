6808f1f9da27c5278a766756942fc84760710019
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-14 16:36:24 -0300

    Saved lisa/__log__.py @2019-12-14 19:36

--- lisa/__log__.py
+++ lisa/__log__.py
@@ -109,3 +109,21 @@ SyntaxError: non-keyword arg after keyword arg
                                                               ^
 SyntaxError: non-keyword arg after keyword arg
 '''},
+{'date': 'Sat Dec 14 2019 16:36:25.274 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 81
+    Basico()
+  module <module> line 71
+    self.cesta.outro, self.cesta2.outro = self.cesta2.outro, self.cesta.outro
+AttributeError: 'Veiculo' object has no attribute 'outro'
+'''},
