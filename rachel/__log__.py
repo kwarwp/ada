dc9fad49f80558714e8f7cd54aded91bf176f75d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-14 23:00:07 -0300

    Saved rachel/__log__.py @2019-12-15 02:00

--- rachel/__log__.py
+++ rachel/__log__.py
@@ -442,3 +442,21 @@ NameError: name 'todos' is not defined
     self.corda_telhado = Elemento(CORDA, x=520, y=-12,w=270,h=150, cena=cena, vai=self.sobe_desce)
 AttributeError: 'Controlador' object has no attribute 'sobe_desce'
 '''},
+{'date': 'Sat Dec 14 2019 23:00:05.923 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 183
+    Controlador()
+  module <module> line 158
+    self.corda_direita = Elemento(CORDA, x=660, y=130,w=280,h=180, cena=todos, vai=self.sobe_desce)
+NameError: name 'todos' is not defined
+'''},
