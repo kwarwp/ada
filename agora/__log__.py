a15fd3a97a324efc2395e31a79c70e5af6363e57
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-19 11:52:12 -0300

    Saved agora/__log__.py @2019-12-19 14:52

--- agora/__log__.py
+++ agora/__log__.py
@@ -28,3 +28,25 @@ SyntaxError: invalid syntax
     nomes = NOMES[:]
 NameError: name 'NOMES' is not defined
 '''},
+{'date': 'Thu Dec 19 2019 11:52:11.499 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 48
+    Application()
+  module <module> line 27
+    self.calendar()
+  module <module> line 42
+    [self.calendar <= tile  or tiler(wd, tile) for wd, tile in self.tiles]
+  module <module> line 34
+    shuffle(nomes)
+TypeError: 'str' object does not support item assignment
+'''},
