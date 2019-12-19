fbcdaaddfc00dfad612ff5b03676833e5dba3bb0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-19 11:54:17 -0300

    Saved agora/main.py @2019-12-19 14:54

--- agora/main.py
+++ agora/main.py
@@ -14,7 +14,7 @@ STYLE["height"]= "600px"
 IGR = "https://i.imgur.com/"
 CENA, RECT, SLATE, FACES = f"{IGR}kH1aOtS.jpg", f"{IGR}92GKogg.png", f"{IGR}pT6cuym.jpg", f"{IGR}utEu3Ib.png"
 NOME = """Adriana Ana Maria Sandra Juliana Antônio Carlos Francisco João José Bruna Camila Jéssica
-Letícia Amanda Lucas Luiz Mateus Guilherme Pedro"""
+Letícia Amanda Lucas Luiz Mateus Guilherme Pedro""".split()
 
 
 class Application:
@@ -30,7 +30,7 @@ class Application:
         ihour = "8:00 9:30 10:00 10:05 10:35 10:40 11:10 11:15 11:45 12:00".split()
         
         def tiler(wd, tile):
-            nomes = NOMES[:]
+            nomes = NOME[:]
             shuffle(nomes)
             tile <= html.BUTTON(wd.upper(), Class="tile is-child is-dark is-outlined is-inverted")
             [tile <= html.BUTTON(f"{hora} - {nome}", Class="tile is-child is-dark is-outlined is-inverted")
