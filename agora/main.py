c2403bff56e603868f8015746fe87438bfebd640
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-19 11:50:22 -0300

    Saved agora/main.py @2019-12-19 14:50

--- agora/main.py
+++ agora/main.py
@@ -7,11 +7,14 @@ __author__ = "Carlo Oliveira"
 __version__ = "19.12.18"
 from _spy.vitollino.main import Cena,Elemento,Texto, STYLE, INVENTARIO
 from browser import document, html
+from random import choice, shuffle
 
 STYLE["width"]=1350
 STYLE["height"]= "600px"
 IGR = "https://i.imgur.com/"
-CENA, RECT, SLATE = f"{IGR}kH1aOtS.jpg", f"{IGR}92GKogg.png", f"{IGR}pT6cuym.jpg"
+CENA, RECT, SLATE, FACES = f"{IGR}kH1aOtS.jpg", f"{IGR}92GKogg.png", f"{IGR}pT6cuym.jpg", f"{IGR}utEu3Ib.png"
+NOME = """Adriana Ana Maria Sandra Juliana Antônio Carlos Francisco João José Bruna Camila Jéssica
+Letícia Amanda Lucas Luiz Mateus Guilherme Pedro"""
 
 
 class Application:
@@ -24,10 +27,20 @@ class Application:
         self.calendar()
         
     def calendar(self):
+        ihour = "8:00 9:30 10:00 10:05 10:35 10:40 11:10 11:15 11:45 12:00".split()
+        
+        def tiler(wd, tile):
+            nomes = NOMES[:]
+            shuffle(nomes)
+            tile <= html.BUTTON(wd.upper(), Class="tile is-child is-dark is-outlined is-inverted")
+            [tile <= html.BUTTON(f"{hora} - {nome}", Class="tile is-child is-dark is-outlined is-inverted")
+            for hora, nome in zip(ihour, nomes)]
         self.contents.html = ""
         self.calendar = html.DIV(Class="tile is-ancestor")
-        self.tiles = [html.DIV(Class="tile is-ancestor" Id=f"weekday-{wd}") for wd in "seg ter qua qui sex".split()]
-        [self.calendar <= tile for tile in self.tiles]
+        weekdays = "seg ter qua qui sex".split()
+        self.tiles = [(wd, html.DIV(Class="tile is-parent is-vertical", Id=f"weekday-{wd}")) for wd in weekdays]
+        [self.calendar <= tile  or tiler(wd, tile) for wd, tile in self.tiles]
+        self.contents <= self.calendar
         
 
 
