32d274e752cc390defea68785d9187ea7a60e97c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-19 10:34:58 -0300

    Created agora/__log__.py @2019-12-19 13:34

---
+++ agora/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Thu Dec 19 2019 10:34:54.460 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 29
+  self.tiles = [html.DIV(Class="tile is-ancestor" Id=f"weekday-{wd}") for wd in "seg ter qua qui sex".split()]
+                                                   ^
+SyntaxError: invalid syntax
+'''},
