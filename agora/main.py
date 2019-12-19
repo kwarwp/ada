69b7350448be6a68efb3f68ea11d8a8423256a06
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-19 10:22:17 -0300

    Saved agora/main.py @2019-12-19 13:22

--- agora/main.py
+++ agora/main.py
@@ -6,6 +6,7 @@ Gerência de Recursos Educacionais
 __author__ = "Carlo Oliveira"
 __version__ = "19.12.18"
 from _spy.vitollino.main import Cena,Elemento,Texto, STYLE, INVENTARIO
+from browser import document, html
 
 STYLE["width"]=1350
 STYLE["height"]= "600px"
@@ -15,9 +16,15 @@ CENA, RECT, SLATE = f"{IGR}kH1aOtS.jpg", f"{IGR}92GKogg.png", f"{IGR}pT6cuym.jpg
 
 class Application:
     def __init__(self):
+        self.contents = document.querySelector("div.container")
         cena = Cena(CENA)
         cena.vai()
         slate = Elemento(SLATE, cena=cena)
+        self.calendar()
+        
+    def calendar(self):
+        self.contents.html = ""
+        
 
 
 if __name__ == "__main__":
