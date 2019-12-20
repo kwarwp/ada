2dfbacc02ea5321c509a1926fa6aa7ef28f15eb9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-20 12:41:48 -0300

    Saved agora/__log__.py @2019-12-20 15:41

--- agora/__log__.py
+++ agora/__log__.py
@@ -201,3 +201,21 @@ AdrianaTraceback (most recent call last):
     write=stream. write
 AttributeError: 'list' object has no attribute 'write'
 '''},
+{'date': 'Fri Dec 20 2019 12:41:47.377 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 139
+    main()
+  module <module> line 133
+    [print(a.nome, [h.nome for h in a.horarios]) for a in Turma.LISTA]
+AttributeError: 'Item' object has no attribute 'nome'
+'''},
