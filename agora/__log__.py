b10156c414372d28037f0e8d8b7815b3dd30cb7c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-22 07:53:55 -0300

    Saved agora/__log__.py @2019-12-22 10:53

--- agora/__log__.py
+++ agora/__log__.py
@@ -365,3 +365,21 @@ Traceback (most recent call last):
     [print(a.nome, [s.nome for s in a.turmas]) for a in Sala.LISTA]
 AttributeError: 'Item' object has no attribute 'nome'
 '''},
+{'date': 'Sun Dec 22 2019 07:53:54.895 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 98
+    SEMSALA = Sala(0)   
+  module <module> line 84
+    [turma.atualiza(self) for turma in turmas]
+TypeError: 'NoneType' object is not iterable
+'''},
