9075fa2be1699b49bbbe45fae9b1192a494f8b69
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-22 13:26:43 -0300

    Saved agora/__log__.py @2019-12-22 16:26

--- agora/__log__.py
+++ agora/__log__.py
@@ -515,3 +515,27 @@ TypeError: 'Turma' object is not iterable
     [turma.rege(self) for turma in self.turmas]
 TypeError: 'Turma' object is not iterable
 '''},
+{'date': 'Sun Dec 22 2019 13:26:42.15 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 214
+    main()
+  module <module> line 198
+    agora = Agora().limpa()
+  module <module> line 153
+    super().__init__(nome)
+  module <module> line 59
+    self.adiciona(self)
+  module <module> line 62
+    self.lista().append(item)
+AttributeError: 'Agora' object has no attribute 'lista'
+'''},
