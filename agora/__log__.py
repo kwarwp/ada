a85ee575db6360df1de0a45d8a9c6c7e36843698
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-22 07:09:58 -0300

    Saved agora/__log__.py @2019-12-22 10:09

--- agora/__log__.py
+++ agora/__log__.py
@@ -243,3 +243,21 @@ AttributeError: 'Item' object has no attribute 'nome'
     self.lista.append(Item)
 AttributeError: 'method' object has no attribute 'append'
 '''},
+{'date': 'Sun Dec 22 2019 07:09:57.975 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 137
+    main()
+  module <module> line 131
+    [print(a.nome, [h.nome for h in a.horarios]) for a in  Turma.LISTA]
+AttributeError: 'Item' object has no attribute 'nome'
+'''},
