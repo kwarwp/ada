80e5275a5a303e48358589cbb4f5e1e1deaa87c1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-22 08:03:55 -0300

    Saved agora/__log__.py @2019-12-22 11:03

--- agora/__log__.py
+++ agora/__log__.py
@@ -437,3 +437,21 @@ AttributeError: 'Item' object has no attribute 'nome'
     [print(a.nome, [h.nome for h in a.horarios]) for a in  t] #Turma.LISTA]
 AttributeError: 'Item' object has no attribute 'nome'
 '''},
+{'date': 'Sun Dec 22 2019 08:03:54.797 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 153
+    main()
+  module <module> line 146
+    [print(a.nome, a.horarios) for a in urma.LISTA]
+NameError: name 'urma' is not defined
+'''},
