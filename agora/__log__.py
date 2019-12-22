c02f606945211f256bb7fdab6bfd673d1d69a45a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-22 13:35:06 -0300

    Saved agora/__log__.py @2019-12-22 16:35

--- agora/__log__.py
+++ agora/__log__.py
@@ -597,3 +597,23 @@ AttributeError: 'Agora' object has no attribute 'entidade'
     self.entidade[tipo](**kwargs)
 TypeError: __init__() got an unexpected keyword argument 'hora'
 '''},
+{'date': 'Sun Dec 22 2019 13:35:05.276 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 212
+    main()
+  module <module> line 184
+    [agora.cria("Turma", nome=nome, horarios=[
+  module <module> line 173
+    self.entidade[tipo](**kwargs)
+KeyError: Turma
+'''},
