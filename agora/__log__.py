4e1799b5c5b71321509c3f7e85b6096869a27b76
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-22 08:59:55 -0300

    Saved agora/__log__.py @2019-12-22 11:59

--- agora/__log__.py
+++ agora/__log__.py
@@ -475,3 +475,23 @@ NameError: name 'urma' is not defined
     [turma.rege(self) for turma in self.turmas]
 TypeError: 'Turma' object is not iterable
 '''},
+{'date': 'Sun Dec 22 2019 08:59:54.121 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 174
+    main()
+  module <module> line 164
+    p = [Pessoa(nome, choice(t)) for nome in NOME]
+  module <module> line 75
+    [self.turmas.rege(self) for turma in self.turmas]
+TypeError: 'Turma' object is not iterable
+'''},
