7fc64990d7b71c4607e1e632ee33c5708ca0f922
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-23 13:03:39 -0300

    Saved agora/__log__.py @2019-12-23 16:03

--- agora/__log__.py
+++ agora/__log__.py
@@ -1120,3 +1120,25 @@ IndexError: list index out of range
     self.tiles = [(wd, html.DIV(Class="tile is-parent is-vertical", Id=f"weekday-{wd}"), html.BOX()) for wd in WDS]
 AttributeError: 'module' object has no attribute 'BOX'
 '''},
+{'date': 'Mon Dec 23 2019 13:03:38.268 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 273
+    main()
+  module <module> line 244
+    agora.mostra_disponibilidades()
+  module <module> line 215
+    tile = html.DIV(Class="tile is-parent is-vertical", Id=f"weekday-{wd}")
+  module <module> line 1
+    (wd)
+NameError: name 'wd' is not defined
+'''},
