11c7937b4b7fc96cca9976ab56535be21ecbf667
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-23 10:09:13 -0300

    Saved agora/__log__.py @2019-12-23 13:09

--- agora/__log__.py
+++ agora/__log__.py
@@ -888,3 +888,27 @@ Traceback (most recent call last):
     disp = [ inicio #(i, tuple(range(inicio, inicio+fim)))
 TypeError: 'int' object is not iterable
 '''},
+{'date': 'Mon Dec 23 2019 10:09:11.511 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''{'Sala': <class 'Sala'>, 'Turma': <class 'Turma'>, 'Pessoa': <class 'Pessoa'>, 'K': <class 'Fundamental2'>, 'J': <class 'Fundamental1'>, 'I': <class 'Infantil'>, 'U': <class 'Horario'>}
+[fund2, agr3a, agr3b, agr4, agr2a, nido1, agr2b, nido, mus1, mus2, mul, idi] [fund2, agr3a, agr3b, agr4, agr2a, nido1, agr2b, nido, mus1, mus2, mul, idi]
+Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 259
+    main()
+  module <module> line 230
+    print(agora.mostra_disponibilidades())
+  module <module> line 201
+    return tiler("seg", html.DIV())
+  module <module> line 195
+    disp = [ (i, p.disponibilidade) #(i, tuple(range(inicio, inicio+fim)))
+TypeError: 'int' object is not iterable
+'''},
