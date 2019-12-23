03eedb18b417942c46813bb06b0978035a92d177
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-23 10:12:00 -0300

    Saved montessori/main.py @2019-12-23 13:11

--- montessori/main.py
+++ montessori/main.py
@@ -7,3 +7,4 @@ class Application:
         "Agr21MG1 Agr21MG2 Agr22MG1 Agr22MG2"
         "Agr21TG1 Agr21TG2 Agr22TG1 Agr22TG2 Agr23TG1 Agr23TG2"
         "Agr3MG1 Agr3MG2 Agr3TG1 Agr3TG2 Agr3TG3 Agr4MG1 Agr4MG2 Agr4TG1 Agr4TG2"
+        "Agr5MG1 Agr5MG2 Agr5MG3 Agr6MG1 Agr6MG2 Agr6MG3"
