9e711288fd9cb64ccbfd3ff6a1378117db1fce93
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-23 09:49:15 -0300

    Saved montessori/main.py @2019-12-23 12:49

--- montessori/main.py
+++ montessori/main.py
@@ -2,4 +2,8 @@ from browser import document, html
 
 class Application:
     def __init__(self):
-        self.contents = document["pydiv"]    
+        self.contents = document["pydiv"]
+        "NidMG1 Nid1MG1 NidTG1 Nid1TG1"
+        "Agr21MG1 Agr21MG2 Agr22MG1 Agr22MG2"
+        "Agr21TG1 Agr21TG2 Agr22TG1 Agr22TG2 Agr23TG1 Agr23TG2"
+        "Agr3MG1 Agr3MG2 Agr3TG1 Agr3TG2 Agr3TG3 Agr4MG1 Agr4MG2 Agr4TG1 Agr4TG2"
