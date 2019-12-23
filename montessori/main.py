5a8b19dd89b4861158919d4aa625d9bb02dbccc5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-23 10:27:18 -0300

    Saved montessori/main.py @2019-12-23 13:27

--- montessori/main.py
+++ montessori/main.py
@@ -3,8 +3,12 @@ from browser import document, html
 class Application:
     def __init__(self):
         self.contents = document["pydiv"]
-        "NidMG1 Nid1MG1 NidTG1 Nid1TG1"
-        "Agr21MG1 Agr21MG2 Agr22MG1 Agr22MG2"
-        "Agr21TG1 Agr21TG2 Agr22TG1 Agr22TG2 Agr23TG1 Agr23TG2"
-        "Agr3MG1 Agr3MG2 Agr3TG1 Agr3TG2 Agr3TG3 Agr4MG1 Agr4MG2 Agr4TG1 Agr4TG2"
+        turmas="NidMG1 Nid1MG1 NidTG1 Nid1TG1"\
+        "Agr21MG1 Agr21MG2 Agr22MG1 Agr22MG2"\
+        "Agr21TG1 Agr21TG2 Agr22TG1 Agr22TG2 Agr23TG1 Agr23TG2"\
+        "Agr3MG1 Agr3MG2 Agr3TG1 Agr3TG2 Agr3TG3 Agr4MG1 Agr4MG2 Agr4TG1 Agr4TG2"\
         "Agr5MG1 Agr5MG2 Agr5MG3 Agr6MG1 Agr6MG2 Agr6MG3"
+        professores= """Claudia2 Priscila Cibele Lukas Alguém 
+        Larissa Paulo Dudu Sabrina Fabiana Carla1 Carla2 Rebeca kelly Eduardo Daniel 
+        Adriana Deni Gustavo Sol Rosane Gisele AnaRoxo Claudia1"""
+        
