7f29f2b819450c614020e7fc71ded79c5f5f26be
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-30 14:47:01 -0300

    Saved rachel/__log__.py @2019-12-30 17:47

--- rachel/__log__.py
+++ rachel/__log__.py
@@ -765,3 +765,23 @@ AttributeError: 'gameInicio' object has no attribute 'musA'
     self.musA.x = -1200
 AttributeError: 'gameInicio' object has no attribute 'musA'
 '''},
+{'date': 'Mon Dec 30 2019 14:47:00.432 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 276
+    gameInicio()
+  module <module> line 42
+    self.toca(self)
+  module <module> line 55
+    self.musA.x = -1200
+AttributeError: 'gameInicio' object has no attribute 'musA'
+'''},
