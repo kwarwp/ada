3b050d4bb269203395454c9320697cc997c0dfe0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-04-03 14:19:00 -0300

    Saved mickey/__log__.py @2020-04-03 17:19

--- mickey/__log__.py
+++ mickey/__log__.py
@@ -111,3 +111,25 @@ NameError: name 'cor_cauda' is not defined
     self.cauda_superior=self.desenha_o_corpo(cor_cauda,l=t*1/PHI**2,d= (t*1.5/PHI,t*1/PHI4))
 NameError: name 'cor_cauda' is not defined
 '''},
+{'date': 'Fri Apr 03 2020 14:18:59.713 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 120
+    cenario.principal()
+  module <module> line 38
+    mickey_feio=MickeyFeio()
+  module <module> line 44
+    self.esqueleto=compound(self.desenha())
+  module <module> line 48
+    self.corpo = self.desenha_o_corpo(self.cor_do_corpo,l=t)
+AttributeError: 'MickeyFeio' object has no attribute 'cor_do_corpo'
+'''},
