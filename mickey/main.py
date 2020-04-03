28fd8c162def11dae97b9981391d97e532c86d26
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-04-03 13:50:09 -0300

    Saved mickey/main.py @2020-04-03 16:50

--- mickey/main.py
+++ mickey/main.py
@@ -1,3 +1,10 @@
 # ada.mickey.main.py
+from browser import doc, html
 from _spy.vpython.main import *
-Glow("pydiv")
+from math import sqrt, pi
+doc['pydiv'].html=''
+_gs=Glow('pydiv')
+scene=canvas()
+scene.background= color.blue #"rgba(0, 0, 200, 0.5)"
+scene.width = 600
+scene.height = 600
