db88bcae7aaa773c4df07e2ba9107e447cc2658d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-11 14:41:20 -0300

    Created kellee/trigon.py @2020-07-11 14:41

---
+++ kellee/trigon.py
@@ -0,0 +1,13 @@
+# ada.kellee.trigon.py
+# SPDX-License-Identifier: GPL-3.0-or-later
+""" Projeto sem descrição, (mude esta linha).
+
+.. codeauthor:: Nome Sobrenome <mail@local.tipo>
+
+Changelog
+---------
+.. versionadded::    20.07
+        Descreva o que você adicionou no código.
+
+"""
+texto = "Vovó viu a maçã"
