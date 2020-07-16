0b251e706cf19acaa7ad48a99a999a9ce0d13650
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-15 22:52:49 -0300

    Saved roxanne/__log__.py @2020-07-16 01:52

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -326,3 +326,19 @@ AttributeError: 'module' object has no attribute 'case'
     class Testing(unittest.case.TestCase):
 AttributeError: 'module' object has no attribute 'case'
 '''},
+{'date': 'Wed Jul 15 2020 22:52:48.258 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 200
+    class Testing(unittest.case.TestCase):
+AttributeError: 'module' object has no attribute 'case'
+'''},
