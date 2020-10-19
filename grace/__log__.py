6b3b3e707ec5846fa42d030cb51638409c0ff8bb
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-10-19 19:20:17 -0300

    Saved grace/__log__.py @2020-10-19 22:20

--- grace/__log__.py
+++ grace/__log__.py
@@ -123,3 +123,19 @@ Traceback (most recent call last):
     raise NotImplementedError("Blocking functions like time.sleep() are not "
 NotImplementedError: Blocking functions like time.sleep() are not supported in the browser. Use functions in module browser.timer instead.
 '''},
+{'date': 'Mon Oct 19 2020 19:20:16.697 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 14
+    color('red', 'yellow')
+NameError: name 'color' is not defined
+'''},
