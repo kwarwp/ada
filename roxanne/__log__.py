0155d4782290ec4560b36c390e53cbf37150968c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-11-05 11:38:12 -0300

    Saved roxanne/__log__.py @2020-11-05 14:38

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -445,3 +445,21 @@ AttributeError: 'module' object has no attribute 'case'
     _gs = glow('pydiv')
 NameError: name 'glow' is not defined
 '''},
+{'date': 'Thu Nov 05 2020 11:38:11.637 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 260
+    main()
+  module <module> line 246
+    _gs = glow('pydiv')
+NameError: name 'glow' is not defined
+'''},
