700455b7cc7c58446370caab05b4dca5e2a4290e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-11-05 11:41:42 -0300

    Saved roxanne/__log__.py @2020-11-05 14:41

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -463,3 +463,24 @@ NameError: name 'glow' is not defined
     _gs = glow('pydiv')
 NameError: name 'glow' is not defined
 '''},
+{'date': 'Thu Nov 05 2020 11:41:41.490 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''{'_': []}
+Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 260
+    main()
+  module <module> line 256
+    TABULEIRO.jogada()
+  module <module> line 169
+    casa_da_jogada = choice(self.casas)  # choice(TABULEIRO)  # TABULEIRO[0]
+NameError: name 'choice' is not defined
+'''},
