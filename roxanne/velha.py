402ac6122dcc5cff5717006c4ea3b4042c49ee09
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-11-10 14:28:43 -0300

    Saved roxanne/velha.py @2020-11-10 17:28

--- roxanne/velha.py
+++ roxanne/velha.py
@@ -119,9 +119,14 @@ def main():
     cylinder(pos=(-50, -150, 20), size=(300, 20, 10), axis=(0, 1, 0), texture="https://i.imgur.com/X1mxjn2b.jpg") #"https://i.imgur.com/0ezlRYUb.jpg")
     cylinder(pos=(50, -150, 20), size=(300, 20, 10), axis=(0, 1, 0), texture="https://i.imgur.com/X1mxjn2b.jpg") #"https://i.imgur.com/0ezlRYUb.jpg")
     cylinder(pos=(0, 0, 20), size=(20, 70, 70), axis=(0, 0, 1), texture="https://i.imgur.com/IPcmVFnb.jpg")
-    box(pos=(100, 100, 20), size=(60, 60, 60), texture="https://i.imgur.com/d5fWaoab.jpg")
     box(pos=(-100, -100, 20), size=(20, 70, 20), axis=(1, 1, 0), texture="https://i.imgur.com/d5fWaoab.jpg")
     box(pos=(-100, -100, 20), size=(70, 20, 20), axis=(1, 1, 0), texture="https://i.imgur.com/d5fWaoab.jpg")
+    cylinder(pos=(100, 100, 20), size=(20, 70, 70), axis=(0, 0, 1), texture="https://i.imgur.com/IPcmVFnb.jpg")
+    box(pos=(-100, 100, 20), size=(20, 70, 20), axis=(1, 1, 0), texture="https://i.imgur.com/d5fWaoab.jpg")
+    box(pos=(-100, 100, 20), size=(70, 20, 20), axis=(1, 1, 0), texture="https://i.imgur.com/d5fWaoab.jpg")
+    cylinder(pos=(0, 100, 20), size=(20, 70, 70), axis=(0, 0, 1), texture="https://i.imgur.com/IPcmVFnb.jpg")
+    box(pos=(0, -100, 20), size=(20, 70, 20), axis=(1, 1, 0), texture="https://i.imgur.com/d5fWaoab.jpg")
+    box(pos=(0, -100, 20), size=(70, 20, 20), axis=(1, 1, 0), texture="https://i.imgur.com/d5fWaoab.jpg")
 
 
 if __name__ == "__main__":
