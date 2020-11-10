d643fde34d3dacf3f1a777e37a28f86422a3712c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-11-10 14:01:53 -0300

    Saved roxanne/velha.py @2020-11-10 17:01

--- roxanne/velha.py
+++ roxanne/velha.py
@@ -114,8 +114,8 @@ def main():
     cena.width = 900
     cena.height = 600
     box(pos=(0, 0, 0), size=(300, 300, 30), texture="https://i.imgur.com/0ezlRYUb.jpg")
-    cylinder(pos=(0, 0, 30), size=(80, 80, 30), axis=(0, 0, 1), texture="https://i.imgur.com/IPcmVFnb.jpg")
-    box(pos=(100, 100, 30), size=(80, 80, 30), texture="https://i.imgur.com/d5fWaoab.jpg")
+    cylinder(pos=(0, 0, 30), size=(30, 60, 60), axis=(0, 0, 1), texture="https://i.imgur.com/IPcmVFnb.jpg")
+    box(pos=(100, 100, 30), size=(60, 60, 60), texture="https://i.imgur.com/d5fWaoab.jpg")
 
 
 if __name__ == "__main__":
