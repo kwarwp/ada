22e29a4053b3c5177ad1f4baadf13a623d69647f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-12-11 11:39:00 -0300

    Saved roxanne/__log__.py @2020-12-11 14:39

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -522,3 +522,19 @@ Exception: <SyntaxError: missing ] in computed property name>
     class Testing(unittest.case.TestCase):
 AttributeError: 'module' object has no attribute 'case'
 '''},
+{'date': 'Fri Dec 11 2020 11:38:56.920 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 200
+    class Testing(unittest.case.TestCase):
+AttributeError: 'module' object has no attribute 'case'
+'''},
