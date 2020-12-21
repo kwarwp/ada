a37536100bd4b0d0fd80c079c12f28fe299a4958
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-12-21 16:08:56 -0300

    Created roxanne/nanite.py @2020-12-21 19:08

---
+++ roxanne/nanite.py
@@ -0,0 +1,33 @@
+# ada.roxanne.nanite.py
+# SPDX-License-Identifier: GPL-3.0-or-later
+""" Projeto sem descrição, (mude esta linha).
+
+.. codeauthor:: Nome Sobrenome <mail@local.tipo>
+
+Changelog
+---------
+.. versionadded::    20.12
+        Descreva o que você adicionou no código.
+
+"""
+class Nanite:
+    CENAS = """https://imgur.com/q308L34
+https://imgur.com/v1CVsjO
+https://imgur.com/7cXtyqA
+https://imgur.com/jHnybc0
+https://imgur.com/WyiiE0T
+https://imgur.com/KZoXpRI
+https://imgur.com/iILubFM
+https://imgur.com/regZ51L
+https://imgur.com/fUDoUc0
+https://imgur.com/CljPeMU
+https://imgur.com/bDqM8oP
+https://imgur.com/SghQp9q
+https://imgur.com/o8Hj2F4
+https://imgur.com/ctDxOKA
+https://imgur.com/gWvkAiR
+https://imgur.com/SJ428kZ
+https://imgur.com/kQWpngJ"""
+    
+if __name__ == "__main__":
+    print(x.split("/")[-1] fox x in Nanite.CENAS.split())
