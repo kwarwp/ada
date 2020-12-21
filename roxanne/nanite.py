04a5d4732e3f7ad6f9ff201e1517229692782ef6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-12-21 16:57:53 -0300

    Saved roxanne/nanite.py @2020-12-21 19:57

--- roxanne/nanite.py
+++ roxanne/nanite.py
@@ -15,16 +15,17 @@ class Nanite:
     'q308L34', 'v1CVsjO', '7cXtyqA', 'jHnybc0', 'WyiiE0T', 'KZoXpRI',
     'iILubFM', 'regZ51L', 'fUDoUc0', 'CljPeMU', 'bDqM8oP', 'SghQp9q',
     'o8Hj2F4', 'ctDxOKA', 'gWvkAiR', 'SJ428kZ', 'kQWpngJ']
-    def __init__(j):
-        self.quadrantes = """
-occipital CO 	frontal CF 	parietal CP
-endocrino menor EE 	respiratório RE 	sensorial SN
-tegumentar TE 	digestório DI 	cardiovascular CA
-torso muscular TO 	endócrino EC 	endócrino maior EA
-membro muscular MM 	excretor EX 	reprodutor RX
-esquelético menor SE 	esquelético SC 	esquelético maior SA
-"""
+    def __init__(self, j):
+        self.setores = [
+        ['CO', 'CF', 'CP'],
+        ['EE', 'RE', 'SN'],
+        ['TE', 'DI', 'CA'],
+        ['TO', 'EC', 'EA'],
+        ['MM', 'EX', 'RX'],
+        ['SE', 'SC', 'SA']]
+        self.quadrantes = "OV ON NO NV, OS O N NL, SO S L LN, SV SL LS LV"
     
 if __name__ == "__main__":
     from _spy.vitollino.main import Jogo
-    print([[x[-2:] for x in l.split(" 	")] for l in Nanite(j).quadrantes.split()])
+    import re
+    print([re.findall(r" ([A-Z]+)",l) for l in Nanite(Jogo).quadrantes.split("\n")])
