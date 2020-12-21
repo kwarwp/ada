78a1e11b8c168d85e5ae1201de4258c862da34f6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-12-21 16:35:02 -0300

    Saved roxanne/__log__.py @2020-12-21 19:35

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -582,3 +582,19 @@ SyntaxError: invalid syntax
                  ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Dec 21 2020 16:35:01.925 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 30
+    print([[x[-2:] for x in l.split(" 	")] for l in Nanite(j).quadrantes.split()])
+NameError: name 'j' is not defined
+'''},
