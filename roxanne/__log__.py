d9f998b27bc9d15f729dd3098de1032bd2690557
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-12-21 16:43:57 -0300

    Saved roxanne/__log__.py @2020-12-21 19:43

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -630,3 +630,19 @@ TypeError: __init__() takes 1 positional argument but more were given
     print([[re.findall(" ([A-Z]{2})")] for l in Nanite(Jogo).quadrantes.split()])
 TypeError: findall missing 1 positional argument: 'string'
 '''},
+{'date': 'Mon Dec 21 2020 16:43:56.549 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 31
+    print([[re.findall(" ([A-Z]{2})"),l] for l in Nanite(Jogo).quadrantes.split()])
+TypeError: findall missing 1 positional argument: 'string'
+'''},
