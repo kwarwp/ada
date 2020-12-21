881a824df8fdc3dc6379863578e44b8587c3b678
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-12-21 18:54:43 -0300

    Saved roxanne/__log__.py @2020-12-21 21:54

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -664,3 +664,21 @@ TypeError: findall missing 1 positional argument: 'string'
     self.mundo.update({setor+quadrante:{
 NameError: name 'p_cardeal' is not defined
 '''},
+{'date': 'Mon Dec 21 2020 18:54:42.527 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 51
+    print(Nanite(Jogo).mundo)
+  module <module> line 44
+    self.mundo.update({setor+quadrante:{
+AttributeError: 'Nanite' object has no attribute 'IMGUR'
+'''},
