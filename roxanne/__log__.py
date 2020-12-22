883528b862d27b3d2863b283d1a9c49e0eb4e942
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-12-22 16:21:00 -0300

    Saved roxanne/__log__.py @2020-12-22 19:21

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -736,3 +736,23 @@ NameError: name 'minilines' is not defined
     nome_sala, nome_norte, nome_leste = quad, self.miniline[y-1][x], self.miniline[y][x-1]
 AttributeError: 'Nanite' object has no attribute 'miniline'
 '''},
+{'date': 'Tue Dec 22 2020 16:20:59.838 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 67
+    n = Nanite(JOGO)
+  module <module> line 56
+    self.mapear(nome_sala, nome_norte, nome_leste)
+  module <module> line 59
+    sala.norte.meio = norte.norte
+AttributeError: 'str' object has no attribute 'norte'
+'''},
