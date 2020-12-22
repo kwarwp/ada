b327c9509a614206429d8f5be54d78d1983ea455
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-12-22 16:22:44 -0300

    Saved roxanne/__log__.py @2020-12-22 19:22

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -756,3 +756,23 @@ AttributeError: 'Nanite' object has no attribute 'miniline'
     sala.norte.meio = norte.norte
 AttributeError: 'str' object has no attribute 'norte'
 '''},
+{'date': 'Tue Dec 22 2020 16:22:43.740 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 68
+    n = Nanite(JOGO)
+  module <module> line 56
+    self.mapear(nome_sala, nome_norte, nome_leste)
+  module <module> line 59
+    sala, norte, leste = getattr(self.salas, sala), getattr(self.salas, norte), getattr(self.salas, leste)
+AttributeError: 'Sala' object has no attribute 'CPNV'
+'''},
