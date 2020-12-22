9384f0c835d0b0a4b55848e6fd65e216ee721c6a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-12-22 16:19:39 -0300

    Saved roxanne/__log__.py @2020-12-22 19:19

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -700,3 +700,21 @@ AttributeError: 'Nanite' object has no attribute 'IMGUR'
     self.mundo.update({setor+quadrante:{
 TypeError: unhashable type: 'list'
 '''},
+{'date': 'Tue Dec 22 2020 16:19:39.15 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 68
+    n = Nanite(JOGO)
+  module <module> line 54
+    for y, line in enumerate(minilines):
+NameError: name 'minilines' is not defined
+'''},
