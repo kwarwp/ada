cd4031c8854896ecefba40aca54e42d7ba9c6475
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-05-06 09:04:44 -0300

    Created carinhas/__log__.py @2021-05-06 12:04

---
+++ carinhas/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Thu May 06 2021 09:04:42.938 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 103
+  [self.j.a(ACTIV.format(f"{grupo}_{color[oid//8]}_{oid % 8}") w=w, h=w, x=w*oid, cena=cena)
+                                                                ^
+SyntaxError: invalid syntax
+'''},
