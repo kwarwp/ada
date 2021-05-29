7a208a28d4c6b53f63138d84b70b62a904841421
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-05-29 17:08:56 -0300

    Saved kellee/__log__.py @2021-05-29 20:08

--- kellee/__log__.py
+++ kellee/__log__.py
@@ -207,3 +207,19 @@ AttributeError: 'module' object has no attribute 'case'
     class Testing(unittest.case.TestCase):
 AttributeError: 'module' object has no attribute 'case'
 '''},
+{'date': 'Sat May 29 2021 17:08:56.657 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 43
+    from roxanne.main import test
+ImportError: cannot import name 'test'
+'''},
