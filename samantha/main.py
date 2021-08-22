0ed599bef25f92f7e42a36ad6ef46f9fb4ff2de0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-08-22 16:45:46 -0300

    Saved samantha/main.py @2021-08-22 19:45

--- samantha/main.py
+++ samantha/main.py
@@ -2,16 +2,22 @@ from _spy.vitollino.main import Cena, Texto, Elemento, STYLE
 
 ################################################TAKE1##########################################################
 
-STYLE['width'] = 1000
-STYLE['height'] = "600px"
+STYLE['width'] = 1200
+STYLE['height'] = "800px"
 
 entrada = "https://media-cdn.tripadvisor.com/media/photo-s/07/94/9a/9a/primeiro-portao-instalado.jpg"
 pixacao = "https://interartive.org/wp-content/uploads/pixo1.jpg"
 lagodastartarugas = "https://image.slidesharecdn.com/jardimbotnico-rj-121110093756-phpapp02/95/jardim-botnico-rj-27-638.jpg?cb=1352540459"
 portao = "https://activufrj.nce.ufrj.br/file/pedropeclat/1528988110963.png?disp=inline"
+ALPHA = "https://imgur.com/bePU7Xn.jpg"
+SPD = "https://imgur.com/r4bpNmn.png"  # https://i.imgur.com/f0C5HdL.png"
 def qualquernome():
 
     cena1 = Cena(img = entrada)
+    elm = Elemento(ALPHA, w=1200, h=800, cena=cena1)
+    spd = Elemento(SPD, x=100, y=100, cena=cena1)
+    elm.siz = (2400, 2400)
+    '''
     elemento1 = Elemento(img = pixacao, style = dict(left=0, top=95,height= '505px', width= 250,bottom=20,))
     elemento2 = Elemento(img = portao, style = dict(left= 215, top=0,height= '600px', width= 575,bottom=0))
     lago = Cena(img = lagodastartarugas)
@@ -19,7 +25,7 @@ def qualquernome():
     elemento2.entra(cena1)
     txtcena1 = Texto(cena1, "O portao esta trancado")
     elemento2.vai = txtcena1.vai
-    elemento1.vai=lago.vai
+    elemento1.vai=lago.vai'''
     cena1.vai()
 
 qualquernome()
