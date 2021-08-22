f97de39a871c7d7bb938dfc4fe5af357ddbd0aeb
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-08-22 18:01:49 -0300

    Saved samantha/main.py @2021-08-22 21:01

--- samantha/main.py
+++ samantha/main.py
@@ -10,12 +10,15 @@ pixacao = "https://interartive.org/wp-content/uploads/pixo1.jpg"
 lagodastartarugas = "https://image.slidesharecdn.com/jardimbotnico-rj-121110093756-phpapp02/95/jardim-botnico-rj-27-638.jpg?cb=1352540459"
 portao = "https://activufrj.nce.ufrj.br/file/pedropeclat/1528988110963.png?disp=inline"
 ALPHA = "https://imgur.com/bePU7Xn.jpg"
-SPD = "https://imgur.com/r4bpNmn.png"  # https://i.imgur.com/f0C5HdL.png"
+SPD = "https://imgur.com/jnqSHOh.gif"  # https://i.imgur.com/f0C5HdL.png"
+FIR = "https://imgur.com/BJCG0jB.gif"
+WAT = "https://imgur.com/IqLSYQ9.gif"
 def qualquernome():
 
     cena1 = Cena(img = entrada)
     elm = Elemento(ALPHA, w=1200, h=800, cena=cena1)
-    spd = Elemento(SPD, x=100, y=100, cena=cena1)
+    spd = Elemento(SPD, x=200, y=100, w=200, h=100, cena=cena1)
+    fir = Elemento(WAT, x=0, y=0, w=1200, h=800, o=0.7, cena=cena1)
     elm.siz = (2400, 2400)
     '''
     elemento1 = Elemento(img = pixacao, style = dict(left=0, top=95,height= '505px', width= 250,bottom=20,))
