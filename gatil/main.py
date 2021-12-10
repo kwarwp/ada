245e19003793d57cb6be59db7940f9bfd2f895ae
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-12-10 12:42:44 -0300

    Saved gatil/main.py @2021-12-10 15:42

--- gatil/main.py
+++ gatil/main.py
@@ -1,1 +1,11 @@
 # ada.gatil.main.py
+from _spy.vitollino.main import Cena, STYLE
+STYLE.update(width=1350, height="800px")
+GATIL_MOS = "https://i.imgur.com/5ZISX93.jpg"
+
+
+class Gatil(Cena):
+    def __init__(self, img, x=0, y=0, w=1350, h=800):
+        super().__init__(img)
+        
+Cena(GATIL_MOS).vai()
