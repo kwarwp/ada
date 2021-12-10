8b081e4d6a2942bc59d5b1d5d54101d89d8ec00c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-12-10 13:29:35 -0300

    Created gatil/__log__.py @2021-12-10 16:29

---
+++ gatil/__log__.py
@@ -0,0 +1,7 @@
+
+{'date': 'Fri Dec 10 2021 13:29:33.421 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+RuntimeError: too much recursion
+  module '$exec_2098' line 27
+        self.vai()
+'''},
