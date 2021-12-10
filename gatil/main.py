ce5526763ddbb0122cdf77e949bed549e49aaee4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-12-10 13:30:07 -0300

    Saved gatil/main.py @2021-12-10 16:30

--- gatil/main.py
+++ gatil/main.py
@@ -1,11 +1,31 @@
 # ada.gatil.main.py
-from _spy.vitollino.main import Cena, STYLE
+from _spy.vitollino.main import Cena, STYLE. Elemento
+from browser import html
 STYLE.update(width=1350, height="800px")
 GATIL_MOS = "https://i.imgur.com/5ZISX93.jpg"
+GATIL_POR = "https://i.imgur.com/Ockz2ae.png"
 
+class Abrigo:
+    GW = 1350
+    GH = 800
+    IW = 4000
+    IH = 1000
+    DW = (IW-GW)//6
+    def __init__(self):
+        super().__init__(img)
 
 class Gatil(Cena):
-    def __init__(self, img, x=0, y=0, w=1350, h=800):
+    def __init__(self, img, x=0, y=0):
         super().__init__(img)
-        
-Cena(GATIL_MOS).vai()
+        #self.elt = html.DIV(style=STYLE, )
+        self.dx, self.dy = x*Abrigo.DW, y*200, 
+        self.cena = c = Elemento(img, x=0, y=0, w=1350, h=800, cena=self)
+        # self.elt.style.width = w
+        c.siz = (Abrigo.IW, Abrigo.IH)
+        c.pos = (-self.dx, -self.dy)
+    def vai(self):
+        super().vai()
+        c0 = Elemento(img, x=140, y=340, w=200, h=200, cena=self)
+        p0 = Elemento(GATIL_POR, x=100, y=300, w=300, h=300, cena=self)
+    
+Gatil(GATIL_MOS).vai()
