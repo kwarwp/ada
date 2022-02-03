3658e44f152867d32cb8960cb2aa25189b091bcc
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-02-03 18:33:28 -0300

    Saved gatil/main.py @2022-02-03 21:33

--- gatil/main.py
+++ gatil/main.py
@@ -6,15 +6,19 @@ GATIL_MOS = "https://i.imgur.com/5ZISX93.jpg"
 GATIL_POR = "https://i.imgur.com/Ockz2ae.png"
 PETUNIO = "https://i.imgur.com/2KeouVt.png"
 IM = "https://i.imgur.com/{}.jpg"
+IMP = "https://i.imgur.com/{}.png"
 SA = "VV1xbBG SEblwJG JVXK8gA nyly8wp".split()
 SB = "NqNXbr4 2QdVrAj jvM9BQC 2KZpwVf".split()
 WIND = "https://imgur.com/3LJN7lT.gif"
 GATAR = "https://imgur.com/WcrEeLj.png"
-STRAY = "https://i.imgur.com/hlA5iCO.png"
+STRAY = "qooCvWD SfGf1gv hlA5iCO RWc9j9Q FPWh9Nt".split()
+STRAYS = "pMEwzkz llqWlNC LAUEEAj KA7r39r 8nBLnkF".split()
 PIX = "https://imgur.com/0OoP7wt.jpg"
 RAIN = "https://cdn.wallpapersafari.com/44/7/bvCdLK.gif"
 STORM = "https://media.giphy.com/media/xT9GEDhzERbjDD15O8/giphy.gif"
 FLOOD = "https://media.giphy.com/media/3HoB7BmMnKMdq/giphy.gif"
+HAIL = "https://i.imgur.com/ZZ8nEkV.gif"
+HALO = "https://i.imgur.com/XDuFNZw.png"
 NO = []
 class Abrigo:
     GW = 1350
@@ -72,9 +76,10 @@ class Gatil(Cena):
         sala_b = Sala(*[IM.format(lnk) for lnk in SB])
         lab0 = Labirinto(sala_a, sala_b, sala_b, sala_b, sala_b)
         # self.cena = c = Elemento(WIND, x=0, y=0, w=1350, h=800, o=0.4, cena=sala_b.norte)
-        self.rain = r = Elemento(FLOOD, x=0, y=0, w=1350, h=800, o=0.4, cena=sala_b.norte)
+        # self.rain = r = Elemento(HAIL, x=0, y=0, w=1350, h=800, o=0.4, cena=sala_b.norte)
         self.hero = h = Elemento(PETUNIO, x=200, y=550, w=130, h=100, cena=sala_b.norte)
-        self.stray = s = Elemento(STRAY, x=600, y=550, w=130, h=100, cena=sala_b.norte)
+        self.stray = s = Elemento(IMP.format(STRAY[0]), x=600, y=550, w=130, h=100, cena=sala_b.norte)
+        # self.strays = z = Elemento(IMP.format(STRAYS[0]), x=300, y=50, w=650, h=650, cena=sala_b.norte)
         self.gatar = g = Elemento(GATAR, x=200, y=550, w=100, h=100)
         self.pix = p = Elemento(PIX, x=200, y=550, w=100, h=100)
         INV.inicia()
