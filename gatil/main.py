b37cb845612b1099441e78bff0462530071b54a9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-02-03 17:54:51 -0300

    Saved gatil/main.py @2022-02-03 20:54

--- gatil/main.py
+++ gatil/main.py
@@ -1,5 +1,5 @@
 # ada.gatil.main.py
-from _spy.vitollino.main import Cena, STYLE, Elemento, Sala, Labirinto
+from _spy.vitollino.main import Cena, STYLE, Elemento, Sala, Labirinto, INVENTARIO as INV
 from browser import html
 STYLE.update(width=1350, height="800px")
 GATIL_MOS = "https://i.imgur.com/5ZISX93.jpg"
@@ -9,6 +9,13 @@ IM = "https://i.imgur.com/{}.jpg"
 SA = "VV1xbBG SEblwJG JVXK8gA nyly8wp".split()
 SB = "NqNXbr4 2QdVrAj jvM9BQC 2KZpwVf".split()
 WIND = "https://imgur.com/3LJN7lT.gif"
+GATAR = "https://imgur.com/WcrEeLj.png"
+STRAY = "https://i.imgur.com/hlA5iCO.png"
+PIX = "https://imgur.com/0OoP7wt.jpg"
+RAIN = "https://cdn.wallpapersafari.com/44/7/bvCdLK.gif"
+STORM = "https://media.giphy.com/media/xT9GEDhzERbjDD15O8/giphy.gif"
+FLOOD = "https://media.giphy.com/media/3HoB7BmMnKMdq/giphy.gif"
+NO = []
 class Abrigo:
     GW = 1350
     GH = 800
@@ -17,6 +24,32 @@ class Abrigo:
     DW = (IW-GW)//6
     def __init__(self):
         super().__init__(img)
+
+class Rua(Cena):
+    def __init__(self, img, lixo=NO,  gente=NO,  video=NO,  stray=NO):
+        super().__init__(img)
+        self.img = img
+        #self.elt = html.DIV(style=STYLE, )
+        self.dx, self.dy = x*Abrigo.DW, y*200, 
+        self.cena = c = Elemento(img, x=0, y=0, w=1350, h=800, cena=self)
+        self.hero = h = Elemento(PETUNIO, x=400, y=350, w=250, h=200, cena=self)
+        # self.elt.style.width = w
+        c.siz = (Abrigo.IW, Abrigo.IH)
+        c.pos = (-self.dx, -self.dy)
+    def vai_(self):
+        super().vai()
+        c0 = Elemento(self.img, x=140, y=340, w=200, h=200, cena=self)
+        p0 = Elemento(GATIL_POR, x=100, y=300, w=300, h=300, cena=self)
+    def vai(self):
+        sala_a = Sala(*[IM.format(lnk) for lnk in SA])
+        sala_a.norte.vai()
+        sala_b = Sala(*[IM.format(lnk) for lnk in SB])
+        lab0 = Labirinto(sala_a, sala_b, sala_b, sala_b, sala_b)
+        self.cena = c = Elemento(WIND, x=0, y=0, w=1350, h=800, o=0.4, cena=sala_b.norte)
+        self.hero = h = Elemento(PETUNIO, x=200, y=550, w=130, h=100, cena=sala_b.norte)
+        self.gatar = g = Elemento(GATAR, x=200, y=550, w=100, h=100)
+        INV.bota(g)
+        sala_b.norte.vai()
 
 class Gatil(Cena):
     def __init__(self, img, x=0, y=0):
@@ -38,8 +71,15 @@ class Gatil(Cena):
         sala_a.norte.vai()
         sala_b = Sala(*[IM.format(lnk) for lnk in SB])
         lab0 = Labirinto(sala_a, sala_b, sala_b, sala_b, sala_b)
+        # self.cena = c = Elemento(WIND, x=0, y=0, w=1350, h=800, o=0.4, cena=sala_b.norte)
+        self.rain = r = Elemento(FLOOD, x=0, y=0, w=1350, h=800, o=0.4, cena=sala_b.norte)
         self.hero = h = Elemento(PETUNIO, x=200, y=550, w=130, h=100, cena=sala_b.norte)
-        self.cena = c = Elemento(WIND, x=0, y=0, w=1350, h=800, o=0.4, cena=sala_b.norte)
+        self.stray = s = Elemento(STRAY, x=600, y=550, w=130, h=100, cena=sala_b.norte)
+        self.gatar = g = Elemento(GATAR, x=200, y=550, w=100, h=100)
+        self.pix = p = Elemento(PIX, x=200, y=550, w=100, h=100)
+        INV.inicia()
+        INV.bota(g)
+        INV.bota(p)
         sala_b.norte.vai()
     
 Gatil(GATIL_MOS).vai()
