77c902dc3a35bcf121e75d67c9e3da0547c9a664
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-02-03 18:45:32 -0300

    Saved gatil/__log__.py @2022-02-03 21:45

--- gatil/__log__.py
+++ gatil/__log__.py
@@ -29,3 +29,21 @@ RuntimeError: too much recursion
     c0 = Elemento(img, x=140, y=340, w=200, h=200, cena=self)
 NameError: name 'img' is not defined
 '''},
+{'date': 'Thu Feb 03 2022 18:45:31.150 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 45
+    class Sala(SalaVit):
+  module <module> line 46
+    def __init__(self, n=NADA, l=NADA, s=NADA, o=NADA, nome='', **kwargs):
+NameError: name 'NADA' is not defined
+'''},
