a1a4af6afd4b3bfb6545171d67ee00920c58da65
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-02-07 17:57:51 -0300

    Saved gatil/__log__.py @2022-02-07 20:57

--- gatil/__log__.py
+++ gatil/__log__.py
@@ -247,3 +247,23 @@ SyntaxError: invalid syntax
     raise error. HTTPError('file not found')
 HTTPError: file not found
 '''},
+{'date': 'Mon Feb 07 2022 17:57:51.66 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 187
+    Gatil(GATIL_MOS).vai()
+  module <module> line 185
+    Thrash(x.elt, sala_b.norte.elt)
+  module <module> line 111
+    obj = svg.use(href=f"#{label}", x=5000 - 400*randint(0,9), y=300 -r200*randint(0,9), width=50, height=50,
+NameError: name 'r200' is not defined
+'''},
