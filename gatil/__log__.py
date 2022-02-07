26293c68d331a928fc19150f487c9a7482d4f2b9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-02-07 16:18:30 -0300

    Saved gatil/__log__.py @2022-02-07 19:18

--- gatil/__log__.py
+++ gatil/__log__.py
@@ -223,3 +223,27 @@ NameError: name 'circle' is not defined
                                   ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Feb 07 2022 16:18:30.125 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 185
+    Gatil(GATIL_MOS).vai()
+  module <module> line 183
+    Thrash(x.elt, sala_b.norte.elt)
+  module <module> line 85
+    cache = self.create_script_tag(LIXAO)
+  module <module> line 122
+    _fp = urllib.request.urlopen(src)
+  module urllib.request line 37
+    raise error. HTTPError('file not found')
+HTTPError: file not found
+'''},
