a1f43f233bd3e26c502dd7bf3a5aef0c3b54b888
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-02-09 19:22:00 -0300

    Saved gatil/__log__.py @2022-02-09 22:22

--- gatil/__log__.py
+++ gatil/__log__.py
@@ -281,3 +281,23 @@ SyntaxError: invalid syntax
                                               ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Feb 09 2022 19:21:59.982 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 200
+    Gatil(GATIL_MOS).vai()
+  module <module> line 198
+    Thrash(x.elt, sala_b.norte)
+  module <module> line 93
+    elt <= s
+NameError: name 's' is not defined
+'''},
