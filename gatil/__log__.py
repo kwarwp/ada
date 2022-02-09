45f0ed8a51a47492b7791820d59f3d48898af25e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-02-09 20:15:38 -0300

    Saved gatil/__log__.py @2022-02-09 23:15

--- gatil/__log__.py
+++ gatil/__log__.py
@@ -308,3 +308,21 @@ NameError: name 's' is not defined
                                 ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Feb 09 2022 20:15:37.933 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 208
+    Gatil(GATIL_MOS).vai()
+  module <module> line 164
+    self.trash = Thrash(x.elt, sala_b.norte)
+AttributeError: 'int' object has no attribute 'elt'
+'''},
