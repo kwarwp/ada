0c6490354378b1ed9ea8b1d3399eeb2e670b4d4f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-02-09 23:09:12 -0300

    Saved angie/__log__.py @2022-02-10 02:09

--- angie/__log__.py
+++ angie/__log__.py
@@ -65,3 +65,21 @@ AttributeError: 'Retangulos' object has no attribute 'rubish'
     Retangulos().make_rects(1000, 800)
 TypeError: make_rects() takes 2 positional arguments but more were given
 '''},
+{'date': 'Wed Feb 09 2022 23:09:11.434 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 27
+    Retangulos().make_rects(1000, 800)
+  module <module> line 17
+    r = svg.rect(
+NameError: name 'randint' is not defined
+'''},
