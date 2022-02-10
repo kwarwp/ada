2baafebb03bac977a2bf49fe550fe3408e6a085c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-02-09 23:12:47 -0300

    Saved angie/__log__.py @2022-02-10 02:12

--- angie/__log__.py
+++ angie/__log__.py
@@ -119,3 +119,21 @@ IndexError: list index out of range
     self.rects = Elemento('', x=0, y=0, w=1350, height=800, cena=self.cena)
 NameError: name 'Elemento' is not defined
 '''},
+{'date': 'Wed Feb 09 2022 23:12:46.718 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 29
+    Retangulos().make_rects(1000, 800)
+  module <module> line 12
+    self.rects = Elemento('', x=0, y=0, w=1350, h=800, cena=self.cena)
+NameError: name 'Elemento' is not defined
+'''},
