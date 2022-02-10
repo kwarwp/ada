2ee678872e62becf03bd06d644e5fa6e1ab6f8ab
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-02-10 15:05:06 -0300

    Saved gatil/__log__.py @2022-02-10 18:05

--- gatil/__log__.py
+++ gatil/__log__.py
@@ -660,3 +660,10 @@ SyntaxError: invalid syntax
                                        ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Thu Feb 10 2022 15:05:06.20 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 63
+  posx, posy = -(w//dx) * (posicao % dx), -(h//dy) * (posicao // dx)
+                                         ^
+SyntaxError: invalid syntax
+'''},
