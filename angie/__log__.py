48e2c92bb9c458f03075874f70575d9ba60e2259
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-02-09 23:05:03 -0300

    Saved angie/__log__.py @2022-02-10 02:05

--- angie/__log__.py
+++ angie/__log__.py
@@ -6,3 +6,10 @@
                ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Feb 09 2022 23:05:03.211 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 21
+  r = svg.rect(
+                                                                      ^
+SyntaxError: invalid syntax
+'''},
