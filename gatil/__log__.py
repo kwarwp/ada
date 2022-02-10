e6e14d906bee5bfe6fd148da8324352d4b126738
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-02-09 21:01:47 -0300

    Saved gatil/__log__.py @2022-02-10 00:01

--- gatil/__log__.py
+++ gatil/__log__.py
@@ -359,3 +359,25 @@ Gatil(GATIL_MOS).vai()
                                 ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Feb 09 2022 21:01:47.10 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 208
+    Gatil(GATIL_MOS).vai()
+  module <module> line 182
+    sala_b_args[0] = Rua(sala_b_args[0], self.trash, [
+  module <module> line 78
+    [p[proname](*proargs) for proname, proargs  in props]
+  module <module> line 66
+    super().__init__(HALO, x=x, y=y, w=w, h=h, o=0.2, vai=self.cena, cena=cena)
+AttributeError: 'Trash' object has no attribute 'cena'
+'''},
