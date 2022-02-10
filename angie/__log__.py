b951d6cc55629144b58f7c1cf3b1a0a8ab6c62f5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-02-09 23:06:10 -0300

    Saved angie/__log__.py @2022-02-10 02:06

--- angie/__log__.py
+++ angie/__log__.py
@@ -31,3 +31,21 @@ SyntaxError: invalid syntax
     self.cache = self.create_script_tag(LIXAO)
 AttributeError: 'Retangulos' object has no attribute 'create_script_tag'
 '''},
+{'date': 'Wed Feb 09 2022 23:06:09.385 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 27
+    Retangulos().make_rects(1000, 800)
+  module <module> line 12
+    self.cena.elt <= self.rubish
+AttributeError: 'Retangulos' object has no attribute 'rubish'
+'''},
