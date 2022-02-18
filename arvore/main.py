3434c2b893a9194200e6abe394fb77c2d0a2ef83
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-02-18 16:02:36 -0300

    Saved arvore/main.py @2022-02-18 19:02

--- arvore/main.py
+++ arvore/main.py
@@ -13,6 +13,8 @@ class Arvore:
         
     def make_nodes(self):
         cena = self.cena
+        red, green, siena = 0, -33, -66
+        '''Seletor de deslocamento do sprite'''
         class Node(Elemento):
             def __init__(self, x, y, w, c, h=33):
                 super().__init__(ELIPSE, x=x, y=y, w=w, h=h, o=0.45, cena=cena, vai=self.nomeia)
@@ -27,10 +29,11 @@ class Arvore:
                     position='relative', top=0, left=0, backgroundColor='transparent', padding="0.5rem 1.5rem"))
                 self.o =1
         [Node(x, y, w, c) for x, y, w, c in [
-            [85, 479, 180, -66],[410, 493, 180, -66],[850, 458, 180, -66],
-            [410, 55, 170, -33],[380, 100, 170, -33],[425, 145, 190, -33],
-            [280, 143, 150, 0],[560, 120, 150, 0],[560, 180, 150, 0]
+            [85, 479, 180, siena],[410, 493, 180, siena],[850, 458, 180, siena],
+            [410, 55, 170, green],[380, 100, 170, green],[425, 145, 190, green],
+            [280, 143, 150, red],[560, 120, 150, red],[560, 180, 150, red]
         ]]
+        '''Somente alguns nós estão programados. Outros podem ser acrescentados adicionando os parâmetros'''
 
 
 Arvore().make_nodes()
