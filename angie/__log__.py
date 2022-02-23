3112c7da37364e1b16d124b0421edea270d0081c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-02-23 14:44:18 -0300

    Saved angie/__log__.py @2022-02-23 17:44

--- angie/__log__.py
+++ angie/__log__.py
@@ -253,3 +253,22 @@ Traceback (most recent call last):
     point = (
 AttributeError: 'Linhas' object has no attribute 'random_point'
 '''},
+{'date': 'Wed Feb 23 2022 14:44:17.808 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Generating art
+Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 196
+    Linhas().generate_art() #.make_rects(1300, 800)
+  module <module> line 145
+    svg.line (x1=a, y1=b, x2=c, y2=d, style=f"stroke:rgb(255,0,0);stroke-width:{current_thickness}")
+TypeError: style must be dict, not str
+'''},
