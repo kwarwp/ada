3c9bfe84bc467c83ff03fae6b2b3f60ddf84d649
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-02-23 16:38:07 -0300

    Saved angie/__log__.py @2022-02-23 19:38

--- angie/__log__.py
+++ angie/__log__.py
@@ -312,3 +312,22 @@ Traceback (most recent call last):
     ypoints = points[1:]+points[0]
 TypeError: can only concatenate list (not "tuple") to list
 '''},
+{'date': 'Wed Feb 23 2022 16:38:07.182 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Generating art
+Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 212
+    Linhas().generate_art() #.make_rects(1300, 800)
+  module <module> line 126
+    for i, (a, b), (c, d) in enumerate(liner):
+ValueError: need more than 2 values to unpack
+'''},
