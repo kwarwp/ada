e8d4b94c51e5ffae5f5fb188632b235357a48264
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-02-23 14:42:31 -0300

    Saved angie/__log__.py @2022-02-23 17:42

--- angie/__log__.py
+++ angie/__log__.py
@@ -234,3 +234,22 @@ Traceback (most recent call last):
     point = (
 NameError: name 'random_point' is not defined
 '''},
+{'date': 'Wed Feb 23 2022 14:42:31.30 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Generating art
+Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 192
+    Linhas().generate_art() #.make_rects(1300, 800)
+  module <module> line 100
+    point = (
+AttributeError: 'Linhas' object has no attribute 'random_point'
+'''},
