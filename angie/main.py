fe3734fed378a9684c5238510b6f14fdb383b5e5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-02-23 14:37:43 -0300

    Saved angie/main.py @2022-02-23 17:37

--- angie/main.py
+++ angie/main.py
@@ -6,7 +6,9 @@ ARVORE = "https://imgur.com/VkBQmuU.jpg"
 BLACK = "https://i.imgur.com/cQogon6.jpg"
 from _spy.vitollino.main import Cena, STYLE, Elemento
 from browser import svg
-from random import randint
+from random import randint, random
+import colorsys
+
 STYLE.update(width=1350, height="650px")
 class Retangulos:
     def __init__(self):
@@ -67,7 +69,123 @@ class Linhas:
         self.rects = Elemento('', x=0, y=0, w=1350, h=800, cena=self.cena)
         self.cena.vai()
         self.rects.elt <= self.tela
-        
+
+    def generate_art(self):
+        print("Generating art")
+
+        # Figure out where we are going to put it.
+        #output_dir = os.path.join("output", collection)
+        #image_path = os.path.join(output_dir, f"{name}.png")
+
+        # Set size parameters.
+        rescale = 2
+        image_size_px = 128 * rescale
+        padding = 12 * rescale
+
+        # Create the directory and base image.
+        # os.makedirs(output_dir, exist_ok=True)
+        bg_color = (0, 0, 0)
+        # image = Image.new("RGB", (image_size_px, image_size_px), bg_color)
+
+        # How many lines do we want to draw?
+        num_lines = 10
+        points = []
+
+        # Pick the colors.
+        start_color = random_color()
+        end_color = random_color()
+
+        # Generate points to draw.
+        for _ in range(num_lines):
+            point = (
+                random_point(image_size_px, padding),
+                random_point(image_size_px, padding),
+            )
+            points.append(point)
+
+        # Center image.
+        # Find the bounding box.
+        min_x = min([p[0] for p in points])
+        max_x = max([p[0] for p in points])
+        min_y = min([p[1] for p in points])
+        max_y = max([p[1] for p in points])
+
+        # Find offsets.
+        x_offset = (min_x - padding) - (image_size_px - padding - max_x)
+        y_offset = (min_y - padding) - (image_size_px - padding - max_y)
+
+        # Move all points by offset.
+        for i, point in enumerate(points):
+            points[i] = (point[0] - x_offset // 2, point[1] - y_offset // 2)
+
+        # Draw the points.
+        current_thickness = 1 * rescale
+        n_points = len(points) - 1
+        for i, point in enumerate(points):
+
+            # Create the overlay.
+            # overlay_image = Image.new("RGB", (image_size_px, image_size_px), (0, 0, 0))
+            # overlay_draw = ImageDraw.Draw(overlay_image)
+
+            if i == n_points:
+                # Connect the last point back to the first.
+                next_point = points[0]
+            else:
+                # Otherwise connect it to the next element.
+                next_point = points[i + 1]
+
+            # Find the right color.
+            factor = i / n_points
+            line_color = interpolate(start_color, end_color, factor=factor)
+
+            # Draw the line.
+            #overlay_draw.line([point, next_point], fill=line_color, width=current_thickness)
+            pts = point + next_point
+            a, b, c, d = *pts
+            svg.line (x1=a, y1=b, x2=c, y2=d, style=f"stroke:rgb(255,0,0);stroke-width:{current_thickness}")
+
+            # Increase the thickness.
+            current_thickness += rescale
+
+            # Add the overlay channel.
+            #image = ImageChops.add(image, overlay_image)
+
+        # Image is done! Now resize it to be smooth.
+        '''
+        image = image.resize(
+            (image_size_px // rescale, image_size_px // rescale), resample=Image.ANTIALIAS
+        )
+
+        # Save the image.
+        image.save(image_path)
+        '''
+
+ 
+
+    def random_color(self):
+
+        # I want a bright, vivid color, so max V and S and only randomize HUE.
+        h = random()
+        s = 1
+        v = 1
+        float_rbg = colorsys.hsv_to_rgb(h, s, v)
+
+        # Return as integer RGB.
+        return (
+            int(float_rbg[0] * 255),
+            int(float_rbg[1] * 255),
+            int(float_rbg[2] * 255),
+        )
+
+    def interpolate(self, start_color, end_color, factor: float):
+        # Find the color that is exactly factor (0.0 - 1.0) between the two colors.
+        new_color_rgb = []
+        for i in range(3):
+            new_color_value = factor * end_color[i] + (1 - factor) * start_color[i]
+            new_color_rgb.append(int(new_color_value))
+
+        return tuple(new_color_rgb)
+       
     def make_nodes(self):
         cena = self.cena
 
