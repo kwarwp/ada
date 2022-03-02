8ae0a4c2d06e231a98a0d17cc85dc6bf435aa988
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-03-02 13:30:34 -0300

    Saved gatil/__log__.py @2022-03-02 16:30

--- gatil/__log__.py
+++ gatil/__log__.py
@@ -914,3 +914,23 @@ TypeError: __init__ missing 1 positional argument: 'img'
     style.update(**_PATTERN.STARRY)
 NameError: name 'style' is not defined
 '''},
+{'date': 'Wed Mar 02 2022 13:30:33.726 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 414
+    Gatil(GATIL_MOS).vai()
+  module <module> line 409
+    cur = Cursor(GATAR, x=200, y=250, w=100, h=100)
+  module gatil.util line 129
+    style.update(**_PATTERN.STARRY)
+AttributeError: 'CSSProperty' object has no attribute 'update'
+'''},
