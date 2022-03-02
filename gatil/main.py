b9de9421c340acbcf265c5aee6a3ba578e965bc3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-03-02 12:49:06 -0300

    Saved gatil/main.py @2022-03-02 15:49

--- gatil/main.py
+++ gatil/main.py
@@ -405,7 +405,8 @@ class Gatil(Cena):
         #INV.bota(g)
         INV.bota(p)
         sala_b.norte.vai()
-        c = Elemento(GATAR, style=dict(left=200, top=250, width=100, height=100))
+        c = Elemento(GATAR, x=200, y=250, w=100, h=100)
+        c.style.update(left=200, top=250, width=100, height=100)
         cur = Cursor(alvo=c)
         #self.trash.dump(sala_b.norte)
         #go = Cena(vai=
