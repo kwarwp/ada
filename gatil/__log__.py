5aaf71bf042412f3679d7374bdf44f9d7caa84ad
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-03-02 12:26:09 -0300

    Saved gatil/__log__.py @2022-03-02 15:26

--- gatil/__log__.py
+++ gatil/__log__.py
@@ -789,3 +789,23 @@ TypeError: can't add 'Elemento' object to DOMNode instance
                                                      ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Mar 02 2022 12:26:09.72 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 413
+    Gatil(GATIL_MOS).vai()
+  module <module> line 408
+    cur = Cursor(alvo=sala_b.norte)
+  module _spy.vitollino.main line 1489
+    dims = [self.alvo.style.top, self.alvo.style.minHeight, self.alvo.style.left, self.alvo.style.width]
+AttributeError: 'Rua' object has no attribute 'style'
+'''},
