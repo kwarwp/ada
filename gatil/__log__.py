da0487dd1de841484a8f821e64ee5a439468ec75
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-03-02 18:00:09 -0300

    Saved gatil/__log__.py @2022-03-02 21:00

--- gatil/__log__.py
+++ gatil/__log__.py
@@ -1032,3 +1032,23 @@ Exception: <TypeError: $B.get_class(...) is undefined>
                                             ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Mar 02 2022 18:00:09.243 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 433
+    Gatil(GATIL_MOS).vai()
+  module <module> line 404
+    sala_b_args[0] = Rua(sala_b_args[0], self.trash, [
+  module <module> line 273
+    [p[proname](*proargs) for proname, proargs  in props]
+TypeError: __init__() takes 1 positional argument but more were given
+'''},
