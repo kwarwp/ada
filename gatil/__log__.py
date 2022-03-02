25a442e6b6293ea506fc8494e320124906a1a974
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-03-02 12:43:56 -0300

    Saved gatil/__log__.py @2022-03-02 15:43

--- gatil/__log__.py
+++ gatil/__log__.py
@@ -829,3 +829,23 @@ AttributeError: 'Rua' object has no attribute 'style'
     dims = [self.alvo.style.top, self.alvo.style.minHeight, self.alvo.style.left, self.alvo.style.width]
 AttributeError: 'dict' object has no attribute 'top'
 '''},
+{'date': 'Wed Mar 02 2022 12:43:55.479 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 414
+    Gatil(GATIL_MOS).vai()
+  module <module> line 409
+    cur = Cursor(alvo=c)
+  module _spy.vitollino.main line 1489
+    dims = [self.alvo.style.top, self.alvo.style.minHeight, self.alvo.style.left, self.alvo.style.width]
+AttributeError: 'dict' object has no attribute 'top'
+'''},
