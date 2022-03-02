71dd8845b2690874d0fa21e8de16f5413e2bc63a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-03-02 18:04:04 -0300

    Saved gatil/__log__.py @2022-03-02 21:04

--- gatil/__log__.py
+++ gatil/__log__.py
@@ -1052,3 +1052,25 @@ SyntaxError: invalid syntax
     [p[proname](*proargs) for proname, proargs  in props]
 TypeError: __init__() takes 1 positional argument but more were given
 '''},
+{'date': 'Wed Mar 02 2022 18:04:03.787 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 433
+    Gatil(GATIL_MOS).vai()
+  module <module> line 404
+    sala_b_args[0] = Rua(sala_b_args[0], self.trash, [
+  module <module> line 273
+    [p[proname](*proargs) for proname, proargs  in props]
+  module <module> line 245
+    super().__init__(img=PETUNIO, x=x, y=y, w=w, h=h, cena=cena)
+TypeError: __init__() got an unexpected keyword argument 'img'
+'''},
