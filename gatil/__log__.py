29e2783f3d8cce6e60ca2a4c4834e8929da27efa
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-03-02 23:07:12 -0300

    Saved gatil/__log__.py @2022-03-03 02:07

--- gatil/__log__.py
+++ gatil/__log__.py
@@ -1259,3 +1259,25 @@ AttributeError: 'NoneType' object has no attribute 'turn'
     eat = time * (len(self.cats)+1)
 AttributeError: 'TheHero' object has no attribute 'cats'
 '''},
+{'date': 'Wed Mar 02 2022 23:07:11.559 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 447
+    Gatil(GATIL_MOS).vai()
+  module <module> line 441
+    sala_b.norte.vai()
+  module <module> line 293
+    TheHero().turn()
+  module <module> line 236
+    GATIL.turn()
+AttributeError: 'NoneType' object has no attribute 'turn'
+'''},
