8f881691b4f99218c538f5bc7b34ff3ecc128833
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-03-02 21:17:19 -0300

    Saved gatil/__log__.py @2022-03-03 00:17

--- gatil/__log__.py
+++ gatil/__log__.py
@@ -1124,3 +1124,25 @@ RuntimeError: too much recursion
   module '$exec_8562' line 481
 Gatil(GATIL_MOS).vai()
 '''},
+{'date': 'Wed Mar 02 2022 21:17:18.363 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 454
+    Gatil(GATIL_MOS).vai()
+  module <module> line 425
+    sala_b_args[0] = Rua(sala_b_args[0], self.trash, [
+  module <module> line 294
+    [p[proname](*proargs) for proname, proargs  in props]
+  module <module> line 266
+    self.enter(cena)
+AttributeError: 'Hero' object has no attribute 'enter'
+'''},
