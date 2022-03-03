eddb065e9d3fa819d916e9e1ad8551de2e3c171f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-03-02 21:49:46 -0300

    Saved gatil/main.py @2022-03-03 00:49

--- gatil/main.py
+++ gatil/main.py
@@ -244,6 +244,7 @@ class TheHero(Elemento):
         self.start()
         
     def start(self):
+        if TheHero.FISH: return
         self.cats = []
         TheHero.FISH = f = [f"{fish}_fish" for fish in range(4)]
         [INV.bota(fish, "https://i.imgur.com/Tjswa4z.png") for fish in f]
