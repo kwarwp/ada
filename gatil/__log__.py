5f21e8c2484ebf846bdb72a15c0ccdb9b3c6d986
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-03-04 20:09:02 -0300

    Saved gatil/__log__.py @2022-03-04 23:09

--- gatil/__log__.py
+++ gatil/__log__.py
@@ -1359,3 +1359,25 @@ SyntaxError: EOL while scanning string literal
 'error': '''
 Exception: <TypeError: $range.tree[0] is undefined>
 '''},
+{'date': 'Fri Mar 04 2022 20:09:01.810 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 470
+    Gatil(GATIL_MOS).vai()
+  module <module> line 464
+    sala_b.norte.vai()
+  module <module> line 309
+    TheHero().do_turn()
+  module <module> line 256
+    eaten = self.cats + 1
+TypeError: can only concatenate list (not "int") to list
+'''},
