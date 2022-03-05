b1e240c17bc64a0fbbf6f69767499d95164e241e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-03-04 21:45:00 -0300

    Saved gatil/__log__.py @2022-03-05 00:45

--- gatil/__log__.py
+++ gatil/__log__.py
@@ -1656,3 +1656,29 @@ AttributeError: 'TheHero' object has no attribute 'resultado'
     h = TheHero().cursor
 AttributeError: 'TheHero' object has no attribute 'cursor'
 '''},
+{'date': 'Fri Mar 04 2022 21:44:59.603 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 481
+    Gatil(GATIL_MOS).vai()
+  module <module> line 455
+    sala_a_args = [Rua(sala, self.trash,[(P.H, [200, 550])]) for sala in sala_a_args]
+  module <module> line 309
+    [p[proname](*proargs) for proname, proargs  in props]
+  module <module> line 278
+    self.hero = TheHero()
+  module <module> line 227
+    self.start()
+  module <module> line 232
+    GATIL.g.vai = resposta
+AttributeError: 'NoneType' object has no attribute 'g'
+'''},
