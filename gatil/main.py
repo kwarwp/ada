cebfab088ee750bf74452a36d60fb9f187c84c92
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-03-04 21:14:17 -0300

    Saved gatil/main.py @2022-03-05 00:14

--- gatil/main.py
+++ gatil/main.py
@@ -229,7 +229,7 @@ class TheHero(Elemento):
         
     def start(self):
         if TheHero.FISH is not None: return
-        TheHero.FISH = f = [f"{fish}_fish" for fish in range(14)]
+        TheHero.FISH = f = [f"{fish}_fish" for fish in range(4)]
         [INV.bota(fish, "https://i.imgur.com/Tjswa4z.png") for fish in f]
         p_names = "s_luck s_char s_asce s_prot m_keen m_lead m_snea m_cunn b_nimb b_heal b_stre b_pers".split()
         p_names += "p_loot p_levl p_turn p_cats p_xper".split()
