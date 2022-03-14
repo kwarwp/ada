f7c3c720a57a297c672cb7bf4c779f21e30c22db
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-03-14 18:40:01 -0300

    Saved natalia/main.py @2022-03-14 21:40

--- natalia/main.py
+++ natalia/main.py
@@ -3,12 +3,14 @@ from _spy.vitollino.main import Cena,Elemento
 from _spy.vitollino.main import INVENTARIO as inv
 CANADA= "https://www.estudopratico.com.br/wp-content/uploads/2019/04/ottawa-capital-canada.jpg"
 SALA= " https://cdn.imgbin.com/17/18/18/imgbin-kim-taehyung-bts-k-pop-tomorrow-exo-others-B9xAfxjVW72prTfKWQzahU4PQ.jpg"
+'''
 class anna():
     sala=Elemento(img=SALA)
     canada=Cena(img=CANADA)
     sala.entra(canada)
     canada.vai()
 #anna()
+'''
 class Numer:
     def __init__(self, name):
         words = name.split()
