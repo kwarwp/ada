51cf94456acd1aabf56f47a7ba1a5e95733f4539
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-03-28 16:45:14 -0300

    Saved gatil/__log__.py @2022-03-28 19:45

--- gatil/__log__.py
+++ gatil/__log__.py
@@ -1753,3 +1753,21 @@ Traceback (most recent call last):
   module <module> line 7
     from random import randint, shuffle, choice, choices
 '''},
+{'date': 'Mon Mar 28 2022 16:45:13.535 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 224
+    class TheHero(Elemento):
+  module <module> line 233
+    def start(self, cena=cena):
+NameError: name 'cena' is not defined
+'''},
