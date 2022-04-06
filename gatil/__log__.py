df526f81edbbce57ffc857c1a1b95e9c2e6f297c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-04-06 10:53:45 -0300

    Saved gatil/__log__.py @2022-04-06 13:53

--- gatil/__log__.py
+++ gatil/__log__.py
@@ -1935,3 +1935,21 @@ IndexError: pop index out of range
     salas_args = [[Rua(sala, self.trash,[(P.H, [200, 550]), (P.S, [1043, (573+G.pop()) if rua in Z else 573]),
 TypeError: append() takes 2 positional argument but more were given
 '''},
+{'date': 'Wed Apr 06 2022 10:53:45.275 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 607
+    Gatil(GATIL_MOS).vai()
+  module <module> line 549
+    salas_args = [[Rua(sala, self.trash,[(P.H, [200, 550]), (P.S, [1043, (573+G.pop()) if rua in Z else 573]),
+TypeError: append() takes 2 positional argument but more were given
+'''},
