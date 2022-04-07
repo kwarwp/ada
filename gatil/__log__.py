6d2becd950530ac0467e5d37cf40179b8162edd3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-04-07 17:33:25 -0300

    Saved gatil/__log__.py @2022-04-07 20:33

--- gatil/__log__.py
+++ gatil/__log__.py
@@ -2273,3 +2273,25 @@ AttributeError: 'list' object has no attribute 'toggle'
     self.properties[P.S][0].toggle()
 IndexError: list index out of range
 '''},
+{'date': 'Thu Apr 07 2022 17:33:19.504 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 736
+    Gatil(GATIL_MOS).vai()
+  module <module> line 702
+    sala_b.norte.vai()
+  module <module> line 489
+    TheHero().do_turn(self)
+  module <module> line 311
+    rua.hero.tit = f"XP:{self.xper} LV:{self.levl}"
+AttributeError: 'Rua' object has no attribute 'hero'
+'''},
