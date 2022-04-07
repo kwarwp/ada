690207f0953be3a71429c48bea1c7e90d222bb3c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-04-07 15:31:01 -0300

    Saved gatil/__log__.py @2022-04-07 18:31

--- gatil/__log__.py
+++ gatil/__log__.py
@@ -2225,3 +2225,27 @@ TypeError: __init__() got an unexpected keyword argument 'cena'
     Rua.GOOD.pop().furia()
 IndexError: pop index out of range
 '''},
+{'date': 'Thu Apr 07 2022 15:31:00.262 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 733
+    Gatil(GATIL_MOS).vai()
+  module <module> line 699
+    sala_b.norte.vai()
+  module <module> line 488
+    Rua.enfuriar()
+  module <module> line 359
+    Rua.GOOD[0].turno(Rua.GOOD[1]) if len(Rua.GOOD) > 1 else None
+  module <module> line 349
+    self.properties[P.S].toggle()
+AttributeError: 'list' object has no attribute 'toggle'
+'''},
