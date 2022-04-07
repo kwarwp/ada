6af55e76477534686a9f1c8cb091aaca79ea7646
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-04-06 22:55:20 -0300

    Saved gatil/__log__.py @2022-04-07 01:55

--- gatil/__log__.py
+++ gatil/__log__.py
@@ -2077,3 +2077,23 @@ AttributeError: 'Rua' object has no attribute 'nome'
     Rua.fury()
 AttributeError: 'Rua' object has no attribute 'fury'
 '''},
+{'date': 'Wed Apr 06 2022 22:55:19.513 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 693
+    Gatil(GATIL_MOS).vai()
+  module <module> line 659
+    sala_b.norte.vai()
+  module <module> line 453
+    Rua.fury()
+AttributeError: 'Rua' object has no attribute 'fury'
+'''},
