9020d315df3dade785989e39879ab222759b6acd
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-04-13 15:55:09 -0300

    Created callie/__log__.py @2022-04-13 18:55

---
+++ callie/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Wed Apr 13 2022 15:55:07.356 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 4
+  GOGHIntru2 = 
+                ^
+SyntaxError: invalid syntax
+'''},
