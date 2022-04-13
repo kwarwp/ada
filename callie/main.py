142421ec94ac973fb5172e3d3695ae020af3bac3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-04-13 16:15:09 -0300

    Saved callie/main.py @2022-04-13 19:15

--- callie/main.py
+++ callie/main.py
@@ -1,11 +1,7 @@
 from _spy.vitollino.main import Cena, Elemento, Droppable, Dragger, Codigo
 
-GOGHRoom ="https://upload.wikimedia.org/wikipedia/commons/f/ff/Camera_da_Letto_Vincent_van_Gogh.jpg"
-GOGHIntru2 = 
+GOGHRoom ="https://i.imgur.com/CRNsfXO.jpg"
 GOGHIntru = "https://images.vexels.com/media/users/3/139889/isolated/preview/da50fec14dc402a17863b78227684335-silhueta-do-gato-2-by-vexels.png"
-GOGHQ1 =
-GOGHQ2 =
-GOGHQ3 =
 
 #Drag and drops: Elemento -> na Cena; Elemento -> Elemento; Elemento para inventário
 
