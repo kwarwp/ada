7dc7e9fa3bfb337b0299a4effc4e622818e319d4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-04-13 15:55:25 -0300

    Saved callie/__log__.py @2022-04-13 18:55

--- callie/__log__.py
+++ callie/__log__.py
@@ -6,3 +6,10 @@
                 ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Apr 13 2022 15:55:24.668 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module browser.aio line 1
+  <!DOCTYPE html>
+  ^
+SyntaxError: invalid syntax
+'''},
