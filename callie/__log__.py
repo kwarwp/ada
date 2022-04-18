2bf19922a4c7dd330c8a5917d1c664b30d5fa3d7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-04-18 15:49:53 -0300

    Saved callie/__log__.py @2022-04-18 18:49

--- callie/__log__.py
+++ callie/__log__.py
@@ -120,3 +120,27 @@ AttributeError: 'list' object has no attribute 'indexof'
     self.face = [face.show() if inx == face_index else face.hide()
 AttributeError: 'list' object has no attribute 'indexf'
 '''},
+{'date': 'Mon Apr 18 2022 15:49:52.242 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 56
+    Cubos(CENAS)
+  module <module> line 51
+    cubos = [Cubo(inx=inx, faces=cenas) for inx in range(12)]
+  module <module> line 45
+    self.faces = [Face(cubo=self, inx=inx, face=face) for face in faces]
+  module <module> line 22
+    self.h = h
+  module _spy.vitollino.main line 708
+    self.elt.style.height = "{}px".format(value)
+AttributeError: 'Face' object has no attribute 'elt'
+'''},
