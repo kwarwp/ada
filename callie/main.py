1d3f94c30e0077b8c2732c8f8ebd3ec4318ea157
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-04-18 14:56:41 -0300

    Saved callie/main.py @2022-04-18 17:56

--- callie/main.py
+++ callie/main.py
@@ -1,20 +1,35 @@
 from _spy.vitollino.main import Cena, Elemento, STYLE
 STYLE.update(width=850, height="650px")
 
+IMGUR = "https://i.imgur.com/{}.jpg"
 GOGHRoom ="https://i.imgur.com/CRNsfXO.jpg"
+FUNDO = "qWcEao4"
 GOGHIntru = "https://i.imgur.com/nVpyITK.png"
 CITY1 = "https://i.imgur.com/jiJY1NY.jpg"
 LAND1 = "https://i.imgur.com/GsdFmpz.jpg"
-
+CENAS = "CRNsfXO nVpyITK jiJY1NY GsdFmpz T6pmXbY dJ4WOIh "
+CENAS_ = "swVe1IW      "
+OFF = 2000
 
 class Cubos:
-    def __init__(self):
+    def __init__(self, cenas, tw=800, th=500, nx=4, ny=3):
+        class Face(Elemento):
+            def __init__(self,inx, face, **kwargs):
+                w, h = tw//nx, h//ny
+                x, y = (inx % nx)*w, (inx // nx)*h
+                super().__init__(face, x=x, y=y-OFF, w=w, h=h, cena=cena)
+                self.siz = (tw, th)
+                self.pos = (-x, -y)
+        class Cubo:
+            def __init__(self,inx, faces, **kwargs):
+                self.faces = [Face(inx=inx, face=face) for face in faces]
         cena = Cena(GOGHRoom).vai()
-        el = Elemento(GOGHIntru, cena=cena)
+        el = Elemento(FUNDO, cena=cena)
+        cubos = [Cubo(inx=inx, faces=faces) for inx in range(12)]
 #Drag and drops: Elemento -> na Cena; Elemento -> Elemento; Elemento para inventário
 
 if __name__ == "__main__":
-    Cubos()
+    Cubos(CENAS)
 
 
 
