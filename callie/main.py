aa303e91f2532dea36699bacd033013ca9def711
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-04-19 09:50:52 -0300

    Saved callie/main.py @2022-04-19 12:50

--- callie/main.py
+++ callie/main.py
@@ -12,16 +12,17 @@ LAND1 = "https://i.imgur.com/GsdFmpz.jpg"
 CENAS = "CRNsfXO swVe1IW jiJY1NY GsdFmpz T6pmXbY dJ4WOIh".split()
 CENAS_ = "swVe1IW nVpyITK     "
 OFF = 2000
+OFX, OFY = 100, 50
 
 class Cubos:
-    def __init__(self, cenas, tw=800, th=500, nx=4, ny=3):
+    def __init__(self, cenas, tw=600//3*4, th=600, nx=4, ny=3):
         class Face(Elemento):
             def __init__(self,cubo, inx, face, **kwargs):
                 self.cubo = cubo
                 w, h = tw//nx, th//ny
                 self.dh = h
                 x, y = (inx % nx)*w, (inx // nx)*h
-                super().__init__(IMGUR.format(face), x=x, y=y-OFF, w=w, h=h, cena=cena, vai=self.vai)
+                super().__init__(IMGUR.format(face), x=x+OFX, y=y-OFF+OFY, w=w, h=h, cena=cena, vai=self.vai)
                 self.siz = (tw, th)
                 self.pos = (-x, -y)
                 self.quad = 0
@@ -32,7 +33,7 @@ class Cubos:
                 self.y -= OFF if self.y > 10  else 0
                 return False
             def orient(self, ori):
-                self.elt.style.transform="rotate({ori*90}deg)"
+                self.elt.style.transform=f"rotate({ori*90}deg)"
                 return False
             def vai(self, evt):
                 e = evt.target;
@@ -43,8 +44,9 @@ class Cubos:
                 self.quad = 0
                 self.quad = 0 if (e > 0) and ( n <h) else 1 if (e > 0) and ( n >h) else 3 if (e < 0) and ( n >h) else 2
                 #self.quad += 1 if  > self.h else -1
-                alert(f"x: {x} y: {y} qd: {self.quad}")
+                #alert(f"x: {x} y: {y} qd: {self.quad}")
                 self.cubo.roll(randint(0,5))
+                self.cubo.go(self.quad)
         class Cubo:
             ROLL = [[16,4,20,12],[18,8,22,0],[17,12,23,4],[16,0,20,8],[6,1,12,11],[12,3,6,10]]
             def __init__(self,inx, faces, **kwargs):
@@ -55,6 +57,9 @@ class Cubos:
                 self.face = [face.show() if facer == face_index else face.hide()
                              for face_index, face in enumerate(self.faces)].index(True)
                 self.faces[self.face].orient(self.orient)
+            def go(self,inx):
+                go_face_roll = slf.ROLL[self.face][inx]
+                self.roll(go_face_roll)
         cena = Cena(IMGUR.format(FUNDO)).vai()
         #el = Elemento(IMGUR.format(FUNDO), cena=cena)
         cubos = [Cubo(inx=inx, faces=cenas) for inx in range(12)]
