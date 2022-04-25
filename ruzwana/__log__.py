da59a2b4992320c27781328a58ede21f7649feef
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-04-25 15:11:03 -0300

    Saved ruzwana/__log__.py @2022-04-25 18:11

--- ruzwana/__log__.py
+++ ruzwana/__log__.py
@@ -144,3 +144,20 @@ Traceback (most recent call last):
   module <module> line 2
     from _spy.vitollino import jogos
 '''},
+{'date': 'Mon Apr 25 2022 15:11:02.319 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''<class 'Cubos'>
+Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 4
+    Cubos(jogos.CENAS)
+NameError: name 'Cubos' is not defined
+'''},
