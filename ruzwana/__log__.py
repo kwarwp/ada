7edf703101284bff23c30c8db540b7607468dd74
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-04-27 18:34:21 -0300

    Saved ruzwana/__log__.py @2022-04-27 21:34

--- ruzwana/__log__.py
+++ ruzwana/__log__.py
@@ -298,3 +298,21 @@ TypeError: __new__() missing 3 positional arguments: e,m,d
     ator.tit = ator.nome
 AttributeError: 'list' object has no attribute 'tit'
 '''},
+{'date': 'Wed Apr 27 2022 18:34:20.939 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 51
+    roteiro = Roteiro(cena, ele, rot)
+  module <module> line 18
+    ator.tit = ator.nome
+AttributeError: 'list' object has no attribute 'tit'
+'''},
