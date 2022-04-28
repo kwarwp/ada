d0321af66ebe91d823cf8982534c2c89403fdd08
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-04-28 11:10:26 -0300

    Saved ruzwana/__log__.py @2022-04-28 14:10

--- ruzwana/__log__.py
+++ ruzwana/__log__.py
@@ -432,3 +432,21 @@ SyntaxError: invalid syntax
     def __init__(self,nomes=NOMES, yy=40, xx=20, dx=100):
 NameError: name 'NOMES' is not defined
 '''},
+{'date': 'Thu Apr 28 2022 11:10:25.529 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 68
+    Guerreiras()
+  module <module> line 49
+    self.elenco = [Elemento(IMGUR.format(ELENCO[conta+1]), y=yy, x=xx+dx*conta, cena=cena) for conta in range(5)]
+NameError: free variable 'cena' referenced before assignment in enclosing scope
+'''},
