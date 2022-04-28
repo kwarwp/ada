b200c03b70092e6433e7582eeaf8994efbc9e040
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-04-28 11:46:22 -0300

    Saved ruzwana/__log__.py @2022-04-28 14:46

--- ruzwana/__log__.py
+++ ruzwana/__log__.py
@@ -518,3 +518,19 @@ TypeError: __new__() missing 1 positional argument: prox
     Fala = namedtuple('Fala','ator fala age prox', defaults=(None,))
 TypeError: namedtuple() got an unexpected keyword argument 'defaults'
 '''},
+{'date': 'Thu Apr 28 2022 11:46:21.482 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 6
+    Fala = namedtuple('Fala','ator fala age prox', defaults=(None,)*4)
+TypeError: namedtuple() got an unexpected keyword argument 'defaults'
+'''},
