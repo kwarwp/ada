5e894fa7c58351daafbdf32d7536040a95bfe2aa
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-04-28 11:10:08 -0300

    Saved ruzwana/__log__.py @2022-04-28 14:10

--- ruzwana/__log__.py
+++ ruzwana/__log__.py
@@ -414,3 +414,21 @@ TypeError: __init__() takes 3 positional argument but more were given
                                      ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Thu Apr 28 2022 11:10:07.951 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 47
+    class Guerreiras:
+  module <module> line 48
+    def __init__(self,nomes=NOMES, yy=40, xx=20, dx=100):
+NameError: name 'NOMES' is not defined
+'''},
