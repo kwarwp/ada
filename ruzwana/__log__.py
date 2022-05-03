e4c18c895c9ef54553f4d7bf8c644f791e90b2e0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-05-03 18:20:31 -0300

    Saved ruzwana/__log__.py @2022-05-03 21:20

--- ruzwana/__log__.py
+++ ruzwana/__log__.py
@@ -568,3 +568,21 @@ AttributeError: 'NoneType' object has no attribute 'prox'
                                                                                           ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Tue May 03 2022 18:20:30.366 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 110
+    Sorrisos()
+  module <module> line 101
+    self.elenco = [Elemento(IMGUR.format(ELENCO[conta+1]), y=yy, x=xx+dx*conta, cena=cena) for conta in [0, 5]]
+IndexError: list index out of range
+'''},
