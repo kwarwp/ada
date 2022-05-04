a787860792794ae0af0ac4db4c35c058a5ce4224
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-05-04 10:43:44 -0300

    Saved ruzwana/__log__.py @2022-05-04 13:43

--- ruzwana/__log__.py
+++ ruzwana/__log__.py
@@ -660,3 +660,23 @@ AttributeError: 'NoneType' object has no attribute 'ator'
     roteiro = [Fala(a, f, g if g else p.ator, x ) for [a, f, g, x], p in prox]
 AttributeError: 'NoneType' object has no attribute 'ator'
 '''},
+{'date': 'Wed May 04 2022 10:43:43.107 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 166
+    Guerreiras()
+  module <module> line 127
+    roteiro = Roteiro(cena, rot, ele)
+  module <module> line 21
+    roteiro = [Fala(a, f, g if g else p.ator, x ) for [a, f, g, x], p in prox]
+AttributeError: 'NoneType' object has no attribute 'ator'
+'''},
