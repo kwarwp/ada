60692972968f4d319430466c7c80b44ebbb72de0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-05-09 11:35:38 -0300

    Saved ruzwana/__log__.py @2022-05-09 14:35

--- ruzwana/__log__.py
+++ ruzwana/__log__.py
@@ -680,3 +680,23 @@ AttributeError: 'NoneType' object has no attribute 'ator'
     roteiro = [Fala(a, f, g if g else p.ator, x ) for [a, f, g, x], p in prox]
 AttributeError: 'NoneType' object has no attribute 'ator'
 '''},
+{'date': 'Mon May 09 2022 11:35:37.417 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 170
+    Sorrisos()
+  module <module> line 148
+    roteiro = Roteiro(cena, rot, ele, self.foi)
+  module <module> line 22
+    roteiro = [Fala(a, f, g if g else (p.ator if p else None), x, mini=dic_ator[a].mini) for [a, f, g, x], p in prox]
+TypeError: __new__() got an unexpected keyword argument 'mini'
+'''},
