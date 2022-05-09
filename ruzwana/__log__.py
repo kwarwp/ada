3453f0e42f859100270946fd009c24894e30fad5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-05-09 14:16:22 -0300

    Saved ruzwana/__log__.py @2022-05-09 17:16

--- ruzwana/__log__.py
+++ ruzwana/__log__.py
@@ -758,3 +758,21 @@ TypeError: __new__() got an unexpected keyword argument 'mini'
     ele = [Ator(ato,nom, 1, A.e),
 NameError: name 'ato' is not defined
 '''},
+{'date': 'Mon May 09 2022 14:16:21.834 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 173
+    Sorrisos()
+  module <module> line 145
+    rot = [
+NameError: name 'kerexu' is not defined
+'''},
