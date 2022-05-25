990afb6dbcce7b8d204b7d438d64d89a042cf6a0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-05-25 03:01:28 -0300

    Saved morgan/__log__.py @2022-05-25 06:01

--- morgan/__log__.py
+++ morgan/__log__.py
@@ -213,3 +213,23 @@ AttributeError: 'int' object has no attribute '__getitem__'
     imagem, kwargs = (imagem, {}) if isinstance(imagem, str) \
 AttributeError: 'int' object has no attribute '__getitem__'
 '''},
+{'date': 'Wed May 25 2022 03:01:27.819 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 14
+    cenaAgua = Cena(imagem_agua, style=dict(height=60,widht=60, left=600, top=20))
+  module _spy.vitollino.main line 1096
+    Cena.c(**kwargs)
+  module _spy.vitollino.main line 1139
+    imagem, kwargs = (imagem, {}) if isinstance(imagem, str) \
+KeyError: img
+'''},
