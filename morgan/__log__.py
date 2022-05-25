0ea36de0ff9b3254a49661eb0de3d573f633ce89
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-05-25 02:59:41 -0300

    Saved morgan/__log__.py @2022-05-25 05:59

--- morgan/__log__.py
+++ morgan/__log__.py
@@ -193,3 +193,23 @@ TypeError: 'Cena' object is not iterable
     imagem, kwargs = (imagem, {}) if isinstance(imagem, str) \
 AttributeError: 'int' object has no attribute '__getitem__'
 '''},
+{'date': 'Wed May 25 2022 02:59:40.386 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 14
+    cenaAgua = Cena(imagem_agua, width = 220)
+  module _spy.vitollino.main line 1096
+    Cena.c(**kwargs)
+  module _spy.vitollino.main line 1139
+    imagem, kwargs = (imagem, {}) if isinstance(imagem, str) \
+AttributeError: 'int' object has no attribute '__getitem__'
+'''},
