4ab7b530d462e5c49ab46a50bdc6fc0c26727385
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-05-25 00:32:29 -0300

    Saved naomi/__log__.py @2022-05-25 03:32

--- naomi/__log__.py
+++ naomi/__log__.py
@@ -64,3 +64,21 @@ AttributeError: 'Elemento' object has no attribute 'direita'
     Cena.direita(img=cr7)
 AttributeError: 'Cena' object has no attribute 'direita'
 '''},
+{'date': 'Wed May 25 2022 00:32:28.541 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 6
+    class jogador():
+  module <module> line 7
+    star.meio=Cena(img=STAR)
+NameError: name 'star' is not defined
+'''},
