f0e5c19111b6e1fd733aa83f79383a4f7dc15888
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-05-27 02:21:58 -0300

    Saved naomi/__log__.py @2022-05-27 05:21

--- naomi/__log__.py
+++ naomi/__log__.py
@@ -82,3 +82,19 @@ AttributeError: 'Cena' object has no attribute 'direita'
     star.meio=Cena(img=STAR)
 NameError: name 'star' is not defined
 '''},
+{'date': 'Fri May 27 2022 02:21:58.651 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 6
+    oceano.vai()
+AttributeError: 'str' object has no attribute 'vai'
+'''},
