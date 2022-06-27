0802cc34808ac866c09ada854ce12bee70dac0c2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-06-27 17:59:46 -0300

    Saved aventura/__log__.py @2022-06-27 20:59

--- aventura/__log__.py
+++ aventura/__log__.py
@@ -31,3 +31,21 @@ AttributeError: 'Aventura' object has no attribute 'main'
     print(ADV[:100])
 NameError: name 'ADV' is not defined
 '''},
+{'date': 'Mon Jun 27 2022 17:59:45.694 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 332
+    Aventura().main(ADV)
+  module <module> line 28
+    i = "\n".join( ini for kind, ini in self.roteiro if kind == "I")
+ValueError: need more than 1 value to unpack
+'''},
