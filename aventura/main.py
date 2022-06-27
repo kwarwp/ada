953fcf3dc56a026efdc6ebadd4b3d77739b43cc5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-06-27 17:39:49 -0300

    Created aventura/main.py @2022-06-27 20:39

---
+++ aventura/main.py
@@ -0,0 +1,30 @@
+# ada.aventura.main.py
+# SPDX-License-Identifier: GPL-3.0-or-later
+""" Engenho de aventuras textuais.
+
+.. codeauthor:: Carlo Oliveira <carlo@ufrj.br>
+
+Changelog
+---------
+
+.. versionadded::    22.06
+
+    Leitura dos dados.
+"""
+__version__ = "22.06"
+
+
+class Cenario:
+    ...
+    
+
+class Personagem:
+    ...
+    
+
+class Aventura:
+    ...
+    
+    
+if __name__ == "__main__":
+    Aventura().main()
