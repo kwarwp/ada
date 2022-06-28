ebd5cb3bd20561ff0a4ff9dc57ce9bcea42588ef
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-06-28 10:13:44 -0300

    Saved aventura/__log__.py @2022-06-28 13:13

--- aventura/__log__.py
+++ aventura/__log__.py
@@ -299,3 +299,23 @@ TypeError: sequence item 0: expected str instance, Objeto found
     Aventura().main(ADV)
 TypeError: __init__() missing 2 positional arguments: adv,cenario
 '''},
+{'date': 'Tue Jun 28 2022 10:13:43.463 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 393
+    Aventura().main(ADV)
+  module <module> line 88
+    locais.pop().vai()
+  module <module> line 33
+    texto = self.descreve + "\nVocê pode ver:\n" + "\n".join(ob.mostra() for ob in self.objeto.keys())
+AttributeError: 'str' object has no attribute 'mostra'
+'''},
