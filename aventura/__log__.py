8fa02ba3edf2392705cf42ae74302edb276b86ed
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-06-28 12:24:34 -0300

    Saved aventura/__log__.py @2022-06-28 15:24

--- aventura/__log__.py
+++ aventura/__log__.py
@@ -414,3 +414,27 @@ SyntaxError: keyword argument repeated
     verbo, substantivo = self.verbo, self.decreve or fala.descreve
 AttributeError: 'Verbo' object has no attribute 'decreve'
 '''},
+{'date': 'Tue Jun 28 2022 12:24:33.709 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 418
+    Aventura().main(ADV)
+  module <module> line 112
+    locais = [Cenario(rot[ini:fim], self) for ini,fim in zip(lro, lro[1:]+[len(rot)])]
+  module <module> line 29
+    self.obj = [Objeto(adv[ini:fim],self) for ini,fim in zip(lro, lro[1:]+[len(adv)])]
+  module <module> line 58
+    verbos = [Verbo(adv[ini:fim], cenario).itens()
+  module <module> line 82
+    self.acao = [acoes[kind](suplement) for kind, suplement in adv[::-1]]
+NameError: name 'move' is not defined
+'''},
