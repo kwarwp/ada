7c8913a32ea88f037ab5e1a188aa68571c80ec2f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-06-29 11:06:34 -0300

    Saved aventura/__log__.py @2022-06-29 14:06

--- aventura/__log__.py
+++ aventura/__log__.py
@@ -770,3 +770,31 @@ TypeError: itens() missing 1 positional argument: it
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Wed Jun 29 2022 11:06:33.643 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 455
+    Aventura().main(ADV)
+  module <module> line 149
+    locais.pop().vai()
+  module <module> line 37
+    self.interpreta(fala)
+  module <module> line 53
+    self.objeto[substantivo].vai(fala) if substantivo in self.objeto else self.nop(fala)
+  module <module> line 74
+    self.verbo[verbo].vai(self) if verbo in self.verbo else self.cenario.nop(fala, self.nome)
+  module <module> line 110
+    [action() for action in self.acao]
+  module <module> line 88
+    return go(arg, **kwarg)
+TypeError: noper() takes 1 positional arguments but more were given
+'''},
