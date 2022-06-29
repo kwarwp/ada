63db0eb2d56da86b73dcff159e60bbe2254255f8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-06-29 17:23:24 -0300

    Saved aventura/__log__.py @2022-06-29 20:23

--- aventura/__log__.py
+++ aventura/__log__.py
@@ -1120,3 +1120,97 @@ StopIteration: SEU PAI ESTA LIGADAO NA TEVE...
     self.hero.bota(nome, self.objeto[nome])
 KeyError: CHAV
 '''},
+{'date': 'Wed Jun 29 2022 17:23:23.147 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 505
+    Aventura().main(ADV)
+  module <module> line 199
+    locais.pop().vai()
+  module <module> line 38
+    self.interpreta(fala)
+  module <module> line 65
+    self.objeto[substantivo].vai(fala) if substantivo in self.objeto else self.nop(fala)
+  module <module> line 86
+    self.verbo[verbo].vai(self) if verbo in self.verbo else self.cenario.nop(fala, self.nome)
+  module <module> line 133
+    self.cenario.interpreta(input(f"vb:{self.descreve}")) if self.descreve else None
+  module <module> line 57
+    self.vai()
+  module <module> line 38
+    self.interpreta(fala)
+  module <module> line 65
+    self.objeto[substantivo].vai(fala) if substantivo in self.objeto else self.nop(fala)
+  module <module> line 86
+    self.verbo[verbo].vai(self) if verbo in self.verbo else self.cenario.nop(fala, self.nome)
+  module <module> line 139
+    except StopIteration as e:
+  module <module> line 57
+    self.vai()
+  module <module> line 38
+    self.interpreta(fala)
+  module <module> line 65
+    self.objeto[substantivo].vai(fala) if substantivo in self.objeto else self.nop(fala)
+  module <module> line 86
+    self.verbo[verbo].vai(self) if verbo in self.verbo else self.cenario.nop(fala, self.nome)
+  module <module> line 139
+    except StopIteration as e:
+  module <module> line 57
+    self.vai()
+  module <module> line 38
+    self.interpreta(fala)
+  module <module> line 65
+    self.objeto[substantivo].vai(fala) if substantivo in self.objeto else self.nop(fala)
+  module <module> line 86
+    self.verbo[verbo].vai(self) if verbo in self.verbo else self.cenario.nop(fala, self.nome)
+  module <module> line 140
+    self.cenario.interpreta(input(self.message))
+  module <module> line 65
+    self.objeto[substantivo].vai(fala) if substantivo in self.objeto else self.nop(fala)
+  module <module> line 86
+    self.verbo[verbo].vai(self) if verbo in self.verbo else self.cenario.nop(fala, self.nome)
+  module <module> line 139
+    except StopIteration as e:
+  module <module> line 57
+    self.vai()
+  module <module> line 38
+    self.interpreta(fala)
+  module <module> line 63
+    self.interpreta(fala)
+  module <module> line 65
+    self.objeto[substantivo].vai(fala) if substantivo in self.objeto else self.nop(fala)
+  module <module> line 86
+    self.verbo[verbo].vai(self) if verbo in self.verbo else self.cenario.nop(fala, self.nome)
+  module <module> line 139
+    except StopIteration as e:
+  module <module> line 102
+    self.cenario.nop(["tentar", self.verbo], "Falhou")
+  module <module> line 47
+    self.interpreta(input(texto)) if not self.exit else None
+  module <module> line 57
+    self.vai()
+  module <module> line 38
+    self.interpreta(fala)
+  module <module> line 65
+    self.objeto[substantivo].vai(fala) if substantivo in self.objeto else self.nop(fala)
+  module <module> line 86
+    self.verbo[verbo].vai(self) if verbo in self.verbo else self.cenario.nop(fala, self.nome)
+  module <module> line 139
+    except StopIteration as e:
+  module <module> line 100
+    return go(arg, **kwarg)
+  module <module> line 172
+    self.cenario.remove(substantivo)
+  module <module> line 43
+    del self.objeto[objeto]
+KeyError: CHAV
+'''},
