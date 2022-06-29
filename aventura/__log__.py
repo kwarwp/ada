c9c7a1878660b0ff713923e90554fbc6dee0ab9c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-06-29 12:00:20 -0300

    Saved aventura/__log__.py @2022-06-29 15:00

--- aventura/__log__.py
+++ aventura/__log__.py
@@ -950,3 +950,33 @@ ValueError: need more than 1 value to unpack
     obj, descreve, *_ = cmd.split(":")
 ValueError: need more than 1 value to unpack
 '''},
+{'date': 'Wed Jun 29 2022 12:00:20.210 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 464
+    Aventura().main(ADV)
+  module <module> line 158
+    locais.pop().vai()
+  module <module> line 37
+    self.interpreta(fala)
+  module <module> line 53
+    self.objeto[substantivo].vai(fala) if substantivo in self.objeto else self.nop(fala)
+  module <module> line 74
+    self.verbo[verbo].vai(self) if verbo in self.verbo else self.cenario.nop(fala, self.nome)
+  module <module> line 117
+    [action() for action in self.acao]
+  module <module> line 88
+    return go(arg, **kwarg)
+  module <module> line 125
+    objeto, descreve = local.split(":")
+ValueError: too many values to unpack (expected 2)
+'''},
