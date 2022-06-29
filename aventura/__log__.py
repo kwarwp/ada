412f17fe35ae35ab9cfb2cb3bc823f63a17192f3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-06-29 17:40:42 -0300

    Saved aventura/__log__.py @2022-06-29 20:40

--- aventura/__log__.py
+++ aventura/__log__.py
@@ -1250,3 +1250,81 @@ KeyError: CHAV
     local = Cenario.OBJ[objeto]
 KeyError: RUA
 '''},
+{'date': 'Wed Jun 29 2022 17:40:40.848 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 506
+    Aventura().main(ADV)
+  module <module> line 200
+    locais.pop().vai()
+  module <module> line 38
+    self.interpreta(fala)
+  module <module> line 66
+    self.objeto[substantivo].vai(fala) if substantivo in self.objeto else self.nop(fala)
+  module <module> line 87
+    self.verbo[verbo].vai(self) if verbo in self.verbo else self.cenario.nop(fala, self.nome)
+  module <module> line 140
+    except StopIteration as e:
+  module <module> line 66
+    self.objeto[substantivo].vai(fala) if substantivo in self.objeto else self.nop(fala)
+  module <module> line 87
+    self.verbo[verbo].vai(self) if verbo in self.verbo else self.cenario.nop(fala, self.nome)
+  module <module> line 140
+    except StopIteration as e:
+  module <module> line 101
+    return go(arg, **kwarg)
+  module <module> line 160
+    local.vai()
+  module <module> line 38
+    self.interpreta(fala)
+  module <module> line 66
+    self.objeto[substantivo].vai(fala) if substantivo in self.objeto else self.nop(fala)
+  module <module> line 87
+    self.verbo[verbo].vai(self) if verbo in self.verbo else self.cenario.nop(fala, self.nome)
+  module <module> line 141
+    self.cenario.interpreta(input(self.message))
+  module <module> line 66
+    self.objeto[substantivo].vai(fala) if substantivo in self.objeto else self.nop(fala)
+  module <module> line 87
+    self.verbo[verbo].vai(self) if verbo in self.verbo else self.cenario.nop(fala, self.nome)
+  module <module> line 140
+    except StopIteration as e:
+  module <module> line 66
+    self.objeto[substantivo].vai(fala) if substantivo in self.objeto else self.nop(fala)
+  module <module> line 87
+    self.verbo[verbo].vai(self) if verbo in self.verbo else self.cenario.nop(fala, self.nome)
+  module <module> line 140
+    except StopIteration as e:
+  module <module> line 66
+    self.objeto[substantivo].vai(fala) if substantivo in self.objeto else self.nop(fala)
+  module <module> line 87
+    self.verbo[verbo].vai(self) if verbo in self.verbo else self.cenario.nop(fala, self.nome)
+  module <module> line 140
+    except StopIteration as e:
+  module <module> line 101
+    return go(arg, **kwarg)
+  module <module> line 160
+    local.vai()
+  module <module> line 38
+    self.interpreta(fala)
+  module <module> line 66
+    self.objeto[substantivo].vai(fala) if substantivo in self.objeto else self.nop(fala)
+  module <module> line 87
+    self.verbo[verbo].vai(self) if verbo in self.verbo else self.cenario.nop(fala, self.nome)
+  module <module> line 140
+    except StopIteration as e:
+  module <module> line 101
+    return go(arg, **kwarg)
+  module <module> line 158
+    objeto, descreve = local.split(":")
+ValueError: need more than 1 value to unpack
+'''},
