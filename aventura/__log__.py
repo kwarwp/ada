be271742693ab343d498387b5e4e8aed1d246281
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-06-29 11:59:22 -0300

    Saved aventura/__log__.py @2022-06-29 14:59

--- aventura/__log__.py
+++ aventura/__log__.py
@@ -926,3 +926,27 @@ KeyError: CIBELE, FUI COMPRAR REVISTINHAS
     obj, descreve = cmd.split(":")
 ValueError: need more than 1 value to unpack
 '''},
+{'date': 'Wed Jun 29 2022 11:59:21.297 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 464
+    Aventura().main(ADV)
+  module <module> line 156
+    locais = [Cenario(rot[ini:fim], self) for ini,fim in zip(lro, lro[1:]+[len(rot)])]
+  module <module> line 30
+    self.obj = [Objeto(adv[ini:fim],self) for ini,fim in zip(lro, lro[1:]+[len(adv)])]
+  module <module> line 69
+    verbos = [Verbo(adv[ini:fim], cenario).itens()
+  module <module> line 106
+    obj, descreve, *_ = cmd.split(":")
+ValueError: need more than 1 value to unpack
+'''},
