ec94dfd2f5d8467b98b0deb5770ecc74ab6c59af
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-06-30 12:08:52 -0300

    Saved aventura/__log__.py @2022-06-30 15:08

--- aventura/__log__.py
+++ aventura/__log__.py
@@ -1668,3 +1668,39 @@ KeyError: CHAV
     objeto, descreve = local.split(":")
 ValueError: too many values to unpack (expected 2)
 '''},
+{'date': 'Thu Jun 30 2022 12:08:51.954 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 535
+    Aventura().main(ADV)
+  module <module> line 228
+    locais.pop().vai()
+  module <module> line 38
+    self.interpreta(fala)
+  module <module> line 73
+    obj.vai(fala) if obj else  self.nop(fala)
+  module <module> line 94
+    self.verbo[verbo].vai(self) if verbo in self.verbo else self.cenario.nop(fala, self.nome)
+  module <module> line 150
+    except StopIteration as e:
+  module <module> line 73
+    obj.vai(fala) if obj else  self.nop(fala)
+  module <module> line 94
+    self.verbo[verbo].vai(self) if verbo in self.verbo else self.cenario.nop(fala, self.nome)
+  module <module> line 150
+    except StopIteration as e:
+  module <module> line 108
+    return go(arg, **kwarg)
+  module <module> line 172
+    objeto, descreve = local.split(":")
+ValueError: too many values to unpack (expected 2)
+'''},
