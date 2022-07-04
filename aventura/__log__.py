c6c8e6516e07da62395d723b171ca7eecbd4e98c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-07-04 12:17:38 -0300

    Saved aventura/__log__.py @2022-07-04 15:17

--- aventura/__log__.py
+++ aventura/__log__.py
@@ -2231,3 +2231,39 @@ SyntaxError: keyword can't be an expression
                                 ^
 SyntaxError: keyword can't be an expression
 '''},
+{'date': 'Mon Jul 04 2022 12:17:37.84 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 543
+    Aventura().main(ADV)
+  module <module> line 235
+    locais.pop().vai()
+  module <module> line 42
+    self.interpreta(fala)
+  module <module> line 77
+    obj.vai(fala) if obj else  self.nop(fala)
+  module <module> line 98
+    self.verbo[verbo].vai(self) if verbo in self.verbo else self.cenario.nop(fala, self.nome)
+  module <module> line 147
+    self.cenario.interpreta(input(f"vb:{self.descreve}")) if self.descreve else None
+  module <module> line 77
+    obj.vai(fala) if obj else  self.nop(fala)
+  module <module> line 98
+    self.verbo[verbo].vai(self) if verbo in self.verbo else self.cenario.nop(fala, self.nome)
+  module <module> line 153
+    except StopIteration as e:
+  module <module> line 112
+    return go(arg, **kwarg)
+  module <module> line 171
+    lro = self.lro if self.lro else descreve
+AttributeError: 'Verbo' object has no attribute 'lro'
+'''},
