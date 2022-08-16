0c5ea192a4213a91e7de3251d4cfe180fc424c16
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-08-16 14:16:53 -0300

    Saved libby/main.py @2022-08-16 17:16

--- libby/main.py
+++ libby/main.py
@@ -1,6 +1,7 @@
 # ada.libby.main.py
 from _spy.vitollino.main  import Cena,Elemento
 from _spy.vitollino.main import INVENTARIO as inv
+from browser import svg, document
 CDD="https://upload.wikimedia.org/wikipedia/commons/e/e9/Cidade_de_Deus.jpg"
 FLASH="https://pngriver.com/wp-content/uploads/2018/03/Download-Flash-PNG-Pic-For-Designing-Projects.png"
 class sempai():
@@ -9,3 +10,23 @@ class sempai():
     flash.entra(cdd)
     cdd.vai()
 sempai()
+
+class Box:
+    def __init__(self, box):
+        self.box = box
+    def paint(self, box):
+        self.box = box
+
+
+class Svg Painter:
+    def __init__(self):
+        root = document["pydiv"]
+        self.fill = "red"
+        self.opacity = 0.6
+        self.shape = dict(b=lambda x, y, w, h, it=self: svg.rect(x=x, y=y, width=w, height=h, fill=it.fill, fillOpacity=it.opacity))
+        self.canvas = svg.svg(viewBox="0 0 1200 650", width=1200, height=650)
+    def paint(self, shape="b", **kwargs):
+        self.canvas <= self.shape[shape](**kwargs)
+        
+SvgPainter().paint("b", 10, 10, 200, 100)
+        
