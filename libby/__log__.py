3236fcfafe7bf750f392ae9df15f77fdadbf0e89
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-08-16 14:17:15 -0300

    Saved libby/__log__.py @2022-08-16 17:17

--- libby/__log__.py
+++ libby/__log__.py
@@ -27,3 +27,19 @@ IndentationError: expected an indented block
              ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Tue Aug 16 2022 14:17:14.664 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 31
+    SvgPainter().paint("b", 10, 10, 200, 100)
+TypeError: paint() takes 2 positional argument but more were given
+'''},
