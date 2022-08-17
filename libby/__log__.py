16df90dfe31839da00b5194bffc3e8ebefe10e04
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-08-17 14:00:54 -0300

    Saved libby/__log__.py @2022-08-17 17:00

--- libby/__log__.py
+++ libby/__log__.py
@@ -187,3 +187,23 @@ SyntaxError: invalid syntax
                                                                                                      ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Aug 17 2022 14:00:54.96 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 109
+    main = Main()
+  module <module> line 100
+    style={'position':"absolute", 'left':'0px', 'top': f'{tp}px', 'min-height':"30px", 'background':bg}
+  module <module> line 1
+    (tp)
+NameError: name 'tp' is not defined
+'''},
