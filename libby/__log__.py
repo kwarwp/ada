ea692bb6efe1f183a108c9fd9d856016f21f47bb
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-08-17 13:55:31 -0300

    Saved libby/__log__.py @2022-08-17 16:55

--- libby/__log__.py
+++ libby/__log__.py
@@ -166,3 +166,10 @@ SyntaxError: invalid syntax
                                                                                                                                       ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Aug 17 2022 13:55:30.984 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 100
+  self.colors = [html.DIV(
+                                                                                                                                      ^
+SyntaxError: invalid syntax
+'''},
