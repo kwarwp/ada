8f0c19c5bccb58302fceab6c973d1ff714825cde
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-08-17 14:13:25 -0300

    Saved libby/main.py @2022-08-17 17:13

--- libby/main.py
+++ libby/main.py
@@ -95,7 +95,7 @@ class SvgMarquee:
         
 class Main:
     def __init__(self):
-        self.menu = html.DIV(style={'position':"absolute", 'left':'0px', 'top':'0px', 'z-index': 10})
+        self.menu = html.DIV(style={'position':"absolute", 'left':'100px', 'top':'100px', 'z-index': 10})
         colors = "yellow green red blue grey peru".split()
         self.colors = [html.DIV(style=
         {'position':"absolute", 'left':'0px', 'top': f'{tp}px', 'min-height':"30px", 'background':bg}
