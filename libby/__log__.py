73d63c348179697230a8f3baabc27c87416f24a1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-08-18 10:34:31 -0300

    Saved libby/__log__.py @2022-08-18 13:34

--- libby/__log__.py
+++ libby/__log__.py
@@ -350,3 +350,19 @@ AttributeError: 'Main' object has no attribute 'filling'
     self.painter = painter or SvgPainter(s)
 NameError: name 's' is not defined
 '''},
+{'date': 'Thu Aug 18 2022 10:34:31.44 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 141
+    s.paint("b", f="yellow", x=10, y=10, w=400, h=200)
+NameError: name 's' is not defined
+'''},
