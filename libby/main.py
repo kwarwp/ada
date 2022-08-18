e80ec6e287d7ea5cfb41546b3580c5cbe1db33d7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-08-18 16:54:20 -0300

    Saved libby/main.py @2022-08-18 19:54

--- libby/main.py
+++ libby/main.py
@@ -118,12 +118,13 @@ class Main:
             cor = ev.target.style.backgroundColor
             self.filler(color=cor)
             self.painter.filler(color=cor)
-        colors = "transparent yellow green red blue black peru transparent transparent transparent transparent".split()
+        colors = "transparent yellow green red blue black peru".split()
         tools = ["transparent"] * 3
         ncol = len(colors)+1
         style = {}
-        self.colors = [html.DIV(style=sty(tp, bg)
-        ) for tp, bg in zip(list(range(80, ncol*40, 40)), colors)]
+        off = 40*ncol - 40
+        self.colors = [html.DIV(style=sty(tp, bg)) for tp, bg in zip(list(range(80, ncol*40, 40)), colors)]
+        self.tools = [html.DIV(style=sty(tp, bg)) for tp, bg in zip(list(range(off, ncol*40, 40)), tools)]
         document <= self.menu
         edit = "fa-solid fa-paintbrush"
         select = "fa-solid fa-object-group"
@@ -138,7 +139,7 @@ class Main:
             self.menu <= col
             col.bind("click", lay)
         off = 40*ncol - 40
-        for _col, _tool in zip(self.colors[-3:], (edit, select, zoom)):
+        for _col, _tool in zip(self.tools, (edit, select, zoom)):
             _col.style.top = f"{off}px"
             off+=40
             self.menu <= _col
