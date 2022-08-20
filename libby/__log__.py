7ca172616d6f1234b8fd042a8b516d318f42220d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-08-20 09:22:25 -0300

    Saved libby/__log__.py @2022-08-20 12:22

--- libby/__log__.py
+++ libby/__log__.py
@@ -391,3 +391,9 @@ NameError: name 'zoom' is not defined
                  ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Sat Aug 20 2022 09:22:25.79 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+Exception: function statement requires a name
+  module '$exec_2589' line 166
+main = Main()
+'''},
