89dedea1738d36c9f01e5c9269e636a4d170d5a6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-08-20 10:30:40 -0300

    Saved libby/__log__.py @2022-08-20 13:30

--- libby/__log__.py
+++ libby/__log__.py
@@ -483,3 +483,21 @@ AttributeError: 'Main' object has no attribute 'model'
     BOX.append(self)
 NameError: name 'BOX' is not defined
 '''},
+{'date': 'Sat Aug 20 2022 10:30:40.325 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 185
+    main = Main().main()
+  module <module> line 139
+    self.marquee.main()
+TypeError: 'Main' object is not callable
+'''},
