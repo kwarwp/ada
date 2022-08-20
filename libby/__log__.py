1cd26f4e5ae26a1bd056e34fd7cb4e1bd47826cb
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-08-20 10:13:19 -0300

    Saved libby/__log__.py @2022-08-20 13:13

--- libby/__log__.py
+++ libby/__log__.py
@@ -433,3 +433,27 @@ main = Main()
     self.model.paint(f=f, **kwargs)
 AttributeError: 'Main' object has no attribute 'model'
 '''},
+{'date': 'Sat Aug 20 2022 10:13:19.403 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 180
+    main = Main().main()
+  module <module> line 131
+    self.painter = painter or SvgPainter(self)
+  module <module> line 55
+    self.screen = self.paint("b", x=0, y=0, w=1200, h=650)
+  module <module> line 60
+    self.main.paint(f=f, **kwargs)
+  module <module> line 176
+    self.model.paint(f=f, **kwargs)
+AttributeError: 'Main' object has no attribute 'model'
+'''},
