5a871b3305ee845c1b5cf7b715391c19150d673f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-09-05 16:33:07 -0300

    Saved kathryn/__log__.py @2022-09-05 19:33

--- kathryn/__log__.py
+++ kathryn/__log__.py
@@ -54,3 +54,20 @@ Traceback (most recent call last):
     with urllib.urlopen(link) as f:
 AttributeError: 'module' object has no attribute 'urlopen'
 '''},
+{'date': 'Mon Sep 05 2022 16:33:06.720 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''1
+Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 5
+    with urlopen(link) as f:
+AttributeError: 'FileIO' object has no attribute '__exit__'
+'''},
