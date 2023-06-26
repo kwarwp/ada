b3bce74ebfecb2ad054ee3e9efea65848f1574d8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-06-26 13:06:57 -0300

    Saved soraya/__log__.py @2023-06-26 16:06

--- soraya/__log__.py
+++ soraya/__log__.py
@@ -24,3 +24,23 @@ NameError: name 'x' is not defined
                                                                                           ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Jun 26 2023 13:06:57.278 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 40
+    Remedios()
+  module <module> line 33
+    self.remedio = Elemento(REMEDIOS, x=100, y=100, w=100, h=300, style=style.format(0, 0), cena=self.cena)
+  module _spy.vitollino.main line 551
+    self.style.update(**style)
+TypeError: string indices must be integers
+'''},
