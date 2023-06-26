b95f097ca6668de0f6a074d3fb757af39a290d64
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-06-26 12:56:07 -0300

    Saved soraya/main.py @2023-06-26 15:56

--- soraya/main.py
+++ soraya/main.py
@@ -17,4 +17,21 @@ Changelog
 .. codeauthor:: Carlo Oliveira <carlo@nce.ufrj.br>
 
 """
+from browser import document, alert, html
+from _spy.vitollino.main import Cena, STYLE, Elemento
+STYLE['width'] = 740
+#STYLE['height'] = "100%"
 
+A_NORTE = "https://i.imgur.com/aLEjWgB.png"
+FARMACIA = "https://i.imgur.com/9GeVtsf.jpg"
+REMEDIOS = "https://i.imgur.com/2gMnd3Y.jpg"
+
+class Remedios:
+    def __init__(self):
+        self.cena = Cena(FARMACIA)
+        self.remedio = Elemento(REMEDIOS, cena=self.cena)
+def _main():
+    document['pydiv'].html = ""
+    a_norte = Cena(img=A_NORTE)
+    a_norte.vai()
+Remedios()
