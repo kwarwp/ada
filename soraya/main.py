dd2b623fbd36f46e885be7eb2b0b9e41dc3c7104
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-06-26 13:45:44 -0300

    Saved soraya/main.py @2023-06-26 16:45

--- soraya/main.py
+++ soraya/main.py
@@ -19,19 +19,34 @@ Changelog
 """
 from browser import document, alert, html
 from _spy.vitollino.main import Cena, STYLE, Elemento
-STYLE['width'] = 740
+STYLE['width'] = 1000
 #STYLE['height'] = "100%"
 
 A_NORTE = "https://i.imgur.com/aLEjWgB.png"
 FARMACIA = "https://i.imgur.com/9GeVtsf.jpg"
 REMEDIOS = "https://i.imgur.com/2gMnd3Y.jpg"
-
+REMEDIOA = "https://imgur.com/YiERY7h.png"
+REMEDIOB = "https://i.imgur.com/AzV3ZQK.png"
+REMEDIOC = "https://i.imgur.com/f5FjW4L.png"
 class Remedios:
     def __init__(self):
         self.cena = Cena(FARMACIA)
-        self.remedio = Elemento(REMEDIOS, cena=self.cena)
-def _main():
-    document['pydiv'].html = ""
-    a_norte = Cena(img=A_NORTE)
-    a_norte.vai()
-Remedios()
+        # style = "position:relative; min-width:600px; top:{}px; left:{}px;"
+        # style = dict(position="relative", minWidth="600px", top="0px", left="0px", overflow="hidden")
+        style = dict(position="absolute", bottom="0px", marginLeft="50px", marginBottom="110px", color="white")
+        self.remedio = Elemento(REMEDIOA, x=100, y=100, w=200, h=400, texto="HIPERTENSÃO", cena=self.cena)
+        self.remedio.elt <= html.DIV("HIPERTENSÃO", style=style)
+        self.remedio = Elemento(REMEDIOB, x=350, y=100, w=200, h=400, cena=self.cena)
+        self.remedio.elt <= html.DIV(r"ANEMIA", style=style)
+        self.remedio = Elemento(REMEDIOC, x=600, y=100, w=200, h=400, cena=self.cena)
+        self.remedio.elt <= html.DIV("DIABETES", style=style)
+        self.cena.vai()
+
+
+def main():
+    Remedios()
+
+
+if __name__ == "__main__":
+    main()
+
