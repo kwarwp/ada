94f3fc13172c84449f0878ca6c4a9b1762711531
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-06-28 15:58:24 -0300

    Saved danae/main.py @2023-06-28 18:58

--- danae/main.py
+++ danae/main.py
@@ -19,8 +19,24 @@ Changelog
 from browser import document, alert, html
 from _spy.vitollino.main import Cena, STYLE, Elemento
 STYLE['width'] = 1000
+IMGSIZE = "3400px"
+W, H = 800, 600
+
+
+class CenaSprite(Cena):
+    def __init__(self,image, **kwargs):
+        super().__init__(image, index,  **kwargs)
+        # style=dict(left=x, top=x, width="80px", height="80px", overflow="hidden")
+        style = dict(position="relative", left=f"-{index % 4 * W}px", top=f"-{index // 4 * H}px", width=IMGSIZE,
+                     height=IMGSIZE)
+        style.update({"max-width": IMGSIZE, "max-height": IMGSIZE})
+        self.elt = html.DIV(style=STYLE)
+        self.img = html.IMG(src=self.img, width=width, style=style, title=nome)
+        self.elt <= self.img
+
 #STYLE['height'] = "100%"
 LAND = "https://i.imgur.com/Cmyq9vd.jpg"
 c=Cena(LAND)
-c.elt.style=dict(backgroundPosition="right 100px bottom 100px")
+c.elt.width="800px"
+c.elt.style=dict(left="-800px", width="800px", minWidth="6400px")
 c.vai()
