058625a5356c91dbeaed26f90ffeccf92d28f0e7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-06-28 15:59:54 -0300

    Saved danae/__log__.py @2023-06-28 18:59

--- danae/__log__.py
+++ danae/__log__.py
@@ -102,3 +102,21 @@ AttributeError: 'int' object has no attribute '__getitem__'
     imagem, kwargs = (imagem, {}) if isinstance(imagem, str) \
 AttributeError: 'int' object has no attribute '__getitem__'
 '''},
+{'date': 'Wed Jun 28 2023 15:59:54.245 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 39
+    c=CenaSprite(LAND, index=6)
+  module <module> line 34
+    self.img = html.IMG(src=self.img, width=width, style=style, title=nome)
+NameError: name 'width' is not defined
+'''},
