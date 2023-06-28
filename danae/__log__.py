9e6edbc4df7329da4e107625f9f23d9aeab75509
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-06-28 15:44:50 -0300

    Saved danae/__log__.py @2023-06-28 18:44

--- danae/__log__.py
+++ danae/__log__.py
@@ -42,3 +42,23 @@ SyntaxError: non-keyword arg after keyword arg
     assert trecho_repetido('hixhihi') == 1
 AssertionError: AssertionError
 '''},
+{'date': 'Wed Jun 28 2023 15:44:49.827 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 24
+    c=Cena(LAND)
+  module _spy.vitollino.main line 1098
+    self._cria_divs(width)
+  module _spy.vitollino.main line 1103
+    divesq.style.width = width // 3  # 100
+TypeError: unsupported operand type(s) for //: 'str' and 'int'
+'''},
