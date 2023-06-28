a62005538f3d50ac01765a6dc4420d0e76b1ca6e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-06-28 15:59:33 -0300

    Saved danae/__log__.py @2023-06-28 18:59

--- danae/__log__.py
+++ danae/__log__.py
@@ -82,3 +82,23 @@ TypeError: unsupported operand type(s) for //: 'str' and 'int'
     imagem, kwargs = (imagem, {}) if isinstance(imagem, str) \
 AttributeError: 'int' object has no attribute '__getitem__'
 '''},
+{'date': 'Wed Jun 28 2023 15:59:33.180 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 39
+    c=Cena(LAND, index=6)
+  module _spy.vitollino.main line 1096
+    Cena.c(**kwargs)
+  module _spy.vitollino.main line 1139
+    imagem, kwargs = (imagem, {}) if isinstance(imagem, str) \
+AttributeError: 'int' object has no attribute '__getitem__'
+'''},
