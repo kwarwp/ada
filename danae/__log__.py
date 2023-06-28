c93bf4aeed07239abac52b3e3d285338a5761dbd
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-06-28 16:00:28 -0300

    Saved danae/__log__.py @2023-06-28 19:00

--- danae/__log__.py
+++ danae/__log__.py
@@ -120,3 +120,21 @@ AttributeError: 'int' object has no attribute '__getitem__'
     self.img = html.IMG(src=self.img, width=width, style=style, title=nome)
 NameError: name 'width' is not defined
 '''},
+{'date': 'Wed Jun 28 2023 16:00:28.125 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 39
+    c=CenaSprite(LAND, index=6)
+  module <module> line 34
+    self.img = html.IMG(src=self.img, width=W, style=style, title=nome)
+NameError: name 'nome' is not defined
+'''},
