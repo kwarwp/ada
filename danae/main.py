ec9217e4b665071490678ead47607345086a8298
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-06-28 15:30:20 -0300

    Saved danae/main.py @2023-06-28 18:30

---
+++ danae/main.py
@@ -0,0 +1,24 @@
+# ada.danae.main.py
+""" Aventura Antartica.
+
+Changelog
+---------
+
+.. versionadded::    23.06
+
+    |br| Spike da Antartica (28)
+
+|   **Open Source Notification:** This file is part of open source program **Alite**
+|   **Copyright © 2023  Carlo Oliveira** <carlo@nce.ufrj.br>,
+|   **SPDX-License-Identifier:** `GNU General Public License v3.0 or later <http://is.gd/3Udt>`_.
+|   `Labase <http://labase.selfip.org/>`_ - `NCE <http://portal.nce.ufrj.br>`_ - `UFRJ <https://ufrj.br/>`_.
+
+.. codeauthor:: Carlo Oliveira <carlo@nce.ufrj.br>
+
+"""
+from browser import document, alert, html
+from _spy.vitollino.main import Cena, STYLE, Elemento
+STYLE['width'] = 1000
+#STYLE['height'] = "100%"
+LAND = "https://imgur.com/a/rVULspA.jpg"
+Cena(LAND).vai()
