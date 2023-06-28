c70e647857615c3c79be77942b63721b7a12b856
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-06-28 17:37:27 -0300

    Saved danae/__log__.py @2023-06-28 20:37

--- danae/__log__.py
+++ danae/__log__.py
@@ -197,3 +197,19 @@ NameError: name 'NADA' is not defined
     self._cria_divs(width)
 NameError: name 'width' is not defined
 '''},
+{'date': 'Wed Jun 28 2023 17:37:26.673 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 54
+    lb = [Labirinto(**salas) for salas in LABS]
+NameError: name 'Labirinto' is not defined
+'''},
