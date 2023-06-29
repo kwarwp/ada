a619ffae5faaee925d6d5ec580664c32c6c830fe
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-06-29 10:21:44 -0300

    Saved danae/__log__.py @2023-06-29 13:21

--- danae/__log__.py
+++ danae/__log__.py
@@ -358,3 +358,19 @@ SyntaxError: invalid syntax
     style = dict(position="relative", left=f"-{index % 4 * W}px", top=f"-{index // 4 * H}px", width=f"{W}px",
 NameError: name 'IMAGESIZE' is not defined
 '''},
+{'date': 'Thu Jun 29 2023 10:21:43.909 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 72
+    LA = [(j, (j-1)%64, (j+64+8)%64, (j+1)%(((j+1)//8)*8),  (j+56)%64) for  j in range(0,64,2)]
+ZeroDivisionError: integer division or modulo by zero
+'''},
