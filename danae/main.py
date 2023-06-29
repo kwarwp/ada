778120b389fb00f5d36d9fc34e3245242f6a7df3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-06-29 12:28:43 -0300

    Saved danae/main.py @2023-06-29 15:28

--- danae/main.py
+++ danae/main.py
@@ -20,7 +20,8 @@ from browser import document, alert, html
 from _spy.vitollino.main import Cena, STYLE, Elemento, Salao, NADA, Labirinto, NDCT #, INVENTARIO
 STYLE['width'] = 1350
 W, H = 1350, 650
-IMGSIZE, IMGHEIGHT = f"{4*W}px", f"{4*H}px"
+# IMGSIZE, IMGHEIGHT = f"{8*W}px", f"{4*H}px"
+IMGSIZE, IMGHEIGHT = f"{16*W}px", f"{8*H}px"
 ANICONS = "https://i.imgur.com/A2pPOED.png"
 SNOWY = "https://i.imgur.com/KxcqKpJ.gif"
 SNOW = "https://i.imgur.com/mpOU7Ca.gif"
@@ -28,12 +29,14 @@ HSNOW = "https://i.imgur.com/i5dLK8G.gif"
 BSNOW = "https://i.imgur.com/4B1xuMw.gif"
 SUICONS = "https://i.imgur.com/cM95ybG.jpg"
 ANICONS = "https://i.imgur.com/bmUoCIb.png"
+SUPPLY = "https://i.imgur.com/zWiteqC.gif"
 
 class Sprite(Elemento):
     def __init__(self, img="", vai=None, style=NDCT, tit="", alt="",
-                 x=0, y=0, w=100, h=100, o=1, texto='', foi=None, index=0, sw=100, sh=100, cx=1, b=0,
+                 x=0, y=0, w=100, h=100, o=1, texto='', foi=None, index=0, sw=100, sh=100, cx=1, b=0, s=1,
                  cena="", score=NDCT, drag=False, drop={}, tipo="100% 100%", **kwargs):
-        style=dict(left=x, top=x, width=f"{w}px", height=f"{h}px", overflow="hidden", filter= f"blur({b}px)")
+        #style=dict(left=x, top=x, width=f"{w}px", height=f"{h}px", overflow="hidden", filter= f"blur({b}px)", scale=s)
+        style=dict(width=f"{w}px", height=f"{h}px", overflow="hidden", filter= f"blur({b}px)", scale=s)
         position = f"-{index % cx * w}px -{index // cx * w}px"
         style.update({"max-width": f"{sw}px", "max-height": f"{sh}px", "background-position": position})
 
@@ -42,23 +45,13 @@ class Sprite(Elemento):
                  cena=cena, score=NDCT, drag=drag, drop=drop, tipo=f"{sw}px {sh}px", 
                  style=style,
                  **kwargs)
-        #self.style.update(style)
-        #self.elt = html.DIV(Id=tit, title=tit, style=self.style)
-        # self.elt.style.backgroundPosition=  "-64px -64px"
-        # self.elt.style.backgroundPosition=  "-64p -64"
-        # self.elt.style.update({"background-position": "-64 -64"})
-        #style = dict(position="relative", left=f"-{index % cx * w}px", top=f"-{index // cx * w}px", width=f"{w}px",
-        #             height=f"{h}px")
-        #style.update({"min-width": f"{sw}px", "min-height": f"{sh}px"})
-        # self.img.style = style
 
 class CenaSprite(Cena):
     def __init__(self,image, index, **kwargs):
         super().__init__(image,  **kwargs)
         # style=dict(left=x, top=x, width="80px", height="80px", overflow="hidden")
-        style = dict(position="relative", left=f"-{index % 4 * W}px", top=f"-{index // 4 * H}px", width=f"{W}px",
-                     height=f"{H}px", overflow="hidden")
-        style.update({"min-width": IMGSIZE, "min-height": IMGHEIGHT})
+        style = dict(position="relative", left=f"-{index % 8 * W}px", top=f"-{(index%16) // 4 * H}px", width=f"{W}px",
+                     height=f"{H}px", overflow="hidden", minWidth=IMGSIZE, minHeight=IMGHEIGHT)
         divsty = dict(STYLE)
         divsty.update({"max-width": f"{W}px", "max-height": f"{H}px", "overflow": "hidden"})
         self.elt = html.DIV(style=divsty)
@@ -72,19 +65,26 @@ class SpriteSala(Salao):
         self.nome = nome
         # Sala.c(**kwargs)
         self.p()
-
+def is7(j):
+    if j//8:
+        j+7
 LAND = "https://i.imgur.com/Cmyq9vd.jpg"
-MAPA = [{k:CenaSprite(LAND, index=v) for k, v in zip("nlso", range(l,l+4))} for l in range(0,16,4)]
+MAPA = [{k:CenaSprite(LAND, index=v) for k, v in zip("nlso", range(l,l+4))} for l in range(0,64*7,7)]
 #MAPA = [{k:v for k, v in zip("nlso", range(l,l+4))} for l in range(0,16,4)]
 # print(MAPA)
 sl = [SpriteSala(**sl) for sl in MAPA]
-LABS = [{k: v for k, v in zip("cnlso", [sl[int(s)] for s in cruz])} for cruz in "03131 12020 21313 30202".split()]
+# LA = [(j, (j-1)%(((j)//8+1)*8), (j+64+8)%64, (j+1)%(((j)//8+1)*8),  (j+56)%64) for  j in range(0,64)]
+LA = [(j, (j -1) if (j%8) else (j+7), (j+64+8)%64, (j+1)%(((j)//8+1)*8),  (j+56)%64) for  j in range(0,64)]
+# LABS = [{k: v for k, v in zip("cnlso", [sl[int(s)] for s in cruz])} for cruz in "03131 12020 21313 30202".split()]
+LABS = [{k: v for k, v in zip("cnlso", [sl[int(s)] for s in cruz])} for cruz in LA]
 lb = [Labirinto(**salas) for salas in LABS]
 #c=CenaSprite(LAND, index=6)
 #c.vai()
 #Elemento(BSNOW, w=W, h=H, o=0.7, cena=sl[0].norte)
-a = Sprite(ANICONS, w=64, h=64, o=0.3, index=4, sw=320, sh=256, cx=5, cena=sl[0].norte, b=2, tipo="320px 256px")
-
-
-sl[0].norte.vai()
+s = 64
+i = sl[0].norte
+a = Sprite(ANICONS, x=500, y=200, w=s, h=s, o=0.9, index=0, sw=320, sh=256, cx=5, cena=i, b=1, s=2)  #, tipo="320px 256px")
+s = Elemento(SUPPLY, w=200, cena=i)
+i.vai()
+# i.elt.text=LABS
 #print(a.style)
