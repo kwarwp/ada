a1d2da9c80fc071815b4bf2a0deedeb3d39ff7ed
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-06-29 11:20:30 -0300

    Saved danae/__log__.py @2023-06-29 14:20

--- danae/__log__.py
+++ danae/__log__.py
@@ -438,3 +438,19 @@ ZeroDivisionError: integer division or modulo by zero
     LABS = [{k: v for k, v in zip("cnlso", [sl[int(s)] for s in cruz])} for cruz in LA]
 IndexError: list index out of range
 '''},
+{'date': 'Thu Jun 29 2023 11:20:30.295 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 73
+    LA = [(j, (j(+7 if j//8 else -1))%(((j)//8+1)*8), (j+64+8)%64, (j+1)%(((j)//8+1)*8),  (j+56)%64) for  j in range(0,64)]
+TypeError: 'int' object is not callable
+'''},
