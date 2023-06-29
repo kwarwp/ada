49d4694a438407d0436b2b163cc01aee04a54279
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-06-29 12:04:29 -0300

    Saved danae/__log__.py @2023-06-29 15:04

--- danae/__log__.py
+++ danae/__log__.py
@@ -484,3 +484,19 @@ SyntaxError: invalid syntax
     LABS = [{k: v for k, v in zip("cnlso", [sl[int(s)] for s in cruz])} for cruz in LA]
 IndexError: list index out of range
 '''},
+{'date': 'Thu Jun 29 2023 12:04:28.707 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 77
+    LABS = [{k: v for k, v in zip("cnlso", [sl[int(s)] for s in cruz])} for cruz in LA]
+IndexError: list index out of range
+'''},
