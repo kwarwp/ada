e225f0545fcd7141bfe7207960266e074093fb88
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-08-15 20:30:26 -0300

    Created icongen/__log__.py @2023-08-15 23:30

---
+++ icongen/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Tue Aug 15 2023 20:30:25.346 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 20
+  bk = 
+        ^
+SyntaxError: invalid syntax
+'''},
