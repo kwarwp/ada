9e08a2d0e36572b4097e79f137162fd48f063b3c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-16 09:24:38 -0300

    Saved danae/__log__.py @2023-10-16 12:24

--- danae/__log__.py
+++ danae/__log__.py
@@ -656,3 +656,21 @@ NameError: name 'h' is not defined
     shuffle(comer)
 NameError: name 'shuffle' is not defined
 '''},
+{'date': 'Mon Oct 16 2023 09:24:37.924 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 219
+    t = Thrash().dump(j)
+  module <module> line 91
+    sorte += h.luck + randint(0, h.levl)//3
+AttributeError: 'Hero' object has no attribute 'luck'
+'''},
