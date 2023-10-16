5fb648ff0869d127833962da1de84e858d9a0aae
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-16 10:10:36 -0300

    Saved danae/main.py @2023-10-16 13:10

--- danae/main.py
+++ danae/main.py
@@ -16,8 +16,11 @@ Changelog
 .. codeauthor:: Carlo Oliveira <carlo@nce.ufrj.br>
 
 """
-from browser import document, alert, html
-from _spy.vitollino.main import Cena, STYLE, Elemento, Salao, NADA, Labirinto, NDCT #, INVENTARIO
+from browser import document, alert, html, svg
+from _spy.vitollino.main import Cena, STYLE, Elemento, Salao, NADA, Labirinto, NDCT, INVENTARIO as INV
+from collections import namedtuple
+from random import randint, shuffle, choice
+Hero = namedtuple("Hero", "luck pers levl")
 STYLE['width'] = 1350
 W, H = 1350, 650
 # IMGSIZE, IMGHEIGHT = f"{8*W}px", f"{4*H}px"
@@ -32,6 +35,129 @@ ANICONS = "https://i.imgur.com/bmUoCIb.png"
 SUPPLY = "https://i.imgur.com/zWiteqC.gif"
 SUPPLY = "https://i.imgur.com/PWreEK2.gif"
 PARADRP = "https://i.imgur.com/ShQMIoU.png"
+
+LIXAO = "https://raw.githubusercontent.com/kwarwp/ada/master/gatil/trink/lixocenter.svg"
+GATEIRA = "https://i.imgur.com/Ey0W3TR.png"
+DESISTO = "https://i.imgur.com/OwMSTHC.png"
+#RUBISH = "https://i.imgur.com/4cZQRvF.png"
+RUBISH = "https://i.imgur.com/MSJw5kB.png"
+RUBISH = "https://i.imgur.com/fCEvaqu.png"
+ROFFX, ROFFY, TOFF, SCAL =625, 430, 10, 2.5
+#HALO = "https://imgur.com/FiS2X97.gif"
+HALO = "https://i.imgur.com/fCEvaqu.png"
+lixo = ['mala', 'chaves', 'escova', 'isqueiro', 'suco', 'vinil', 'baralho', 'dez',
+        'eifell', 'porquinho', 'bule', 'luva', 'panda', 'cafe', 'guitarra', 'aranha',
+        'livro', 'soldado', 'garrafa', 'pizza', 'fone', 'microfone', 'plug', 'visa', 'lata', 'moeda', 'carro', 'sino',
+        'presente', 'ipod', 'clarineta', 'cinquenta', 'sujeira', 'blob', 'sujo',
+        'facao', 'copinho', 'espremedor', 'pandeiro', 'sacola', 'latao', 'pimenta', 'areia',
+        'regar', 'latinha', 'casca', 'hd', 'tenis', 'filme', 'ampulheta', 'pimentao',
+        'bumerangue', 'relogio_pulso', 'relogio', 'oculos', 'martelo', 'faca', 'joaninha',
+        'radio', 'bussola', 'chapeu', 'alicate', 'trolha', 'tamborim', 'pelucia', 'formiga',
+        'jornal', 'chave_fenda', 'vidro', 'cd', 'calculadora', 'lapiseira', 'lampada', 'diploma',
+        'lapis', 'tesoura', 'disquete', 'escorpiao', 'bife', 'lagosta', 'pera', 'cubo', 'canivete',
+        'pulover', 'banana', 'tampinha', 'cantil', 'rolha', 'fava', 'vaso', 'vinho', 'bola',
+        'dalia', 'saco', 'melancia', 'azeitona', 'limao', 'hotdog', 'cachecol', 'papel', 'pote',
+        'picareta']
+
+
+class Thrash:
+    def __init__(self):
+        self.sujeira =['sujeira', 'blob', 'sujo', 'formiga', 'areia', 'casca', 'joaninha', 'escorpiao', 'aranha', 'latinha']*4
+        self.cache = self.create_script_tag(LIXAO)
+        self.bonus = 20
+        pycard = document["pycard"]
+        hidden = Elemento('', style={'position':'absolute', 'top':-2000, 'left':-2000})
+        hidden.elt <= self.cache
+        pycard <= hidden.elt
+        self.comida = ['carpa', 'bacalhau', 'atum', 'robalo', 'dourado']
+        self.resposta = self.limpa = lambda *_: None
+    def dump(self, cena, sorte=4):
+        h =  Hero(1, 2,10)#TheHero()
+        self.cena = cena
+        self.fundo = Elemento(RUBISH, x=0, y=0, w=1350, h=800, cena=cena)
+        self.desiste = Elemento(DESISTO, y=100, cena = cena, vai=self.quit)
+        self.remaining_shuffle_count = 20 + 2*h.pers + h.levl//5
+        self.rubish = svg.svg(version="1.1", viewBox="400 250 1000 600", width="1600", height="800")
+        self.fundo.elt <= self.rubish
+        comer = self.comida * (4 + h.levl//2)
+        shuffle(comer)
+        shuffle(lixo)
+        trash = 20 + 2*h.levl
+        sujo = 10 + 2*h.levl
+        sorte += h.luck + randint(0, h.levl)//3
+        pilha = lixo[:trash] + self.sujeira[:sujo] + comer[:sorte] + ['gato']
+        shuffle(pilha)
+        for indice, label in enumerate(pilha):
+            dx, dy = randint(-300,300) , 100  - randint(-100,100)
+            dy = (300 - abs(dx))//2
+            dx, dy = 200 - dx , 100  - randint(-dy, dy)
+            obj = svg.use(id=f"#{indice:03d}{label}", href=f"#{label}", x=200, y=100 , width=250, height=250,
+            transform=f"translate({dx} {dy})  rotate({7*indice} {ROFFX} {ROFFY}) scale(2.5)")
+            self.rubish <= obj
+            obj.bind('click', self._vai)
+            obj.setAttribute("data-didit", "_no_")
+            #obj.click(self._vai)
+            
+    def _vai_(self, ev):
+        self.__vai(ev)
+    def quit(self, *_):
+        self.remaining_shuffle_count = 0
+        if not self.remaining_shuffle_count:
+            self.fundo.elt.remove()
+            self.desiste.elt.remove()
+            TheHero().learn(self.bonus)
+            return
+    def _vai(self, ev):
+        self.remaining_shuffle_count -= 1
+        if not self.remaining_shuffle_count:
+            self.quit()
+            return
+            
+        from browser import document, alert, svg
+        dx, dy = randint(-300,300) , 100  - randint(-100,100)
+        dy = abs(300 -dx)//3
+        dx, dy = 200 - dx , 100  - randint(-dy,dy)
+        #alert (ev.target.id)
+        obj = document[ev.target.id]
+        obj_name = ev.target.id[4:]
+        if obj.getAttribute("data-didit") == "_did_":
+            return
+        if obj_name in self.comida+["gato"]:
+            food = Elemento('', x=0, y=50, w=200, h=200, tit=f"{ev.target.id}_", cena=self.cena)
+            stag = svg.svg(version="1.1", width="200", height="200")
+            food.elt <= stag
+            #food.tit = f"{ev.target.id}_"
+            stag <= obj
+            obj.setAttribute('transform',f"translate(-{ROFFX-485} -{ROFFY-220}) scale(0.60 1.35)")
+            obj.setAttribute('transform',f"translate(-{ROFFX-485} -{ROFFY-220}) scale(0.60 1.35)")
+            INV.bota(food)
+            #self.__vai = lambda *_: None
+            obj.setAttribute("data-didit", "_did_")
+            if obj_name == "gato":
+                obj.setAttribute('transform',f"translate(-{ROFFX-485} -{ROFFY-345}) scale(0.60 0.6)")
+                #food.vai = TheHero().calma
+                #TheHero().blacking(food.tit)
+                return
+            #TheHero().fishing(food.tit)
+        else:
+            obj.setAttribute('transform',f"translate({dx} {dy})  rotate({7*randint(0,70)} {ROFFX} {ROFFY}) scale(2.5)")
+
+        # obj.transform = f"translate({dx} {dy})  rotate({7*randint(0,70)} {ROFFX} {ROFFY}) scale(2.5)")
+        #obj.setTranslate(dx, dy)
+        #obj.setRotate(7*randint(0,70), ROFFX, ROFFY)
+        # self.c.cx =100
+
+    def create_script_tag(self, src):
+        import urllib.request
+        from browser import document
+        _fp = urllib.request.urlopen(src)
+        _data = _fp.read()
+
+        _tag = document.createElement('div')
+        #_tag.type = "image/svg+xml"
+        _tag.html = _data
+        return _tag
+
 class Sprite(Elemento):
     def __init__(self, img="", vai=None, style=NDCT, tit="", alt="",
                  x=0, y=0, w=100, h=100, o=1, texto='', foi=None, index=0, sw=100, sh=100, cx=1, b=0, s=1,
@@ -84,9 +210,11 @@ lb = [Labirinto(**salas) for salas in LABS]
 Elemento(BSNOW, w=W, h=H, o=0.8, cena=sl[0].norte, style={'pointer-events': 'none'})
 s = 64
 i = sl[0].norte
+j = sl[0].leste
 a = Sprite(ANICONS, x=700, y=300, w=s, h=s, o=0.4, index=0, sw=320, sh=256, cx=5, cena=i, b=4, s=1)  #, tipo="320px 256px")
 s = Elemento(SUPPLY, w=200, cena=i)
 p = Elemento(PARADRP, x=888, w=100, h=50, y=500, cena=i)
+t = Thrash().dump(j)
 i.vai()
 # i.elt.text=LABS
 #print(a.style)
