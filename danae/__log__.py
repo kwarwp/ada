c8f91e4a672097580c6761e1d8f3918ffcd2ae13
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-16 09:24:05 -0300

    Saved danae/__log__.py @2023-10-16 12:24

--- danae/__log__.py
+++ danae/__log__.py
@@ -638,3 +638,21 @@ NameError: name 'h' is not defined
     comer = self.comida * (4 + h.levl//2)
 NameError: name 'h' is not defined
 '''},
+{'date': 'Mon Oct 16 2023 09:24:04.479 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 217
+    t = Thrash().dump(j)
+  module <module> line 85
+    shuffle(comer)
+NameError: name 'shuffle' is not defined
+'''},
