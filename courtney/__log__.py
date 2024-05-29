99c9bfd35e2edf878e5e8ba1c269458c51420080
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2024-05-29 16:49:09 -0300

    Saved courtney/__log__.py @2024-05-29 19:49

--- courtney/__log__.py
+++ courtney/__log__.py
@@ -738,3 +738,19 @@ NameError: name 'Sala' is not defined
     nome_da_sala = Sala(n=nome_da_cena_norte,S=nome_da_cena_leste)
 NameError: name 'Sala' is not defined
 '''},
+{'date': 'Wed May 29 2024 16:49:08.823 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 9
+    nome_da_sala = Sala(n=nome_da_cena_norte, S=nome_da_cena_leste)
+NameError: name 'Sala' is not defined
+'''},
