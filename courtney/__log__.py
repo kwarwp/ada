1cbcfb3394d4216bdd2cdee87565345770a6b664
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2024-05-29 16:42:18 -0300

    Saved courtney/__log__.py @2024-05-29 19:42

--- courtney/__log__.py
+++ courtney/__log__.py
@@ -578,3 +578,19 @@ TypeError: 'Controlador' object is not iterable
     self.menino = Personagem(BOY, controlador=self, destino=self.base_destino, cena=cena, tit = "40kg", x=100, y=100, w=60,h=100)
 AttributeError: 'Controlador' object has no attribute 'base_destino'
 '''},
+{'date': 'Wed May 29 2024 16:42:17.877 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 6
+    imagem_norte_ccmn = Cena(ccmn)
+NameError: name 'ccmn' is not defined
+'''},
