73af082534a14b1f5bb290e5accd18a9948b0aab
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2024-05-29 16:46:29 -0300

    Saved courtney/__log__.py @2024-05-29 19:46

--- courtney/__log__.py
+++ courtney/__log__.py
@@ -642,3 +642,19 @@ NameError: name 'ccmn' is not defined
     imagem_norte_ccmn = Cena(ccmn)
 NameError: name 'ccmn' is not defined
 '''},
+{'date': 'Wed May 29 2024 16:46:29.272 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 2
+    nome_da_cena_norte = Cena(IMAGEM_NORTE)
+NameError: name 'IMAGEM_NORTE' is not defined
+'''},
