5a3835faca91308f055fb13b79dc6d1d0ee136bd
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2024-09-16 19:22:05 -0300

    Saved parisa/__log__.py @2024-09-16 22:22

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -633,3 +633,21 @@ HTTPError: file not found
     color = window.glowscript.color
 AttributeError: no attribute glowscript for [object Window]
 '''},
+{'date': 'Mon Sep 16 2024 19:22:08.163 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 4
+    from _spy.vpython.main import *
+  module _spy.vpython.main line 30
+    def canvas(width=600, height=400, background=window.glowscript.color.black):
+AttributeError: no attribute glowscript for [object Window]
+'''},
