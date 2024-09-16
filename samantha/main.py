8b392a07933a9035165f4aa0693f1d4a0f7b4979
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2024-09-16 19:24:22 -0300

    Saved samantha/main.py @2024-09-16 22:24

--- samantha/main.py
+++ samantha/main.py
@@ -2,8 +2,8 @@ from _spy.vitollino.main import Cena, Texto, Elemento, STYLE
 
 ################################################TAKE1##########################################################
 
-STYLE['width'] = 800
-STYLE['height'] = "100px"
+STYLE['width'] = 1200
+STYLE['height'] = "800px"
 
 entrada = "https://media-cdn.tripadvisor.com/media/photo-s/07/94/9a/9a/primeiro-portao-instalado.jpg"
 pixacao = "https://interartive.org/wp-content/uploads/pixo1.jpg"
